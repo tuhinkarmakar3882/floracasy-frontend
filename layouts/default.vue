64764fbe6e9465b70eb19c7dd8e0c25584e1d749
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-05 17:06:02 +0530

    Concise Layout
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,96 +1,36 @@
 <template>
   <v-app>
-    <v-app-bar
-        app
-        color="white"
-        elevate-on-scroll
-        hide-on-scroll
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <NavigationBar/>
     <v-main>
       <v-progress-linear v-if="loading" color="primary darken-6" indeterminate/>
       <nuxt/>
     </v-main>
 
-    <v-footer app color="red" padless>
-      <v-bottom-navigation
-          v-model="bottomNav"
-          dark
-          shift
-          fixed
-          hide-on-scroll
-      >
-        <v-btn>
-          <span>Video</span>
-          <v-icon>mdi-television-play</v-icon>
-        </v-btn>
 
-        <v-btn>
-          <span>Music</span>
-          <v-icon>mdi-music-note</v-icon>
-        </v-btn>
-
-        <v-btn>
-          <span>Book</span>
-          <v-icon>mdi-book</v-icon>
-        </v-btn>
-
-        <v-btn>
-          <span>Image</span>
-          <v-icon>mdi-image</v-icon>
-        </v-btn>
-      </v-bottom-navigation>
-      <!--      <PageFooter/>-->
-    </v-footer>
+    <BottomNavigationBar/>
+    <!--    <v-footer app color="red" padless></v-footer>-->
   </v-app>
 </template>
 
 <script>
+
+
+import LatestArticles from "@/components/LatestArticles";
 import PageFooter from "@/components/PageFooter";
 import NavigationBar from "@/components/NavigationBar";
+import BottomNavigationBar from "@/components/BottomNavigationBar";
 
 export default {
   components: [
+    LatestArticles,
     PageFooter,
-    NavigationBar
+    NavigationBar,
+    BottomNavigationBar,
   ],
-
-  data() {
+  data: () => {
     return {
-      loading: false,
       bottomNav: 3,
     }
-  },
-  computed: {
-    color() {
-      switch (this.bottomNav) {
-        case 0:
-          return 'blue-grey'
-        case 1:
-          return 'teal'
-        case 2:
-          return 'brown'
-        case 3:
-          return 'indigo'
-      }
-    },
   },
 }
 </script>
