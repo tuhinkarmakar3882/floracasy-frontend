8a5b7195f6bd65285f2c7280bf6942b339110053
Swarnab Mukherjee <swarnab2011@gmail.com>
2020-09-07 22:54:19 +0530

    added comment section -itzzsam

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
     server: {
         port: 8080, // default: 3000
-        host: '0.0.0.0' // default: localhost
+        // host: '0.0.0.0' // default: localhost
     },
     /*
     ** Nuxt rendering mode
---
+++ pages/CommentSection.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+    <input v-model="comment" type="text">
+    <button @click="addComment">Click</button>
+    <ul id="example-1">
+        <li v-for="item in comments" :key="item">
+            {{ item }}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default{
+    name : 'CommentSection',
+    data() {
+    return {
+        comment : "",
+        comments : []
+    }
+  },
+  methods : {
+      addComment(){
+          if(this.isValid(this.comment))
+            this.comments.push(this.comment);
+            this.comment = "";
+      },
+      isValid(comment){
+        return comment.trim().length>0;
+      }
+    }
+}
+</script>
