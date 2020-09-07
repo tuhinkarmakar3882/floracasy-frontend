9aabe7f00e4e4aa0615ac6854259cf540bb65e74
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-07 23:17:45 +0530

    Updated CommentSection.vue | Tuhin
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- pages/CommentSection.vue
+++ pages/CommentSection.vue
@@ -1,33 +1,43 @@
 <template>
-<div>
-    <input v-model="comment" type="text">
-    <button @click="addComment">Click</button>
+  <div>
+
+    <v-text-field
+        v-model="comment"
+        label="Comment"
+        required
+    ></v-text-field>
+
+    <v-btn color="primary" @click="addComment">Add Comment</v-btn>
     <ul id="example-1">
-        <li v-for="item in comments" :key="item">
-            {{ item }}
-        </li>
+      <li v-for="item in comments" :key="item">
+        {{ item }}
+      </li>
     </ul>
-</div>
+  </div>
 </template>
 
 <script>
-export default{
-    name : 'CommentSection',
-    data() {
+
+export default {
+  name: 'CommentSection',
+  data() {
     return {
-        comment : "",
-        comments : []
+      comment: "",
+      comments: []
     }
   },
-  methods : {
-      addComment(){
-          if(this.isValid(this.comment))
-            this.comments.push(this.comment);
-            this.comment = "";
-      },
-      isValid(comment){
-        return comment.trim().length>0;
-      }
-    }
+  methods: {
+    addComment() {
+      if (this.isValid(this.comment))
+        this.comments.push(this.comment);
+
+      this.comment = "";
+    },
+
+    isValid(comment) {
+      return comment.trim().length > 0;
+    },
+
+  }
 }
 </script>
