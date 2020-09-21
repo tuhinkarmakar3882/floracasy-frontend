42ac00b2b53135148999005ed644e5f5b414f157
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-21 18:39:58 +0530

    Added Footer.vue
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <footer>
+    <div class="row">
+      <div class="col">
+        <h3>In a Nutshell</h3>
+
+        <KeyPointsList
+            :points="points"
+            :tickSize="24"
+            tickColor="#10a3ce"
+        />
+
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  components: {
+    KeyPointsList: () => import('@/components/KeyPointsList')
+  },
+  data: () => {
+    return {
+      points: [
+        "Enjoy Affordable Pro Perks",
+        "Enjoy Affordable Pro Perks",
+        "Enjoy Affordable Pro Perks",
+        "Enjoy Affordable Pro Perks",
+      ],
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
