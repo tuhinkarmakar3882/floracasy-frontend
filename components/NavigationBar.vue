17afb9068819a509ade3231842ffb84fbab98e2d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-21 17:20:18 +0530

    Added NavigationBar.vue
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ components/NavigationBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="navigation-bar">
+
+    <div class="brand">
+      <Logo :width="100"/>
+      <nuxt-link class="brand-name" to="/">Floracasy</nuxt-link>
+    </div>
+
+    <ul class="menu-list">
+      <li class="menu-list-item">
+        <nuxt-link class="menu-list-link" to="/">Go Premium</nuxt-link>
+      </li>
+      <li class="menu-list-item">
+        <nuxt-link class="menu-list-link" to="/">Write &amp; Earn</nuxt-link>
+      </li>
+      <li class="menu-list-item">
+        <nuxt-link class="menu-list-link" to="/">Log in</nuxt-link>
+      </li>
+      <li class="menu-list-item">
+        <nuxt-link class="menu-list-link" to="/">Join us</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+  components: {
+    Logo: () => import('~/components/Logo'),
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+.navigation-bar {
+
+  .brand {
+    .brand-name {
+    }
+  }
+
+  .menu-list {
+    .menu-list-item {
+      .menu-list-link {
+      }
+    }
+  }
+}
+
+</style>
