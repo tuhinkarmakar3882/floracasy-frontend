86ff3c4c19c6420641fae404d00b6211c7aa533c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-22 17:08:49 +0530

    Updated KeyPointsList.vue to KeyPoint.vue and migrated the logic
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ components/KeyPoint.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div>
+    <v-icon :color="tickColor" :size="tickSize">
+      mdi-check-circle-outline
+    </v-icon>
+    <span>
+      {{ point }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "KeyPoint",
+  props: {
+    tickColor: {
+      type: String,
+      required: true
+    },
+    tickSize: {
+      type: Number,
+      required: true
+    },
+    point: {
+      type: String,
+      required: true
+    },
+  },
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import "assets/variables";
+
+div {
+  color: $muted;
+  display: flex;
+  align-items: flex-start;
+  font-size: 18px;
+  font-weight: 300;
+
+  span {
+    margin-left: 24px;
+  }
+}
+
+</style>
