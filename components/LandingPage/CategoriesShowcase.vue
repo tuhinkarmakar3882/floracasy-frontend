c9044b723e92fd17074ab2aec5018dcc2f38f4cd
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-25 18:59:37 +0530

    Fix Grid Layout
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- components/LandingPage/CategoriesShowcase.vue
+++ components/LandingPage/CategoriesShowcase.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="showcase my-4 py-4 text-center">
+  <div class="showcase my-4 py-6 text-center">
     <v-container>
       <div class="top-header">
         <p class="topic">Categories</p>
@@ -7,30 +7,20 @@
         <p>Explore hundreds of integrations for Ghost to speed up your workflow, or build your own
           custom integrations with our open source developer SDK</p>
       </div>
-      <div class="show-reel grid-container justify-center">
+
+      <div class="grid-container">
+
         <div v-for="item in items" :key="item.name" class="grid-col">
-          <img :alt="item.name" :src="item.image" style="width: 100%; object-fit: cover; border-radius: 50%"/>
+          <img :alt="item.name" :src="item.image"/>
           <p class="my-4">{{ item.name }}</p>
         </div>
+
         <div class="grid-col">
           <img alt="item.name" src="https://picsum.photos/151?random"
                style="width: 100%; object-fit: cover; border-radius: 50%"/>
           <p class="my-4">More</p>
         </div>
-      </div>
 
-      <div class="show-reel grid-container justify-center">
-        <div v-for="item in items" :key="item.name" class="grid-col">
-          <v-chip
-              class="ma-2"
-              color="primary"
-              outlined
-              pill
-          >
-            {{ item.name }}
-            <v-icon right>mdi-account-outline</v-icon>
-          </v-chip>
-        </div>
       </div>
 
       <v-btn class="my-6 btn" color="primary">
@@ -123,20 +113,14 @@ export default {
     grid-column-gap: 36px;
     grid-row-gap: 28px;
 
-    //border: 1px solid green;
-
-    @media only screen and (min-width: $medium) {
-      grid-template-columns: repeat(5, 1fr);
-      .grid-col {
-        width: 150px;
-      }
-    }
-
-
     .grid-col {
-      width: auto;
-      //border: 1px solid yellow;
-      font-size: clamp(22px, 4vw, 24px);
+      border: 1px solid yellow;
+
+      img {
+        width: 100%;
+        object-fit: cover;
+        border-radius: 50%;
+      }
     }
   }
 
