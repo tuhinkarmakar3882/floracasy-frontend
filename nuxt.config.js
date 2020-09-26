e089965534df771276e9f754a4fd1f9c232f7055
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-26 12:12:16 +0530

    Experimenting #2``
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -137,21 +137,22 @@ export default {
         }
     },
 
-    build: {
-        extractCSS: true,
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    styles: {
-                        name: 'styles',
-                        test: /\.(css|vue)$/,
-                        chunks: 'all',
-                        enforce: true
-                    }
-                }
-            }
-        }
-    },
+    // build: {
+    //     extractCSS: true,
+    //     optimization: {
+    //         splitChunks: {
+    //             cacheGroups: {
+    //                 styles: {
+    //                     name: 'styles',
+    //                     test: /\.(css|vue)$/,
+    //                     chunks: 'all',
+    //                     enforce: true
+    //                 }
+    //             }
+    //         }
+    //     }
+    // },
+    build: {},
     // loadingIndicator: '~/cli.html',
     loadingIndicator: {
         name: 'rectangle-bounce',
