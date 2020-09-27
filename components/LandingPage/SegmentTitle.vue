08695b6f31c243cc418f7126c143e2ec831c10e3
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-09-27 16:40:19 +0530

    Extracted into Vue Component
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ components/LandingPage/SegmentTitle.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="top-header">
+    <p class="topic">{{ topic }}</p>
+    <h2 class="mt-0 mb-6 pb-4">{{ title }}</h2>
+    <p>{{ leadingParagraph }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SegmentTitle',
+  props: {
+    topic: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    leadingParagraph: {
+      type: String,
+      required: true
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import "assets/variables";
+
+.top-header {
+
+  .topic {
+    color: $secondary-highlight;
+    font-weight: 700;
+  }
+
+  h2 {
+    font-family: $Prata;
+    font-weight: 400;
+    font-size: clamp(24px, 4vw, 40px);
+    color: $white;
+  }
+
+  p {
+    font-weight: 300;
+  }
+
+}
+</style>
