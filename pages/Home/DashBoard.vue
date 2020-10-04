0a1994a8a4be4fd11354cbf8b9b38711990f716b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-04 22:03:38 +0530

    Added New Protected Route
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/DashBoard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="text-center my-6 py-6 px-4">
+    <v-icon color="gold-tone" size="100">mdi-home</v-icon>
+    <h1 class="my-4">This is my super duper DASHBOARD Page</h1>
+    <p class="my-4">And I love it (Did you notice that the layout has changed?)</p>
+    <hr>
+    <h3 class="my-4">You Can Only see this after logging in.</h3>
+    <p class="my-4">But this is not persistent. This will go away, may be after a refresh. I need to persist it
+      somehow.</p>
+    <hr>
+    <p class="my-4">Anyways, here's a logout button</p>
+    <v-btn class="button px-4" color="secondary-matte" outlined width="250" @click="logout">
+      <span>Log out</span>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashBoard",
+  middleware: "authenticatedRequest",
+  layout: "HomeAppLayout",
+  methods: {
+    async logout() {
+      try {
+        await this.$store.commit('setAuth', false);
+        console.log(this.$store.state.auth);
+        await this.$router.push('/');
+      } catch (e) {
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
