32bfeeb5e3528c74811863a2c7a644e394bb7427
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-08 21:55:24 +0530

    oops
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- pages/Home/Account/Details.vue
+++ pages/Home/Account/Details.vue
@@ -51,54 +51,6 @@
 
 
     </main>
-    <main class="container">
-      <div class="user-profile">
-
-        <img alt="profile picture" class="picture" src="https://picsum.photos/200"/>
-
-        <p class="name"> Swagata Biswas Karmakar </p>
-
-        <p class="designation"> Beauty Blogger </p>
-
-        <ul class="stats">
-          <li> 22 blogs</li>
-          <li> 230 follower</li>
-          <li> 2k engagement</li>
-        </ul>
-
-        <p class="about">
-          Here some one or liner about by blogger; this about is a good hint for readers
-        </p>
-
-        <a href="#"> Swagata_Biswas </a>
-
-        <a href="#"> @swagataB </a>
-
-        <button>Follow</button>
-        <button>Message</button>
-
-        <hr/>
-      </div>
-
-      <div class="blogs">
-        <p>New </p>
-        <img alt="image" src="https://picsum.photos/250">
-        <h3> It is blogs headline & readers love it </h3>
-        <p> Sat Aug 19, 2020 11:34 A.M. </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aperiam, praesentium. Adipisci commodi dolores
-          ducimus in ipsum laboriosam molestias officia possimus quaerat quibusdam reiciendis sequi, temporibus velit.
-          Laborum mollitia, quidem!</p>
-        <button>Read more</button>
-
-        <hr/>
-
-        <h5>
-          <u>1 week ago</u>
-        </h5>
-      </div>
-
-
-    </main>
   </div>
 </template>
 
