0ac849a9e516c74d0b6b5f4d7e32cca750df0a1b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-10 19:27:28 +0530

    Added Setup Scripts for Pm2
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    apps: [{
-        name: 'Floracasy', // App name that shows in `pm2 ls`
-        exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-    }]
+    apps: [
+        {
+            name: 'Floracasy',
+            exec_mode: 'cluster',
+            instances: 'max', // Or a number of instances
+            // script: "./node_modules/nuxt-start/bin/nuxt-start.js",
+            script: './node_modules/nuxt/bin/nuxt.js',
+            args: 'start',
+            env: {
+                "HOST": "127.0.0.1",
+                "PORT": 3001,
+                "NODE_ENV": "production",
+            }
+        }
+    ]
 }
---
+++ scripts/pm2-reset.sh
@@ -0,0 +1,3 @@
+pm2 stop all
+pm2 flush
+pm2 delete all
