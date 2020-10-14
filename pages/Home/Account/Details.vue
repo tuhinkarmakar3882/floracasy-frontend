45b3b3d990dacb932376afd8780fa758d3bc9659
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-14 18:28:18 +0530

    Added Divider
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Account/Details.vue
+++ pages/Home/Account/Details.vue
@@ -3,16 +3,16 @@
 
     <section class="user-profile">
 
-      <div class="basic-data dbx">
-        <img alt="profile-picture dbx" class="picture" src="https://picsum.photos/200"/>
-        <div class="basic-details dbx">
+      <div class="basic-data">
+        <img alt="profile-picture" class="picture" src="https://picsum.photos/200"/>
+        <div class="basic-details">
           <p class="name"> Travis Alice </p>
           <p class="designation"> Beauty Blogger </p>
         </div>
       </div>
 
-      <section class="stats dbx">
-        <div v-for="item in statisticsItem" :key="item.id" class="item dbx">
+      <section class="stats">
+        <div v-for="item in statisticsItem" :key="item.id" class="item">
           <span class="number">{{ item.value }}</span>
           <p class="type"> {{ item.type }} </p>
         </div>
@@ -38,22 +38,21 @@
       </section>
     </section>
 
-    <!--    <section class="blogs">-->
-    <!--      <p>New </p>-->
-    <!--      <img alt="image" src="https://picsum.photos/250">-->
-    <!--      <h3> It is blogs headline & readers love it </h3>-->
-    <!--      <p> Sat Aug 19, 2020 11:34 A.M. </p>-->
-    <!--      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aperiam, praesentium. Adipisci commodi dolores-->
-    <!--        ducimus in ipsum laboriosam molestias officia possimus quaerat quibusdam reiciendis sequi, temporibus velit.-->
-    <!--        Laborum mollitia, quidem!</p>-->
-    <!--      <button>Read more</button>-->
+    <section class="recent-activity">
+      <h3 class="heading">Recent Activities</h3>
 
-    <!--      <hr/>-->
+      <div v-for="blog in blogs" :key="blog.id" class="blog my-8">
+        <div class="content">
+          <img :alt="blog.title" :src="blog.image">
+          <div class="data">
+            <h3> {{ blog.title }} </h3>
+            <small> {{ blog.time }}</small>
+          </div>
+        </div>
 
-    <!--      <h5>-->
-    <!--        <u>1 week ago</u>-->
-    <!--      </h5>-->
-    <!--    </section>-->
+        <v-divider class="mt-8" dark/>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -81,6 +80,44 @@ export default {
           type: "Followers",
         },
       ],
+      blogs: [
+        {
+          id: 0,
+          title: "Now its easy to take care your underarms at home.",
+          image: "https://picsum.photos/200",
+          time: 'Sat Aug 19, 2020 11:34 A.M.',
+        },
+        {
+          id: 1,
+          title: "Love your hair but suffering from Hair fall or hair damage?",
+          image: "https://picsum.photos/200",
+          time: 'Sat Aug 19, 2020 11:34 A.M.',
+        },
+        {
+          id: 2,
+          title: "Bath - Hot water or Cold water? Let's see!",
+          image: "https://picsum.photos/200",
+          time: 'Sat Aug 19, 2020 11:34 A.M.',
+        },
+        {
+          id: 3,
+          title: "What should be your diet schedule?",
+          image: "https://picsum.photos/200",
+          time: 'Sat Aug 19, 2020 11:34 A.M.',
+        },
+        {
+          id: 4,
+          title: "Now you can get salon like pedicure for free at home",
+          image: "https://picsum.photos/200",
+          time: 'Fri Aug 18, 2020 01:34 A.M.',
+        },
+        {
+          id: 5,
+          title: "Miracle Face Mask According To Your Skin Type",
+          image: "https://picsum.photos/200",
+          time: 'Sat Aug 19, 2020 11:34 A.M.',
+        },
+      ]
     }
   },
   mounted() {
@@ -93,8 +130,9 @@ export default {
 @import "assets/variables";
 
 .details-page {
+  padding: 0 1rem;
+
   .user-profile {
-    padding: 0 1rem;
 
     .basic-data {
       display: grid;
@@ -164,6 +202,44 @@ export default {
         font-weight: 300;
         color: #9a9a9a;
         letter-spacing: 0;
+      }
+    }
+  }
+
+  .recent-activity {
+    .heading {
+      text-align: left;
+      font-weight: 400;
+      color: #FFF;
+      font-size: 20px;
+      margin-top: 2rem;
+      font-family: $Prata;
+      margin-bottom: 1rem;
+    }
+
+    .content {
+      display: grid;
+      grid-template-columns: 1fr 3fr;
+      grid-column-gap: 1rem;
+      place-items: center;
+
+      img {
+        width: 100%;
+        object-fit: cover;
+        border-radius: 50%;
+      }
+
+      .data {
+        text-align: left;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+
+        * {
+          margin: 0;
+          padding: 0;
+        }
       }
     }
   }
