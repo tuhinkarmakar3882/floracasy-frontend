d9abfae91387e0b59fa83ebebf71590991545de9
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-15 21:52:27 +0530

    Added Placeholder Carousel
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/DashBoard.vue
+++ pages/Home/DashBoard.vue
@@ -12,12 +12,39 @@
     </section>
 
     <section class="carousel my-5">
-      <v-divider dark/>
-      <h1 class="text-center my-4">The Carousel will Come Here</h1>
-      <v-divider dark/>
+      <v-carousel
+          cycle
+          height="400"
+          hide-delimiter-background
+          show-arrows-on-hover
+      >
+        <v-carousel-item
+            v-for="(slide, i) in slides"
+            :key="i"
+        >
+          <v-sheet
+              :color="colors[i]"
+              height="100%"
+          >
+            <v-row
+                align="center"
+                class="fill-height"
+                justify="center"
+            >
+              <div class="display-3">
+                {{ slide }} Slide
+              </div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
     </section>
 
-
+    <section class="carousel my-5">
+      <v-divider dark/>
+      <h1 class="text-center my-4">Recent Blogs</h1>
+      <v-divider dark/>
+    </section>
   </div>
 </template>
 
@@ -49,6 +76,20 @@ export default {
           id: '4',
           name: 'Music',
         },
+      ],
+      colors: [
+        'indigo',
+        'warning',
+        'pink darken-2',
+        'red lighten-1',
+        'deep-purple accent-4',
+      ],
+      slides: [
+        'First',
+        'Second',
+        'Third',
+        'Fourth',
+        'Fifth',
       ],
     };
   },
