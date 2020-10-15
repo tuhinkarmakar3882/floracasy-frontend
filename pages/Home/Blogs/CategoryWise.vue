86003fca2028b80930c184fd4f03eac436b13498
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-15 10:40:31 +0530

    Added CategoryWise.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/Blogs/CategoryWise.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center my-6 py-6 px-4">
+    <v-icon color="gold-tone" size="100">mdi-apps</v-icon>
+    <h1 class="my-4">This is my super duper Category View Page</h1>
+    <p class="my-4">And I love it </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryWise",
+  layout: "HomeAppLayout",
+  middleware: "authenticatedRequest",
+  mounted() {
+    this.$store.commit('BottomNavigation/update', {linkPosition: 1})
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
