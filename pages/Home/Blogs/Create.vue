e9ea5cab80608fac6c74779ee66ef73425ed6a93
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-15 10:40:51 +0530

    Added Blog Create.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/Blogs/Create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center my-6 py-6 px-4">
+    <v-icon color="gold-tone" size="100">mdi-plus-circle</v-icon>
+    <h1 class="my-4">This is my super duper Create a new Blog Page</h1>
+    <p class="my-4">And I love it </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Create",
+  middleware: "authenticatedRequest",
+  layout: "HomeAppLayout",
+  mounted() {
+    this.$store.commit('BottomNavigation/update', {linkPosition: 2})
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
