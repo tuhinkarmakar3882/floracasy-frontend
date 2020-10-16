11ffd3e23c7f1760b35df84c4a816760c41d9082
Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>
2020-10-16 14:40:23 +0530

    Added Experimental Loaders
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- pages/Home/DashBoard.vue
+++ pages/Home/DashBoard.vue
@@ -5,64 +5,28 @@
 
     <Carousel :carouselItems="carouselItems"/>
 
-    <v-parallax
-        dark
-        src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+
+    <v-sheet
+        v-for="i in [0,1,2,3,4,5,6,7,8,9]"
+        :key="i"
+        class="pa-3"
+        color="grey darken-4"
     >
-      <v-row
-          align="center"
-          justify="center"
-      >
-        <v-col
-            class="text-center"
-            cols="12"
-        >
-          <h1 class="display-1 font-weight-thin mb-4">
-            Parallax
-          </h1>
-          <h4 class="subheading">
-            Build your application today!
-          </h4>
-        </v-col>
-      </v-row>
-    </v-parallax>
+      <v-skeleton-loader
+          class="pb-5"
+          type="card-avatar"
+      />
+      <v-skeleton-loader
+          class="pb-4"
+          type="article"
+      />
+      <v-skeleton-loader
+          class="pb-4"
+          type="actions"
+      />
+      <v-divider class="my-7" dark/>
+    </v-sheet>
 
-    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores aut beatae consequatur cupiditate dicta
-      eaque, eligendi et expedita explicabo facere hic itaque laboriosam molestiae nisi perspiciatis provident, qui rem?
-    </div>
-    <div>Ab atque dolores eaque iste maiores molestiae necessitatibus officia officiis porro quasi qui, quod repellat,
-      sint, vel vitae. Aliquam aperiam atque cum ducimus, eligendi maxime officiis quam quia similique totam!
-    </div>
-    <div>A asperiores autem beatae commodi culpa dicta doloribus ducimus eaque esse in incidunt nam nemo officiis omnis
-      optio perferendis perspiciatis, placeat provident quam quibusdam quis repudiandae sunt ullam vero voluptatem?
-    </div>
-    <div>Modi quam quibusdam sequi ullam? Aliquam, fugiat, possimus! Accusamus consectetur cupiditate dignissimos, illum
-      iste itaque magnam magni necessitatibus nihil quos? Corporis earum ipsa laborum, nostrum quas tempora tenetur
-      totam! Dicta?
-    </div>
-    <div>Ad dolore nam quibusdam sapiente suscipit. Ad aspernatur beatae cum eius enim eos iste maiores minima,
-      molestias mollitia natus, nihil omnis qui quis saepe sapiente soluta sunt totam velit veniam.
-    </div>
-    <div>Aut autem cum illum ipsa, laborum obcaecati quam sequi sit. Accusamus ad amet asperiores delectus distinctio
-      esse ex exercitationem illo modi nemo, nihil, nulla, porro quae quaerat rem voluptates voluptatibus!
-    </div>
-    <div>Alias animi consequuntur dicta iste minus numquam odit optio placeat quo veniam. Dolores eum harum id natus? At
-      blanditiis doloribus facilis magni minima minus mollitia porro sint sit tenetur. Molestias?
-    </div>
-    <div>Accusantium aliquam architecto, at autem commodi dolorem ea error, exercitationem illum in minima nemo nihil
-      nisi nulla omnis quae quam quas quasi quia quidem, quo saepe temporibus ut voluptates voluptatum.
-    </div>
-    <div>Amet assumenda consequatur cum possimus quidem temporibus? Ab eveniet iure molestias placeat porro quaerat quod
-      reprehenderit voluptatem! Debitis, distinctio ducimus fugit id neque non nostrum provident reiciendis, suscipit
-      voluptate voluptatum.
-    </div>
-    <div>Eaque, non numquam quam quas quod quos ullam. Aut culpa eveniet illum minus natus nobis obcaecati pariatur,
-      porro, praesentium quaerat ratione sed tempora vero! Adipisci maiores porro recusandae repellat vero.
-    </div>
-
-    <CategoriesLineUp :categories="categories"/>
-
-    <Carousel :carouselItems="carouselItems"/>
 
   </div>
 </template>
@@ -124,6 +88,11 @@ export default {
           color: 'deep-purple accent-4',
         },
       ],
+      attrs: {
+        class: 'mb-6',
+        // boilerplate: true,
+        elevation: 2,
+      },
     };
   },
   mounted() {
