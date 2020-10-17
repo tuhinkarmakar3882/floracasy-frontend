d7d5b96761448a65ad4921015df61c1bcff2e828
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-17 15:32:01 +0530

    Formatted.And Fixed CSS
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- api/index.js
+++ api/index.js
@@ -17,16 +17,16 @@ router.use((req, res, next) => {
 // Add POST - /api/login
 router.post('/login', (req, res) => {
   if (req.body.username === 'demo' && req.body.password === 'demo') {
-    req.session.authUser = {username: 'demo'}
-    return res.json({username: 'demo'})
+    req.session.authUser = { username: 'demo' }
+    return res.json({ username: 'demo' })
   }
-  res.status(401).json({message: 'Bad credentials'})
+  res.status(401).json({ message: 'Bad credentials' })
 })
 
 // Add POST - /api/logout
 router.post('/logout', (req, res) => {
   delete req.session.authUser
-  res.json({ok: true})
+  res.json({ ok: true })
 })
 
 // Export the server middleware
