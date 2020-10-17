c4c73496603b474d90e59acad7a0a82d5120984f
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-17 22:04:57 +0530

    Added ShowCase Page for StyleGuide.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/StyleGuide.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="style-guide-page">
+    <h1 class="text-center" style="font-size: 32px">Style Guide</h1>
+    <hr />
+    <h3>Would you like to join us?</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad cum
+      enim eveniet, expedita fuga illum laborum natus non omnis pariatur
+      provident quaerat qui quidem quos tempora ullam vitae voluptatem!
+    </p>
+    <br />
+    <div class="buttons">
+      <div class="normal text-center">
+        <button class="primary-btn">Primary Button</button>
+        <button class="secondary-btn">Secondary Button</button>
+        <button class="success-btn">Success Button</button>
+        <button class="danger-btn">Danger Button</button>
+        <button class="info-btn">Info Button</button>
+        <button class="muted-btn">Muted Button</button>
+        <button class="disabled-btn">Disabled Button</button>
+      </div>
+      <div class="outlined text-center">
+        <button class="primary-outlined-btn">Primary Button</button>
+        <button class="secondary-outlined-btn">Secondary Button</button>
+        <button class="success-outlined-btn">Success Button</button>
+        <button class="danger-outlined-btn">Danger Button</button>
+        <button class="info-outlined-btn">Info Button</button>
+        <button class="muted-outlined-btn">Muted Button</button>
+        <button class="disabled-btn">Disabled Button</button>
+      </div>
+    </div>
+
+    <hr />
+
+    <br /><br /><br /><br />
+
+    <h1>This is a sample heading H1</h1>
+    <h2>This is a sample heading H2</h2>
+    <h3>This is a sample heading H3</h3>
+    <h4>This is a sample heading H4</h4>
+    <h5>This is a sample heading H5</h5>
+    <h6>This is a sample heading H6</h6>
+    <p>
+      There are many variations of passages of Lorem Ipsum available, but the
+      majority have suffered alteration in some form, by injected humour, or
+      randomised words which don't look even slightly believable. If you are
+      going to use a passage of Lorem Ipsum, you need to be sure there isn't
+      anything embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as necessary,
+      making this the first true generator on the Internet. It uses a dictionary
+      of over 200 Latin words, combined with a handful of model sentence
+      structures, to generate Lorem Ipsum which looks reasonable. The generated
+      Lorem Ipsum is therefore always free from repetition, injected humour, or
+      non-characteristic words etc. There are many variations of passages of
+      Lorem Ipsum available, but the majority have suffered alteration in some
+      form, by injected humour, or randomised words which don't look even
+      slightly believable. If you are going to use a passage of Lorem Ipsum, you
+      need to be sure there isn't anything embarrassing hidden in the middle of
+      text. All the Lorem Ipsum generators on the Internet tend to repeat
+      predefined chunks as necessary, making this the first true generator on
+      the Internet. It uses a dictionary of over 200 Latin words, combined with
+      a handful of model sentence structures, to generate Lorem Ipsum which
+      looks reasonable. The generated Lorem Ipsum is therefore always free from
+      repetition, injected humour, or non-characteristic words etc. There are
+      many variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injected humour, or randomised
+      words which don't look even slightly believable. If you are going to use a
+      passage of Lorem Ipsum, you need to be sure there isn't anything
+      embarrassing hidden in the middle of text. All the Lorem Ipsum generators
+      on the Internet tend to repeat predefined chunks as necessary, making this
+      the first true generator on the Internet. It uses a dictionary of over 200
+      Latin words, combined with a handful of model sentence structures, to
+      generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is
+      therefore always free from repetition, injected humour, or
+      non-characteristic words etc.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StyleGuide',
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/style-guide';
+</style>
