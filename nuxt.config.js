cda17213626864a9e1d4032312555c1092c5b984
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-17 11:35:51 +0530

    Added Accessibility settings
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,8 +42,7 @@ export default {
       },
       {
         name: 'viewport',
-        content:
-          'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
       },
       {
         hid: 'description',
