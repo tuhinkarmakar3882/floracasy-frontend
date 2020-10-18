7f38719d85adcdcc16de6084f719dab089d1afc7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-18 13:41:21 +0530

    Added RippleButton.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/global/RippleButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    v-ripple="'rgba(255, 255, 255, 0.35)'"
+    type="button"
+    :class="disabled ? 'disabled-btn' : this.$props.classList"
+    :disabled="disabled"
+  >
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'RippleButton',
+  props: {
+    classList: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
