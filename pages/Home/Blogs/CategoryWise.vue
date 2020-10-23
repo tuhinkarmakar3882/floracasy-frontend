42e7534e95b28109221d9986dad9c1cc59588717
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-23 14:57:22 +0530

    Updated Link Pos Number for CategoryWise.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Blogs/CategoryWise.vue
+++ pages/Home/Blogs/CategoryWise.vue
@@ -12,7 +12,7 @@ export default {
   layout: 'MobileApp',
   middleware: 'protectedRoute',
   mounted() {
-    this.$store.commit('BottomNavigation/update', { linkPosition: 1 })
+    this.$store.commit('BottomNavigation/update', { linkPosition: 0 })
   },
 }
 </script>
