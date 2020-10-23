9b455a97fbd85cca5e40a2cb07a83ab9b0603776
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-23 18:29:20 +0530

    Added EditorLayout.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ layouts/EditorLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="editor-layout">
+    <header>
+      <h5 class="mdi mdi-arrow-left" @click="$router.back()" />
+      <button v-ripple class="primary-btn">Publish</button>
+    </header>
+
+    <main>
+      <nuxt />
+    </main>
+
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditorLayout',
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.editor-layout {
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 $medium-unit;
+    height: 2 * $x-large-unit;
+  }
+
+  main {
+    padding-top: 2 * $x-large-unit;
+  }
+
+  button {
+    min-width: auto;
+  }
+}
+</style>
--- pages/Home/Blogs/Create.vue
+++ pages/Home/Blogs/Create.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center my-6 py-6 px-4">
-    <span> mdi-plus-circle </span>
-    <h1 class="my-4">This is my super duper Create a new Blog Page</h1>
+    <h1 class="mdi mdi-plus-circle" />
+    <h5 class="my-4">This is my super duper Create a new Blog Page</h5>
     <p class="my-4">And I love it</p>
   </div>
 </template>
@@ -9,8 +9,8 @@
 <script>
 export default {
   name: 'Create',
-  layout: 'MobileApp',
-  middleware: 'protectedRoute',
+  layout: 'EditorLayout',
+  // middleware: 'protectedRoute',
   mounted() {
     this.$store.commit('BottomNavigation/update', { linkPosition: 2 })
   },
