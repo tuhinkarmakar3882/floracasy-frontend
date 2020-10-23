cdaf339508faa9b61a622554e16f36bf17738271
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-23 14:58:06 +0530

    Updated Link Pos Number for Notifications/index.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Notifications/index.vue
+++ pages/Home/Notifications/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center my-6 py-6 px-4">
-    <span> mdi-bell </span>
-    <h1 class="my-4">This is my super duper Notifications View Page</h1>
+    <h1 class="mdi mdi-bell" />
+    <h6 class="my-4">This is my super duper Notifications View Page</h6>
     <p class="my-4">And I love it</p>
   </div>
 </template>
