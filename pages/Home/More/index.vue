4ffc32a4b4703e280b9035365a609276a3290d6d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-23 14:57:54 +0530

    Updated Link Pos Number for More/index.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/More/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center my-6 py-6 px-4">
+    <h1 class="mdi mdi-dots-horizontal-circle" />
+    <h6 class="my-4">This is my super duper More Options Page</h6>
+    <p class="my-4">And I love it</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MoreOptions',
+  middleware: 'protectedRoute',
+  layout: 'MobileApp',
+  mounted() {
+    this.$store.commit('BottomNavigation/update', { linkPosition: 4 })
+  },
+}
+</script>
