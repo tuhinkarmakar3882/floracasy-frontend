41b10c7321acedbe309e9ab3a33e293a5afe7d09
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-23 14:57:41 +0530

    Updated Link Pos Number for Messages/index.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/Messages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center my-6 py-6 px-4">
+    <h1 class="mdi mdi-message-text" />
+    <h6 class="my-4">This is my super duper Message View Page</h6>
+    <p class="my-4">And I love it</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Messages',
+  middleware: 'protectedRoute',
+  layout: 'MobileApp',
+  mounted() {
+    this.$store.commit('BottomNavigation/update', { linkPosition: -1 })
+  },
+}
+</script>
