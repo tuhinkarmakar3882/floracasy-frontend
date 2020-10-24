377d5d5288b6e4f3283c620151e50577b344e35c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-24 20:46:16 +0530

    Sorry!
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Blogs/Create.vue
+++ pages/Home/Blogs/Create.vue
@@ -99,7 +99,7 @@ import TextInput from '@/components/global/TextInput'
 export default {
   name: 'Create',
   layout: 'EditorLayout',
-  // middleware: 'protectedRoute',
+  middleware: 'protectedRoute',
   components: {
     TextInput,
     VueEditor,
