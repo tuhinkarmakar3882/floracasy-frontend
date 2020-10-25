a814c7b11f2b92781c4a55721e55d05af01bcdb5
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-25 20:34:40 +0530

    Added More Options Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/More/index.vue
+++ pages/Home/More/index.vue
@@ -1,18 +1,124 @@
 <template>
-  <div class="text-center my-6 py-6 px-4">
-    <h1 class="mdi mdi-dots-horizontal-circle" />
-    <h6 class="my-4">This is my super duper More Options Page</h6>
-    <p class="my-4">And I love it</p>
+  <div class="more-options-page">
+    <ul>
+      <li
+        v-for="(option, index) in options"
+        :key="index"
+        v-ripple
+        class="px-4 py-4"
+      >
+        <p>
+          <span
+            class="icon"
+            :class="option.icon"
+            :style="{ color: option.color }"
+          />
+          <span class="option-name">{{ option.name }}</span>
+        </p>
+      </li>
+      <li v-ripple class="px-4 py-4" @click="logout">
+        <p>
+          <span class="icon mdi mdi-power" style="color: #f77479" />
+          <span class="option-name">Log out</span>
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.more-options-page {
+  ul {
+    li {
+      p {
+        display: grid;
+        grid-template-columns: 1fr 4fr;
+        grid-column-gap: 1rem;
+        place-items: center;
+        text-align: left;
+
+        .icon {
+          font-size: 2rem;
+        }
+
+        .option-name {
+          font-size: 1.2rem;
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+</style>
 
 <script>
 export default {
   name: 'MoreOptions',
   middleware: 'protectedRoute',
   layout: 'MobileApp',
+  data() {
+    return {
+      options: [
+        {
+          name: 'Preference',
+          icon: 'mdi mdi-cog',
+          color: '#8FF2E1',
+        },
+        {
+          name: 'Go Premium',
+          icon: 'mdi mdi-crown',
+          color: '#EFB33D',
+        },
+        {
+          name: 'Write & Earn',
+          icon: 'mdi mdi-currency-usd',
+          color: '#EFB33D',
+        },
+        {
+          name: 'Refer & Earn',
+          icon: 'mdi mdi-account-multiple-plus',
+          color: '#00B4B4',
+        },
+        {
+          name: 'About Us',
+          icon: 'mdi mdi-information',
+          color: '#00B4B4',
+        },
+        {
+          name: 'Help & Support',
+          icon: 'mdi mdi-lifebuoy',
+          color: '#ABEDAB',
+        },
+        {
+          name: 'FAQ',
+          icon: 'mdi mdi-help-circle',
+          color: '#ABEDAB',
+        },
+      ],
+    }
+  },
   mounted() {
     this.$store.commit('BottomNavigation/update', { linkPosition: 4 })
+  },
+  methods: {
+    async logout() {
+      try {
+        await this.$store.dispatch('logout')
+        await this.$router.push('/')
+      } catch (e) {}
+    },
+  },
+  head() {
+    return {
+      title: 'More Options',
+      meta: [
+        {
+          // hid: 'description',
+          // name: 'description',
+          // content: 'My custom description',
+        },
+      ],
+    }
   },
 }
 </script>
