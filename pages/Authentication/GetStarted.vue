7b73d45869f415e85b78f312b933179e65e205f4
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-27 18:00:01 +0530

    Fixed Layout for GetStarted.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Authentication/GetStarted.vue
+++ pages/Authentication/GetStarted.vue
@@ -1,44 +1,84 @@
 <template>
-  <div class="content">
-    <h2 class="mt-4 mb-6 text-center">Let&rsquo;s Get Started</h2>
-
+  <div class="get-started-authentication-page">
+    <a
+      v-ripple
+      class="close-btn"
+      aria-label="close button"
+      @click="
+        () => {
+          this.$router.replace('/')
+        }
+      "
+    >
+      <span class="mdi mdi-close" />
+    </a>
+    <h3 class="my-6 text-center">Let&rsquo;s Get Started</h3>
+    <small class="text-center" style="display: block">
+      Get Started with the most advanced <br />
+      password less sign in
+    </small>
     <section class="signupOptionsGrid my-8">
-      <RippleButton
-        v-for="option in signupOptions"
-        :key="option.id"
-        class-list="secondary-outlined-btn"
-        :on-click="login"
-      >
-        <span>Continue with {{ option.provider }}</span>
-      </RippleButton>
+      <button v-ripple class="secondary-outlined-btn" @click="login">
+        Continue with
+        <GoogleIcon class="ml-1 icon" />
+      </button>
     </section>
-
-    <p class="muted-text text-center mt-6 mb-4">
-      Get Started with the most advanced password less sign in
-    </p>
-
-    <div class="text-center my-6">
-      <RippleButton
-        :on-click="
-          () => {
-            this.$router.replace('/')
-          }
-        "
-      >
-        Go Back
-      </RippleButton>
-    </div>
   </div>
 </template>
 
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.get-started-authentication-page {
+  display: block;
+  position: relative;
+  width: clamp(300px, 90vw, 650px);
+  background: darken($card-background, 5%);
+  border-radius: 10px;
+  padding: 2rem 1.5rem;
+  box-shadow: 0 0 4px lighten($card-background, 50%);
+
+  .close-btn {
+    position: absolute;
+    top: $standard-unit;
+    right: $large-unit;
+    font-size: 1.5rem;
+    text-decoration: none !important;
+    z-index: 1;
+  }
+
+  .signupOptionsGrid {
+    display: grid;
+    grid-auto-flow: row;
+    grid-template-columns: 1fr;
+    place-items: center;
+    grid-row-gap: 3vh;
+
+    button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 1.8 * $large-unit;
+      width: 250px;
+      font-size: 1rem;
+
+      & .icon {
+        height: 100%;
+        padding: $standard-unit - $nano-unit;
+      }
+    }
+  }
+}
+</style>
+
 <script>
-import RippleButton from '~/components/global/RippleButton'
+import GoogleIcon from '@/components/Icons/GoogleIcon'
 
 export default {
   name: 'GetStarted',
-  components: { RippleButton },
+  components: { GoogleIcon },
   layout: 'Authentication',
-  middleware: 'publicRoute',
+  // middleware: 'publicRoute',
 
   data() {
     return {
@@ -46,22 +86,7 @@ export default {
         {
           id: 0,
           provider: 'Google',
-          icon: 'mdi-email',
-        },
-        {
-          id: 1,
-          provider: 'Facebook',
-          icon: 'mdi-email',
-        },
-        {
-          id: 2,
-          provider: 'Twitter',
-          icon: 'mdi-email',
-        },
-        {
-          id: 3,
-          provider: 'LinkedIn',
-          icon: 'mdi-email',
+          icon: 'mdi mdi-google',
         },
       ],
     }
@@ -94,34 +119,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-@import 'assets/all-variables';
-
-.content {
-  display: block;
-  width: clamp(300px, 90vw, 650px);
-  background: darken($card-background, 5%);
-  border-radius: 10px;
-  padding: 1.5rem;
-  box-shadow: 0 0 4px lighten($card-background, 50%);
-
-  .signupOptionsGrid {
-    display: grid;
-    grid-auto-flow: row;
-    grid-template-columns: 1fr;
-    place-items: center;
-    grid-row-gap: 3vh;
-  }
-
-  .muted-text {
-    font-weight: 300;
-    text-align: center;
-  }
-
-  button {
-    width: 15.5rem;
-    height: 2.6rem;
-  }
-}
-</style>
