f78d9a05185bc5fb29eacc48bcffdb8294f8e321
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-27 21:54:04 +0530

    Updated  middlewares
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -1,6 +1,11 @@
 'use strict'
 
 export default function ({ store, redirect }) {
+  if (process.server) {
+    // console.log(localStorage.getItem('authUser'))
+  } else {
+    console.log('Not on Server')
+  }
   if (!store.state.authUser) {
     return redirect('/Authentication/GetStarted')
   }
--- middleware/publicRoute.js
+++ middleware/publicRoute.js
@@ -1,7 +1,9 @@
 'use strict'
 
 export default function ({ store, redirect }) {
+  // const localInstanceOfAuthUser = localStorage.getItem('authUser')
   if (store.state.authUser) {
+    // store.commit('SET_USER', JSON.parse(localInstanceOfAuthUser))
     return redirect('/Home/Dashboard')
   }
 }
