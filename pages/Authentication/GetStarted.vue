b5d489bb982263e85b091ffb9e28440b2d87952e
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-27 18:20:29 +0530

    Replacing GetStarted.vue with real Content
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Authentication/GetStarted.vue
+++ pages/Authentication/GetStarted.vue
@@ -1,30 +1,130 @@
 <template>
   <div class="get-started-authentication-page">
-    <a
-      v-ripple
-      class="close-btn"
-      aria-label="close button"
-      @click="
-        () => {
-          this.$router.replace('/')
-        }
-      "
-    >
-      <span class="mdi mdi-close" />
-    </a>
-    <h3 class="my-6 text-center">Let&rsquo;s Get Started</h3>
-    <small class="text-center" style="display: block">
-      Get Started with the most advanced <br />
-      password less sign in
-    </small>
-    <section class="signupOptionsGrid my-8">
-      <button v-ripple class="secondary-outlined-btn" @click="login">
-        Continue with
-        <GoogleIcon class="ml-1 icon" />
-      </button>
-    </section>
+    <div v-if="showLoaderAnimation">
+      <LoadingIcon />
+    </div>
+
+    <div v-else>
+      <a
+        v-ripple
+        class="close-btn"
+        aria-label="close button"
+        @click="
+          () => {
+            this.$router.replace('/')
+          }
+        "
+      >
+        <span class="mdi mdi-close" />
+      </a>
+
+      <h3 class="my-6 text-center">Let&rsquo;s Get Started</h3>
+      <small class="text-center" style="display: block">
+        Get Started with the most advanced <br />
+        password less sign in
+      </small>
+      <section class="signupOptionsGrid my-8">
+        <button v-ripple class="secondary-outlined-btn" @click="signInRedirect">
+          Continue with
+          <GoogleIcon class="ml-1 icon" />
+        </button>
+      </section>
+    </div>
   </div>
 </template>
+
+<script>
+import GoogleIcon from '@/components/Icons/GoogleIcon'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import LoadingIcon from '@/components/LoadingIcon'
+
+export default {
+  name: 'GetStarted',
+  components: { LoadingIcon, GoogleIcon },
+  layout: 'Authentication',
+  middleware: 'publicRoute',
+
+  data() {
+    return {
+      showLoaderAnimation: true,
+      signupOptions: [
+        {
+          id: 0,
+          provider: 'Google',
+          icon: 'mdi mdi-google',
+        },
+      ],
+    }
+  },
+
+  async mounted() {
+    const userExistenceResult = await this.checkForUserExistence()
+    if (userExistenceResult.user === null) {
+      this.hideLoader()
+    } else {
+      await this.$router.replace('/Home/Dashboard')
+    }
+  },
+
+  methods: {
+    async login() {
+      try {
+        await this.$store.dispatch('login', {
+          username: 'demo',
+          password: 'demo',
+        })
+        // console.log(this.$store.state.authUser)
+        await this.$router.push('/')
+      } catch (e) {
+        // console.log(`Error in logging in ${e}`)
+      }
+    },
+
+    showLoader() {
+      this.showLoaderAnimation = true
+    },
+
+    hideLoader() {
+      this.showLoaderAnimation = false
+    },
+
+    async checkForUserExistence() {
+      return await firebase
+        .auth()
+        .getRedirectResult()
+        .then(function (result) {
+          return result
+        })
+    },
+
+    async signInRedirect() {
+      this.showLoader()
+      const provider = new firebase.auth.GoogleAuthProvider()
+      await firebase
+        .auth()
+        .signInWithRedirect(provider)
+        .then(function (result) {
+          alert(result)
+          return result
+        })
+    },
+  },
+
+  head() {
+    return {
+      title: 'Sign in to continue',
+      meta: [
+        {
+          // hid: 'description',
+          // name: 'description',
+          // content: 'My custom description',
+        },
+      ],
+    }
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 @import 'assets/all-variables';
@@ -70,52 +170,3 @@
   }
 }
 </style>
-
-<script>
-import GoogleIcon from '@/components/Icons/GoogleIcon'
-
-export default {
-  name: 'GetStarted',
-  components: { GoogleIcon },
-  layout: 'Authentication',
-  // middleware: 'publicRoute',
-
-  data() {
-    return {
-      signupOptions: [
-        {
-          id: 0,
-          provider: 'Google',
-          icon: 'mdi mdi-google',
-        },
-      ],
-    }
-  },
-  methods: {
-    async login() {
-      try {
-        await this.$store.dispatch('login', {
-          username: 'demo',
-          password: 'demo',
-        })
-        // console.log(this.$store.state.authUser)
-        await this.$router.push('/')
-      } catch (e) {
-        // console.log(`Error in logging in ${e}`)
-      }
-    },
-  },
-  head() {
-    return {
-      title: 'Sign in to continue',
-      meta: [
-        {
-          // hid: 'description',
-          // name: 'description',
-          // content: 'My custom description',
-        },
-      ],
-    }
-  },
-}
-</script>
