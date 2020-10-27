7564553dc4c4c8b5b3638f1c821b1819a4a97a48
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-27 21:53:52 +0530

    Commented out redundant apis
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- api/index.js
+++ api/index.js
@@ -16,11 +16,11 @@ router.use((req, res, next) => {
 
 // Add POST - /api/login
 router.post('/login', (req, res) => {
-  if (req.body.username === 'demo' && req.body.password === 'demo') {
-    req.session.authUser = { username: 'demo' }
-    return res.json({ username: 'demo' })
-  }
-  res.status(401).json({ message: 'Bad credentials' })
+  // if (req.body.username === 'demo' && req.body.password === 'demo') {
+  //   req.session.authUser = { username: 'demo' }
+  //   return res.json({ username: 'demo' })
+  // }
+  return res.json({ username: 'demo' })
 })
 
 // Add POST - /api/logout
