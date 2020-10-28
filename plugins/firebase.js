c91f8ba31946ee9901a049822bbaa03966c302e9
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-28 23:34:17 +0530

    Added Custom firebase.js plugin
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ plugins/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBC8dH53PFPOWqN72FHSZtjM6ekF3gbEOM',
+  authDomain: 'floracasy-673ff.firebaseapp.com',
+  databaseURL: 'https://floracasy-673ff.firebaseio.com',
+  projectId: 'floracasy-673ff',
+  storageBucket: 'floracasy-673ff.appspot.com',
+  messagingSenderId: '804365562035',
+  appId: '1:804365562035:web:c45a8c8ff793dfe2ec00c6',
+  measurementId: 'G-RJ7XT8K378',
+}
+if (firebase.apps.length === 0) firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export default firebase
