1ed1c78bfcdd0033324be6deee9a70aebdfbd482
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-28 23:33:55 +0530

    Simplified Middleware logic for protectedRoute.js & publicRoute.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- middleware/protectedRoute.js
+++ middleware/protectedRoute.js
@@ -1,12 +1,7 @@
 'use strict'
 
 export default function ({ store, redirect }) {
-  if (process.server) {
-    // console.log(localStorage.getItem('authUser'))
-  } else {
-    console.log('Not on Server')
-  }
   if (!store.state.authUser) {
-    return redirect('/Authentication/GetStarted')
+    return redirect('/Authentication/SignInToContinue')
   }
 }
--- middleware/publicRoute.js
+++ middleware/publicRoute.js
@@ -1,9 +1,7 @@
 'use strict'
 
 export default function ({ store, redirect }) {
-  // const localInstanceOfAuthUser = localStorage.getItem('authUser')
   if (store.state.authUser) {
-    // store.commit('SET_USER', JSON.parse(localInstanceOfAuthUser))
     return redirect('/Home/Dashboard')
   }
 }
