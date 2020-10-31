b4901d2ee2b5ba9abb86d78879fa63604f8ff47a
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-10-31 12:56:50 +0530

    Added endpoints.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- api/endpoints.js
+++ api/endpoints.js
@@ -1,5 +1,38 @@
-// const root = "localhost:8000"
-// const endpoints = {
-//   get_blog_overview: `http://${root}/api/get_blog_overview?blogPerPage=3`,
-// }
-// export default endpoints
+const endpoints = {
+  auth: {
+    authenticate: '/auth/authenticate/',
+    logout: '/auth/logout',
+    generateToken: '/auth/token/generate/',
+    refreshToken: '/auth/token/refresh/',
+  },
+  blog: {
+    fetch: 'blog/fetch',
+    create: 'blog/create',
+    detail: 'blog/detail/',
+    update: 'blog/update/',
+    delete: 'blog/delete/',
+  },
+  categories: {
+    test: 'categories/',
+  },
+  comment_system: {
+    test: 'comment_system/',
+  },
+  help_and_support: {
+    fetch: 'help_and_support/fetch',
+    create: 'help_and_support/create',
+    detail: 'help_and_support/detail/',
+    update: 'help_and_support/update/',
+    delete: 'help_and_support/delete/',
+  },
+  leaderboard: {
+    test: 'leaderboard/',
+  },
+  profile_statistics: {
+    test: 'profile_statistics/',
+  },
+  reward_system: {
+    test: 'reward_system/',
+  },
+}
+export default endpoints
