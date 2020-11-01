d675a61324e625ce3a5c77e5b37d0d1ac59f3370
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-01 13:52:05 +0530

    Added Placeholder for firebase on auth changed state
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/firebase-authentication.js
+++ plugins/firebase-authentication.js
@@ -1,13 +1,5 @@
-// import { auth } from './firebase'
+import { auth } from './firebase'
 
-// export default (context, inject) => {
-//   inject('auth', auth)
-
-// auth.onAuthStateChanged((user) => {
-//   if (user) {
-//     console.info('have user', user.toJSON().displayName)
-//   } else {
-//     console.info('no user')
-//   }
-// })
-// }
+export default () => {
+  auth.onAuthStateChanged(() => {})
+}
