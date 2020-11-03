aa28c64b048e585b825e53ce2f0d8a0a604564e8
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-03 13:02:10 +0530

    Added a Sample Notification Drawer
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/Layout/PublicRoutes/NavigationBar.vue
+++ components/Layout/PublicRoutes/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="navigation-bar-page">
-    <h5 v-ripple class="mdi px-3 mdi-menu"></h5>
+    <h5 v-ripple class="mdi px-3 mdi-menu" @click="drawer = !drawer" />
     <section class="brand" @click="navigateTo('/')">
       <!--      <Logo :width="36" />-->
       <h6 v-ripple class="brand-name">Floracasy</h6>
@@ -14,28 +14,52 @@
       Get Started
     </button>
 
-    <!--    <div class="nav-drawer">-->
-    <!--      <ul>-->
-    <!--        <li>-->
-    <!--          <button-->
-    <!--            v-ripple-->
-    <!--            class="secondary-btn"-->
-    <!--            @click="$router.push('/PublicRoutes/GoPremium')"-->
-    <!--          >-->
-    <!--            Go Premium-->
-    <!--          </button>-->
-    <!--        </li>-->
-    <!--        <li>-->
-    <!--          <button-->
-    <!--            v-ripple-->
-    <!--            class="secondary-btn"-->
-    <!--            @click="$router.push('/PublicRoutes/WriteAndEarn')"-->
-    <!--          >-->
-    <!--            Write &amp; Earn-->
-    <!--          </button>-->
-    <!--        </li>-->
-    <!--      </ul>-->
-    <!--    </div>-->
+    <div
+      class="nav-drawer"
+      :style="
+        drawer
+          ? {
+              width: 0,
+              transform: 'translateX(-100%)',
+              overflow: 'hidden',
+            }
+          : {
+              width: '70vw',
+              transform: 'translateX(0)',
+              overflow: 'scroll',
+            }
+      "
+    >
+      <ul class="nav-drawer-options">
+        <li>
+          <button
+            v-ripple
+            class="secondary-outlined-btn"
+            @click="drawer = false"
+          >
+            &times;
+          </button>
+        </li>
+        <li>
+          <button
+            v-ripple
+            class="secondary-btn"
+            @click="$router.push('/PublicRoutes/GoPremium')"
+          >
+            Go Premium
+          </button>
+        </li>
+        <li>
+          <button
+            v-ripple
+            class="secondary-btn"
+            @click="$router.push('/PublicRoutes/WriteAndEarn')"
+          >
+            Write &amp; Earn
+          </button>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
@@ -45,7 +69,7 @@ export default {
   components: {},
   data() {
     return {
-      drawer: false,
+      drawer: true,
       menuOptions: [
         {
           title: 'Go Premium',
@@ -103,6 +127,24 @@ export default {
 
     li {
       font-family: $Nunito-Sans;
+    }
+  }
+
+  .nav-drawer {
+    position: absolute;
+    top: 2 * $xxx-large-unit;
+    left: 0;
+    min-height: 100vh;
+    background-color: $nav-bar-bg;
+    box-shadow: $right-only-box-shadow;
+    z-index: $bring-to-zero-level;
+    transition: all.2s ease-in-out;
+
+    .nav-drawer-options {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
     }
   }
 }
