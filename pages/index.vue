289554af8c6a77c82b5725018cc9e780c5916a83
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-03 10:40:35 +0530

    Removed Redundant Section
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="index-page">
-    <div class="text-center">
-      <h1>Here is the Style Guide</h1>
-      <nuxt-link to="/StyleGuide">
-        <RippleButton class-list="primary-btn">
-          View the Style Guide
-        </RippleButton>
-      </nuxt-link>
-    </div>
-
     <HeroContent />
 
     <CategoriesShowcase />
@@ -21,14 +12,12 @@
 import HeroContent from '~/components/LandingPage/HeroContent'
 import CategoriesShowcase from '~/components/LandingPage/CategoriesShowcase'
 import HearTheExperiences from '~/components/LandingPage/HearTheExperiences'
-import RippleButton from '~/components/global/RippleButton'
 
 export default {
   name: 'LandingPage',
   layout: 'PublicRoutes',
   middleware: 'publicRoute',
   components: {
-    RippleButton,
     HeroContent,
     CategoriesShowcase,
     HearTheExperiences,
