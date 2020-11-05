cd4d9b2cfa43e9e9f0297ce6d73991d13d67d907
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-05 23:07:55 +0530

    Added Help And Support PLaceholder
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/More/HelpAndSupport/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="help-and-support-page">
+    <h1>This is my Super duper Help and Support Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelpAndSupport',
+}
+</script>
