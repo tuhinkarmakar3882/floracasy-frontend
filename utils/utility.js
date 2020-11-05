bcca0c405e3b3c45ad3440e9b774606685226048
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-05 20:25:46 +0530

    Added new utility function for convertIntoMilliseconds
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- utils/utility.js
+++ utils/utility.js
@@ -2,7 +2,15 @@ const utility = {
   timeStringParser(timeString) {
     return new Date(timeString)
   },
-
+  convertIntoMilliseconds(duration) {
+    const years = duration?.years ? duration?.years : 0
+    const months = years * 12 + (duration?.months ? duration?.months : 0)
+    const days = months * 30 + (duration?.days ? duration?.days : 0)
+    const hours = days * 24 + (duration?.hours ? duration?.hours : 0)
+    const minutes = hours * 60 + (duration?.minutes ? duration?.minutes : 0)
+    const seconds = minutes * 60 + (duration?.seconds ? duration?.seconds : 0)
+    return seconds * 1000
+  },
   blogImageParser(blogToBeRendered) {
     const defaultImagePath = '/images/default.svg'
     if (blogToBeRendered.blog_image.length !== 0) {
