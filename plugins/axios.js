b317edb17b826b086a5dfc50b7b056257a8e65ba
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-05 20:44:13 +0530

    Added axios.js interceptor for handling the 401 error
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,26 @@
+const errorMessages = {
+  credentialsWereNotProvided: 'Authentication credentials were not provided.',
+  invalidTokens: 'Given token not valid for any token type',
+}
+
 export default function ({ $axios }) {
   $axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
   $axios.defaults.xsrfCookieName = 'csrftoken'
-  // $axios.onRequest((ctx) => {
-  //   console.log('Making Request with', ctx)
-  // })
+
+  $axios.onError(({ response: error }) => {
+    if (!error) {
+      console.log('Having Trouble to connect')
+    }
+    if (error.status === 401) {
+      if (error.data.detail === errorMessages.credentialsWereNotProvided) {
+        console.warn('No Credentials were Provided')
+        location.reload()
+      }
+      if (error.data.details == errorMessages.invalidTokens) {
+        console.warn('Credentials Expired.')
+        location.reload()
+      }
+    }
+    return Promise.reject(error)
+  })
 }
