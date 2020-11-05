aaf21e323f74c719a28642ec47b50692635fd2fa
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-05 23:07:43 +0530

    Added GoPRemium PLaceholder
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/More/GoPremium/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="go-premium-page">
+    <h1>This is my Super duper Go Premium Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GoPremium',
+}
+</script>
