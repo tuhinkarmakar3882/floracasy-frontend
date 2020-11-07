0d9a5cb292c26de8cce482c056aee9ce1cc96bc1
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-07 14:36:16 +0530

    Fix AppFeel.vue Styles
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ components/Layout/AppFeel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="app">
+    <header>
+      <h5 v-ripple class="mdi mdi-arrow-left" @click="$router.back()" />
+      <slot name="app-bar-title" />
+    </header>
+    <main>
+      <slot name="main"></slot>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppFeel',
+}
+</script>
+
+<style lang="scss">
+@import 'assets/all-variables';
+
+.app {
+  header {
+    //position: fixed;
+    top: 0;
+    left: 0;
+    z-index: $bring-to-front;
+    display: flex;
+    //justify-content: space-between;
+    align-items: center;
+    padding: 0 $medium-unit;
+    height: 2 * $x-large-unit;
+    background-color: $nav-bar-bg;
+    box-shadow: $down-only-box-shadow;
+
+    p {
+      font-size: 1.2rem;
+      font-weight: 400;
+      color: #dadada;
+    }
+  }
+
+  main {
+    //padding-top: $standard-unit;
+    background-color: $body-background;
+  }
+
+  button {
+    min-width: auto;
+    height: $xxx-large-unit;
+    width: 3.5 * $xx-large-unit;
+  }
+}
+</style>
