16432a93f60a9d868ae0ada4a088b545115f965c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-07 14:36:33 +0530

    Added Preferences Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/More/Preferences/index.vue
+++ pages/Home/More/Preferences/index.vue
@@ -1,34 +1,111 @@
 <template>
-  <div class="preferences-page">
-    <div class="navbar">
-      <a href="#"> Back</a>
-    </div>
+  <AppFeel class="preferences-page">
+    <template slot="app-bar-title">
+      <p class="ml-6">Preferences</p>
+    </template>
 
-    <div class="different options">
-      <div>
-        <div class="icon">create</div>
-        <a href="#option"> Edit profile </a>
-      </div>
-
-      <div>
-        <div class="icon">Delete filled</div>
-        <a href="#option"> Delete profile </a>
-      </div>
-
-      <label for="message">Message:</label>
-
-      <select id="message" name="message">
-        <option value="choose one">Choose one</option>
-        <option value="Only Followers">Only Followers</option>
-        <option value="Everyone">Everyone</option>
-        <option value="Only me">Only me</option>
-      </select>
-    </div>
-  </div>
+    <template slot="main" class="my-4">
+      <main class="my-4">
+        <ul>
+          <li
+            v-for="(option, index) in options"
+            :key="index"
+            v-ripple="`${option.color}5F`"
+            class="px-4 py-2"
+            @click="$router.push(option.route)"
+          >
+            <p>
+              <span
+                class="icon"
+                :class="option.icon"
+                :style="{ color: option.color }"
+              />
+              <span class="option-name">{{ option.name }}</span>
+              <span class="mdi mdi-chevron-right arrow-go" />
+            </p>
+          </li>
+          <li v-ripple="'#f774795F'" class="px-4 py-3">
+            <p>
+              <span class="icon mdi mdi-delete" style="color: #f77479" />
+              <span class="option-name">Delete Account</span>
+            </p>
+          </li>
+        </ul>
+      </main>
+    </template>
+  </AppFeel>
 </template>
 
 <script>
+import AppFeel from '@/components/Layout/AppFeel'
+
 export default {
   name: 'Preferences',
+  components: { AppFeel },
+  data() {
+    return {
+      options: [
+        {
+          name: 'Edit Profile',
+          icon: 'mdi mdi-pencil',
+          color: '#8FF2E1',
+          route: '/Home/More/Preferences',
+        },
+        {
+          name: 'Set Notifications',
+          icon: 'mdi mdi-bell',
+          color: '#5dd75d',
+          route: '/Home/More/Preferences',
+        },
+        {
+          name: 'Who can Message',
+          icon: 'mdi mdi-message',
+          color: '#ABEDAB',
+          route: '/Home/More/Preferences',
+        },
+        {
+          name: 'Saved Blogs',
+          icon: 'mdi mdi-bookmark',
+          color: '#18e5e5',
+          route: '/Home/More/Preferences',
+        },
+      ],
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+main {
+  ul {
+    list-style: none;
+
+    li {
+      p {
+        display: grid;
+        grid-template-columns: 1fr 4fr 0.5fr;
+        grid-column-gap: 1rem;
+        place-items: center;
+        text-align: left;
+
+        .icon {
+          font-size: clamp(1.6rem, 8vw, 2rem);
+        }
+
+        .option-name {
+          color: lighten($body-text-default, $lighten-percentage);
+          font-size: clamp(1.1rem, 8vw, 1.2rem);
+          width: 100%;
+        }
+
+        .arrow-go {
+          font-size: clamp(20px, 8vw, 28px);
+          color: #3a3a3a;
+        }
+      }
+    }
+  }
+}
+</style>
