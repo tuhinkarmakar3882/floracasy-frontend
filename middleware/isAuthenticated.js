d86879e52244805373466bdcaf64468ea9ccabf7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-08 15:53:27 +0530

    Improved MiddleWares
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,6 +1,9 @@
 'use strict'
 
+import clientSideAuthenticationStateInitializer from '@/plugins/authenticationStateInitializer'
+
 export default function ({ store, redirect }) {
+  clientSideAuthenticationStateInitializer({ store })
   if (!store.state.authUser) {
     return redirect('/Authentication/SignInToContinue')
   }
