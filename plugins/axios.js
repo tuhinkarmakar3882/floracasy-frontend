ce0b83d809e1617d9b97887a776b502993d47c7b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-08 22:54:44 +0530

    Removed Auto Refresh on Failure
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,15 +1,15 @@
-import handleAxiosRequestError from '@/utils/ErrorHandling'
+// import handleAxiosRequestError from '@/utils/ErrorHandling'
 
 export default function ({ $axios }) {
   $axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
   $axios.defaults.xsrfCookieName = 'csrftoken'
 
-  $axios.onError(({ response: error }) => {
-    if (!error) {
-      console.log('Having Trouble to connect')
-    }
-    handleAxiosRequestError(error)
-  })
+  // $axios.onError(({ response: error }) => {
+  //   if (!error) {
+  //     console.log('Having Trouble to connect')
+  //   }
+  //   handleAxiosRequestError(error)
+  // })
 
   $axios.onRequest((req) => {
     console.log(req.headers.common)
