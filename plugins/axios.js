fd857fffdb72853e83186586134c59768982611c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-09 13:04:56 +0530

    Added Interceptor For axios.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,17 +1,18 @@
-// import handleAxiosRequestError from '@/utils/ErrorHandling'
+import handleAxiosRequestError from '@/utils/ErrorHandling'
 
-export default function ({ $axios }) {
+export default function ({ $axios, $cookies }) {
   $axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
   $axios.defaults.xsrfCookieName = 'csrftoken'
+  $axios.setToken($cookies.get('access'), 'Bearer')
 
-  // $axios.onError(({ response: error }) => {
-  //   if (!error) {
-  //     console.log('Having Trouble to connect')
-  //   }
-  //   handleAxiosRequestError(error)
-  // })
+  $axios.onError(({ response: error }) => {
+    if (!error) {
+      console.log('Having Trouble to connect')
+    }
+    handleAxiosRequestError(error)
+  })
 
   $axios.onRequest((req) => {
-    console.log(req.headers.common)
+    console.log(req.headers)
   })
 }
