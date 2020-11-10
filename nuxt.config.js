4bf58612573020e9dd95ea9140be52d7a23d0c47
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-10 19:24:48 +0530

    Does it matter?
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -157,6 +157,7 @@ export default {
       background_color: packageJson.themeColor,
       theme_color: packageJson.themeColor,
       status_bar: packageJson.themeColor,
+      start_url: '/Home/Dashboard',
     },
     manifest: {
       name: packageJson.appName,
@@ -169,9 +170,6 @@ export default {
       display: 'standalone',
     },
     workbox: {
-      cacheOptions: {
-        revision: packageJson.version,
-      },
       runtimeCaching: [
         {
           urlPattern: 'https://fonts.googleapis.com/.*',
