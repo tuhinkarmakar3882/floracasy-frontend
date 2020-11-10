a233b05bf830138ad69aaac125abcf4b1a16d376
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-10 17:08:46 +0530

    Added selective-ssr.js
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- server/middleware/selective-ssr.js
+++ server/middleware/selective-ssr.js
@@ -1,5 +1,7 @@
+import { navigationRoutes } from '@/navigation/navigationRoutes'
+
 export default function (req, res, next) {
-  const paths = ['']
+  const paths = [navigationRoutes.Home.Blogs.Create.NewBlog]
   if (paths.includes(req.originalUrl)) {
     res.spa = true
   }
