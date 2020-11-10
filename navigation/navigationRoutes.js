741636662becfe3b0de32afad7ff7a37ebb16021
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-10 15:44:51 +0530

    Added All Frontend navigationRoutes.js
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ navigation/navigationRoutes.js
@@ -0,0 +1,50 @@
+export const navigationRoutes = {
+  index: '/',
+
+  Authentication: {
+    SignInToContinue: '/Authentication/SignInToContinue',
+  },
+
+  Home: {
+    Account: {
+      Details: '/Home/Account/Details',
+    },
+
+    Blogs: {
+      Create: {
+        index: '/Home/Blogs/Create',
+        NewBlog: '/Home/Blogs/NewBlog',
+        Drafts: '/Home/Blogs/Drafts',
+      },
+
+      CategoryWise: '/Home/Blogs/CategoryWise',
+      Details: '/Home/Blogs/Details/{id}',
+    },
+
+    Messages: {
+      index: '/Home/Messages',
+    },
+
+    MoreOptions: {
+      index: '/Home/MoreOptions',
+      About: '/Home/MoreOptions/About',
+      FAQ: '/Home/MoreOptions/FAQ',
+      GoPremium: '/Home/MoreOptions/GoPremium',
+      HelpAndSupport: '/Home/MoreOptions/HelpAndSupport',
+      Payments: '/Home/MoreOptions/Payments',
+      Preferences: '/Home/MoreOptions/Preferences',
+      WriteAndEarn: '/Home/MoreOptions/WriteAndEarn',
+    },
+
+    Notifications: {
+      index: '/Home/Notifications',
+    },
+
+    DashBoard: '/Home/DashBoard',
+  },
+
+  PublicRoutes: {
+    GoPremium: '/PublicRoutes/GoPremium',
+    WriteAndEarn: '/PublicRoutes/WriteAndEarn',
+  },
+}
