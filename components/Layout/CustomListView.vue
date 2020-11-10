0a71eb8a157803734948491d492da730d75cc151
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-10 16:25:03 +0530

    Added CustomListView.vue
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

---
+++ components/Layout/CustomListView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="custom-list-view">
+    <ul>
+      <slot name="list-items"></slot>
+      <slot name="last-item"></slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomListView',
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.custom-list-view {
+  ul {
+    list-style: none;
+
+    li {
+      p {
+        display: grid;
+        grid-template-columns: 1fr 4fr 0.5fr;
+        grid-column-gap: 1rem;
+        place-items: center;
+        text-align: left;
+
+        .icon {
+          font-size: clamp(1.6rem, 8vw, 2rem);
+        }
+
+        .option-name {
+          color: lighten($body-text-default, $lighten-percentage);
+          font-size: clamp(1.1rem, 8vw, 1.2rem);
+          width: 100%;
+        }
+
+        .arrow-go {
+          font-size: clamp(20px, 8vw, 28px);
+          color: #3a3a3a;
+        }
+      }
+    }
+  }
+}
+</style>
