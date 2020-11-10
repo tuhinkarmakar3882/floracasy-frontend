1cb267d0bb8b271664e479beb19e155dd2ea2d07
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-10 07:01:04 +0000

    Removed Native ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -195,7 +195,6 @@ export default {
       theme_color: '#0d0d17',
       author: 'Floracasy Team',
       appleStatusBarStyle: 'black',
-      nativeUI: true,
     },
     manifest: {
       name: 'Floracasy',
