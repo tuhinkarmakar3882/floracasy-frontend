d8d05d752e20a019dec0afe9030dfa1bfafe3454
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-11 16:50:37 +0530

    Fix
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- middleware/auto-redirect.js
+++ middleware/auto-redirect.js
@@ -2,5 +2,13 @@ export default function ({ route, redirect }) {
   const modifiedFullPath = route.fullPath.replace('standalone=true', '')
   console.log(modifiedFullPath, route.fullPath)
 
-  if (modifiedFullPath !== route.fullPath) redirect(modifiedFullPath)
+  if (modifiedFullPath !== route.fullPath) {
+    if (process.client) {
+      console.log('client...')
+      window.location = modifiedFullPath
+    } else {
+      console.log('server...')
+      redirect(modifiedFullPath)
+    }
+  }
 }
