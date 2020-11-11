a50adef19121063de4e9cb234dd4046f755437ff
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-11 16:32:22 +0530

    Fix Infinite loop
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- middleware/auto-redirect.js
+++ middleware/auto-redirect.js
@@ -1,6 +1,6 @@
 export default function ({ route, redirect }) {
   const modifiedFullPath = route.fullPath.replace('standalone=true', '')
-  process.client
-    ? (window.location = modifiedFullPath)
-    : redirect(modifiedFullPath)
+  console.log(modifiedFullPath, route.fullPath)
+
+  if (modifiedFullPath !== route.fullPath) redirect(modifiedFullPath)
 }
