bed455b992aeaa1e7da25de6f40174f82aca0ae7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-13 15:57:29 +0530

    Added Notification Screen
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- pages/Home/Notifications/index.vue
+++ pages/Home/Notifications/index.vue
@@ -1,187 +1,80 @@
 <template>
-  <div class="notification-page my-6 px-4">
-    <p class="text-right success">Mark all as read</p>
-    <section v-for="(notification, index) in notifications" :key="index">
-      <h1 :class="`mdi ${notification.icon}`" />
-      <p>{{ notification.content }} {{ index }}</p>
+  <div class="notification-page mb-6">
+    <section class="banner text-center mb-6">
+      <h6 class="mb-4">Would you like to receive notifications?</h6>
+      <small>
+        You can always change this setting later in the
+        <nuxt-link :to="navigationRoutes.Home.MoreOptions.Preferences"
+          >Preferences</nuxt-link
+        >
+      </small>
+      <div class="actions mt-6 mb-4">
+        <button class="secondary-btn mr-5">Yes</button>
+        <button class="danger-outlined-btn">May be later</button>
+      </div>
     </section>
+
+    <section v-if="isContentLoading">
+      <LoadingIcon />
+    </section>
+    <div v-else class="notifications px-4">
+      <section v-for="(notification, index) in notifications" :key="index">
+        <h1 :class="`mdi ${notification.notificationType.icon}`" />
+        <p>{{ notification.message }}</p>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import endpoints from '~/api/endpoints'
+import LoadingIcon from '~/components/LoadingIcon'
+import { navigationRoutes } from '~/navigation/navigationRoutes'
+
 export default {
   name: 'Notifications',
+  components: { LoadingIcon },
   layout: 'MobileApp',
   middleware: 'isAuthenticated',
 
   data() {
     return {
+      navigationRoutes,
       pageTitle: 'Notifications',
-
-      notifications: [
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 0,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-
-        {
-          id: 1,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 2,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-        {
-          id: 3,
-          icon: 'mdi mdi-bell-outline',
-          type: 'info',
-          content: 'This is a sample notification',
-          timestamp: Date.now(),
-        },
-      ],
+      isContentLoading: true,
+      notifications: null,
     }
   },
 
+  computed: {
+    ...mapGetters({
+      user: 'UserManagement/getUser',
+    }),
+  },
+
   async mounted() {
+    this.isContentLoading = true
     await this.$store.dispatch('BottomNavigation/update', {
       linkPosition: 3,
     })
+    const currentUser = await this.$store.getters['UserManagement/getUser']
+    if (!currentUser) {
+      await this.$store.dispatch('UserManagement/fetchData')
+    }
+    try {
+      this.notifications = await this.$axios
+        .$get(endpoints.notification_system.getNotificationsByUid, {
+          params: {
+            uid: this.user.uid,
+          },
+        })
+        .then(({ details }) => details)
+    } catch (e) {
+      this.notifications = []
+    }
+    console.log(this.notifications)
+    this.isContentLoading = false
   },
 
   head() {
@@ -193,12 +86,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import 'assets/all-variables';
+
 .notification-page {
-  section {
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    grid-column-gap: 1rem;
-    place-items: center;
+  .banner {
+    padding: $medium-unit $micro-unit;
+    background-color: $card-background;
+    box-shadow: $default-box-shadow;
+
+    .actions {
+      text-align: center;
+
+      button {
+        min-width: auto;
+
+        &:nth-child(1) {
+          width: 142px;
+        }
+      }
+    }
+  }
+
+  .notifications {
+    section {
+      display: grid;
+      grid-template-columns: 1fr 3fr;
+      grid-column-gap: 1rem;
+      place-items: center;
+    }
   }
 }
 </style>
