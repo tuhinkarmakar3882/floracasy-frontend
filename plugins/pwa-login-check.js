404fe8e4af75c6194196b6b01284060d3b01c7df
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-14 19:56:30 +0530

    Duh! OMg
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/pwa-login-check.js
+++ plugins/pwa-login-check.js
@@ -1,14 +1,15 @@
 import { navigationRoutes } from '@/navigation/navigationRoutes'
 
-export default async ({ store }) => {
+export default async ({ store, redirect }) => {
   if (process.client) {
     localStorage.getItem('is_auth')
       ? (window.location = navigationRoutes.Home.DashBoard)
-      : await forceLogout(store)
+      : await forceLogout(store, redirect)
   }
 }
 
-async function forceLogout(store) {
+async function forceLogout(store, redirect) {
   await store.dispatch('logout')
-  window.location = navigationRoutes.index
+  localStorage.removeItem('is_auth')
+  redirect(navigationRoutes.index)
 }
