19d44f7185d6d1816a7b8e4da39f4502ca969bd8
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-14 19:37:03 +0530

    Fix COnstant reload issue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/pwa-login-check.js
+++ plugins/pwa-login-check.js
@@ -1,10 +1,15 @@
 import { navigationRoutes } from '@/navigation/navigationRoutes'
 
 localStorage.setItem('is_auth', 'true')
-export default () => {
+export default async ({ store }) => {
   if (process.client) {
     localStorage.getItem('is_auth')
       ? (window.location = navigationRoutes.Home.DashBoard)
-      : (window.location = navigationRoutes.index)
+      : await forceLogout(store)
   }
+}
+
+async function forceLogout(store) {
+  await store.dispatch('logout')
+  window.location = navigationRoutes.index
 }
