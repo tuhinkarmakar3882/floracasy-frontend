107cc56824ef9611bc83d1e25c0edc43eee000ee
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-15 12:17:57 +0530

    Added Notification Channel Hook in axios.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,12 @@
 import handleAxiosRequestError from '@/utils/ErrorHandling'
 
-export default function ({ $axios, $cookies }) {
+export default async function ({ $axios, $cookies, store }) {
   $axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
   $axios.defaults.xsrfCookieName = 'csrftoken'
   $axios.setToken($cookies.get('access'), 'Bearer')
+
+  if (store.state.authState)
+    await store.dispatch('NotificationChannel/fetchNotificationChannelId')
 
   $axios.onError(({ response: error }) => {
     if (!error) {
