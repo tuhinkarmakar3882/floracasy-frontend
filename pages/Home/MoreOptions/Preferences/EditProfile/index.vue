633a2169e8342585b829d72302d4c197ef12e94f
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-17 20:06:12 +0530

    Added EditProfile Placeholder
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/MoreOptions/Preferences/EditProfile/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <AppFeel
+    class="preferences-page"
+    :on-back="navigationRoutes.Home.MoreOptions.Preferences.index"
+  >
+    <template slot="app-bar-title"> {{ pageTitle }}</template>
+
+    <template slot="main">
+      <h1>This is my super duper Edit Profile Page</h1>
+      <p>And I love it!</p>
+    </template>
+  </AppFeel>
+</template>
+
+<script>
+import AppFeel from '@/components/Layout/AppFeel'
+import { navigationRoutes } from '@/navigation/navigationRoutes'
+
+export default {
+  name: 'EditProfile',
+  components: { AppFeel },
+  data() {
+    return {
+      navigationRoutes,
+      pageTitle: 'Edit Profile',
+    }
+  },
+  head() {
+    return {
+      title: this.pageTitle,
+    }
+  },
+}
+</script>
