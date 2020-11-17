6bdaf3e5237b60c319751ac95bb7da98037ce0f7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-17 23:09:47 +0530

    Scaffolded Payments Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/Payments/index.vue
+++ pages/Home/MoreOptions/Payments/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="payments-page">
-    <h1>This is my Super duper Payments Page</h1>
-  </div>
+  <AppFeel
+    class="payments-page"
+    :on-back="navigationRoutes.Home.MoreOptions.index"
+  >
+    <template slot="app-bar-title"> {{ pageTitle }}</template>
+
+    <template slot="main">
+      <main class="px-6 my-8">
+        <h1>This is my Super duper Payments Page</h1>
+      </main>
+    </template>
+  </AppFeel>
 </template>
 
 <script>
+import AppFeel from '@/components/Layout/AppFeel'
+import { navigationRoutes } from '@/navigation/navigationRoutes'
+
 export default {
   name: 'Payments',
-
+  components: { AppFeel },
   data() {
     return {
+      navigationRoutes,
       pageTitle: 'Payments',
     }
   },
