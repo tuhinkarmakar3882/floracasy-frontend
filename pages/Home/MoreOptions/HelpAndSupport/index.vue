8ef267d0648a3b9b8e92f787961b236f809bfb55
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-17 23:09:34 +0530

    Scaffolded Help and Support Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/HelpAndSupport/index.vue
+++ pages/Home/MoreOptions/HelpAndSupport/index.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="help-and-support-page">
-    <h1>This is my Super duper Help and Support Page</h1>
-  </div>
+  <AppFeel
+    class="help-and-support-page"
+    :on-back="navigationRoutes.Home.MoreOptions.index"
+  >
+    <template slot="app-bar-title"> {{ pageTitle }}</template>
+
+    <template slot="main">
+      <main class="px-6 my-8">
+        <h1>This is my Super duper Help and Support Page</h1>
+      </main>
+    </template>
+  </AppFeel>
 </template>
 
 <script>
+import AppFeel from '@/components/Layout/AppFeel'
+import { navigationRoutes } from '@/navigation/navigationRoutes'
+
 export default {
   name: 'HelpAndSupport',
+  components: { AppFeel },
   data() {
     return {
+      navigationRoutes,
       pageTitle: 'Help And Support',
     }
   },
