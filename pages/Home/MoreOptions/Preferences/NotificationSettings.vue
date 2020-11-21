93856f1b7be0b2b8971ef2b531e9ce40e1c29c00
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-21 17:35:34 +0530

    Added Switch Button
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/Preferences/NotificationSettings.vue
+++ pages/Home/MoreOptions/Preferences/NotificationSettings.vue
@@ -5,7 +5,21 @@
   >
     <template slot="app-bar-title"> {{ pageTitle }}</template>
 
-    <template slot="main"></template>
+    <template slot="main">
+      <main>
+        <section class="switch-container" @click="switchState = !switchState">
+          <div
+            class="switch-button"
+            :style="[
+              {
+                transform: switchState ? 'translateX(40px)' : 'translateX(4px)',
+              },
+              { backgroundColor: switchState ? '#65db65' : '#ff8282' },
+            ]"
+          />
+        </section>
+      </main>
+    </template>
   </AppFeel>
 </template>
 
@@ -20,6 +34,7 @@ export default {
     return {
       navigationRoutes,
       pageTitle: 'Notification Settings',
+      switchState: false,
     }
   },
   head() {
@@ -29,3 +44,33 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.notification-settings-page {
+  main {
+    padding: 1rem;
+    $size: 36px;
+
+    .switch-container {
+      position: relative;
+      height: $size;
+      width: 2 * $size;
+      border-radius: 50px;
+      background-color: #33404e;
+      box-shadow: $default-box-shadow;
+
+      .switch-button {
+        position: absolute;
+        top: $nano-unit;
+        height: $size - $micro-unit;
+        width: $size - $micro-unit;
+        border-radius: 50px;
+        box-shadow: $default-box-shadow;
+        transition: all 250ms ease-in-out;
+      }
+    }
+  }
+}
+</style>
