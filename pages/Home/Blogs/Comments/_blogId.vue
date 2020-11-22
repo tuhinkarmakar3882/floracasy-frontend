3f091da5771dd5cf3cf42bc65d5297bc3588e94a
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-22 14:41:55 +0530

    [WIP] Setting up infinite loading for blog comments
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Blogs/Comments/_blogId.vue
+++ pages/Home/Blogs/Comments/_blogId.vue
@@ -7,28 +7,79 @@
         <h5>This is my super duper comments page & And I Love It!</h5>
         <pre class="my-4"> Requested Blog: {{ $route.params }} </pre>
       </main>
+
+      <client-only>
+        <infinite-loading @infinite="infiniteHandler">
+          <template slot="spinner">
+            <LoadingIcon class="mt-4 mb-6" />
+            <p>Loading Recent Activities Data...</p>
+          </template>
+          <template slot="error">
+            <p class="danger-light my-6">Network Error</p>
+          </template>
+          <template slot="no-more" />
+          <template slot="no-results">
+            <div class="no-activity">
+              <p class="my-5">Be the first to comment on this!</p>
+            </div>
+          </template>
+        </infinite-loading>
+      </client-only>
     </template>
   </AppFeel>
 </template>
 
 <script>
 import { navigationRoutes } from '@/navigation/navigationRoutes'
-// import endpoints from '@/api/endpoints'
+import endpoints from '@/api/endpoints'
 import AppFeel from '@/components/Layout/AppFeel'
+import ClientOnly from 'vue-client-only'
+import LoadingIcon from '@/components/LoadingIcon'
 
 export default {
-  name: 'CommentSection',
-  components: { AppFeel },
+  components: { LoadingIcon, AppFeel, ClientOnly },
   data() {
     return {
       navigationRoutes,
       pageTitle: 'Comments',
+      comments: [],
+      page: 1,
     }
   },
 
-  mounted() {},
+  async mounted() {
+    await this.$store.dispatch('BottomNavigation/update', { linkPosition: -1 })
+  },
 
-  methods: {},
+  methods: {
+    async setupUser() {
+      const currentUser = await this.$store.getters['UserManagement/getUser']
+      if (!currentUser) {
+        this.loadingProfile = true
+        await this.$store.dispatch('UserManagement/fetchData')
+      }
+    },
+
+    async infiniteHandler($state) {
+      await this.setupUser()
+      try {
+        const { data: results } = await this.$axios.get(
+          endpoints.comment_system.fetchByBlogId,
+          { params: { page: this.page, blog_id: this.$route.params.blogId } }
+        )
+        console.log(results)
+        if (results.length) {
+          this.page += 1
+          this.comments.push(...results)
+          $state.loaded()
+        } else {
+          $state.complete()
+        }
+      } catch (e) {
+        $state.complete()
+      }
+    },
+  },
 
   head() {
     return {
