929bf41282d1e6ec2cfff678ff8ac7bbd9724d7b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-24 18:04:05 +0530

    Added Dummy Page for payment
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/Payments/index.vue
+++ pages/Home/MoreOptions/Payments/index.vue
@@ -7,7 +7,40 @@
 
     <template slot="main">
       <main class="px-6 my-8">
-        <h1>This is my Super duper Payments Page</h1>
+        <h3>Your total earning</h3>
+
+        <h5>$0.00</h5>
+
+        <ul>
+          <li>You can earn unlimited money per month</li>
+
+          <li>
+            You can claim your money only when you have already earned $ 100
+          </li>
+
+          <li>
+            If you have done any kind of bad thing which is against of our
+            rule(like opened floracasy repeatedly by a single user, use any
+            network bouncing app etc. for more details you can check out our
+            earning policy page) ,we will cut money from your earnings .
+          </li>
+        </ul>
+
+        <h4>
+          This section will be replaced by graph. ami onk kuja6i but ki6u bujta
+          prini kisb code 6ilo.
+        </h4>
+
+        <h3>Your blogs total reach</h3>
+        <h3>300</h3>
+
+        <h3>Your profiles total engagement</h3>
+
+        <h3>4k</h3>
+
+        <h6>To show all blogs insight view tap the below button</h6>
+
+        <button>Insight View</button>
       </main>
     </template>
   </AppFeel>
