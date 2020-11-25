f87faa083b671c48e99faf1279dd8d8935b9c61c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-11-26 00:06:06 +0530

    Added Placeholder for SavedBlogs.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/MoreOptions/Preferences/SavedBlogs.vue
@@ -0,0 +1,33 @@
+<template>
+  <AppFeel
+    class="saved-blogs-page"
+    :on-back="navigationRoutes.Home.MoreOptions.Preferences.index"
+  >
+    <template slot="app-bar-title"> {{ pageTitle }}</template>
+    <template slot="main">
+      <h4 class="heading-title">Saved Blogs</h4>
+    </template>
+  </AppFeel>
+</template>
+
+<script>
+import AppFeel from '@/components/Layout/AppFeel'
+import { navigationRoutes } from '@/navigation/navigationRoutes'
+
+export default {
+  name: 'SavedBlogs',
+  components: { AppFeel },
+  middleware: 'isAuthenticated',
+  data() {
+    return {
+      navigationRoutes,
+      pageTitle: 'Saved Blogs',
+    }
+  },
+  head() {
+    return {
+      title: this.pageTitle,
+    }
+  },
+}
+</script>
