6d22b23cac99d94f2c3b0905e39f0aa205d07839
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-01 11:24:21 +0530

    Added Saved Blogs
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/Preferences/SavedBlogs.vue
+++ pages/Home/MoreOptions/Preferences/SavedBlogs.vue
@@ -6,6 +6,28 @@
     <template slot="app-bar-title"> {{ pageTitle }}</template>
     <template slot="main">
       <h4 class="heading-title">Saved Blogs</h4>
+
+      <section v-if="blogs">
+        <article v-for="{ blog } in blogs" :key="blog.id">
+          <BlogPost :blog="blog" />
+        </article>
+      </section>
+
+      <client-only>
+        <infinite-loading @infinite="infiniteHandler">
+          <template slot="spinner">
+            <LoadingIcon class="mt-4 mb-6" />
+            <p class="text-center">Getting Latest Articles...</p>
+          </template>
+          <template slot="error">
+            <p class="danger-light my-6">Network Error</p>
+          </template>
+          <template slot="no-more">
+            <p class="success my-6">That's all for now :)</p>
+          </template>
+          <!--        <template slot="no-results">No results message</template>-->
+        </infinite-loading>
+      </client-only>
     </template>
   </AppFeel>
 </template>
@@ -13,17 +35,44 @@
 <script>
 import AppFeel from '@/components/Layout/AppFeel'
 import { navigationRoutes } from '@/navigation/navigationRoutes'
+import BlogPost from '@/components/common/BlogPost'
+import LoadingIcon from '@/components/LoadingIcon'
+import endpoints from '@/api/endpoints'
 
 export default {
   name: 'SavedBlogs',
-  components: { AppFeel },
+  components: { LoadingIcon, BlogPost, AppFeel },
   middleware: 'isAuthenticated',
   data() {
     return {
       navigationRoutes,
+      blogs: [],
+      savedBlogFetchEndpoint: endpoints.blog.getSavedBlogs,
       pageTitle: 'Saved Blogs',
     }
   },
+
+  methods: {
+    async infiniteHandler($state) {
+      try {
+        const { results, next } = await this.$axios.$get(
+          this.savedBlogFetchEndpoint,
+          { params: { category_name: this.category } }
+        )
+        console.log(results)
+        if (results.length) {
+          this.savedBlogFetchEndpoint = next
+          this.blogs.push(...results)
+          $state.loaded()
+        } else {
+          $state.complete()
+        }
+      } catch (e) {
+        $state.complete()
+      }
+    },
+  },
+
   head() {
     return {
       title: this.pageTitle,
