6c802d9e9ff675d1197bc35c80f0d231324c3955
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-01 22:12:10 +0530

    Bootstrapped Popular Topic
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/HelpAndSupport/PopularTopics/index.vue
+++ pages/Home/MoreOptions/HelpAndSupport/PopularTopics/index.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>This is my super Duper PopularTopics Page</h1>
+  <AppFeel
+    class="help-and-support-page"
+    :on-back="navigationRoutes.Home.MoreOptions.HelpAndSupport.index"
+  >
+    <template slot="app-bar-title"> {{ pageTitle }}</template>
+
+    <template slot="main">
+      <h6>This is my super duper Popular Topics Page</h6>
+      <p>And I Love it</p>
+    </template>
+  </AppFeel>
 </template>
 
 <script>
+import AppFeel from '@/components/Layout/AppFeel'
+import { navigationRoutes } from '@/navigation/navigationRoutes'
 export default {
   name: 'PopularTopics',
+  components: { AppFeel },
+  middleware: 'isAuthenticated',
+  data() {
+    return {
+      navigationRoutes,
+      pageTitle: 'Popular Topics',
+    }
+  },
+
+  head() {
+    return {
+      title: this.pageTitle,
+    }
+  },
 }
 </script>
