4da292499079ab8bdf386fcefa0a65fd6f3c8006
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-03 11:53:50 +0530

    Replacing QuilEditor
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Blogs/Create/New.vue
+++ pages/Home/Blogs/Create/New.vue
@@ -57,6 +57,7 @@
         </button>
       </div>
     </template>
+
     <template slot="main">
       <div v-if="step === 1" class="steps px-4 mt-4">
         <h5 class="heading-title mb-8">What is it about?</h5>
@@ -144,21 +145,10 @@
         </div>
       </div>
 
-      <div v-else-if="step === 3" class="steps mt-4">
+      <div v-else-if="step === 3">
         <h5 class="heading-title mb-8">Write the Blog below</h5>
         <client-only>
-          <div class="blog-body">
-            <quill-editor
-              ref="editor"
-              v-model="content"
-              :options="editorOption"
-              style="
-                position: sticky !important;
-                top: 56px !important;
-                z-index: 1234567890 !important;
-              "
-            />
-          </div>
+          <RichTextEditor />
         </client-only>
       </div>
 
@@ -198,62 +188,13 @@
           </p>
         </section>
         <div class="blog-body">
-          <article class="ql-snow">
-            <div class="ql-editor" v-html="noXSS(content)" />
-          </article>
+          <!--          <editor-content-->
+          <!--            class="editor__content"-->
+          <!--            :editor="editor"-->
+          <!--            :contenteditable="false"-->
+          <!--          />-->
         </div>
       </div>
-      <!--      <section-->
-      <!--        :key="'fixed'"-->
-      <!--        class="py-5"-->
-      <!--        :class="step === 4 ? 'bottom-section-floating' : 'bottom-section'"-->
-      <!--      >-->
-      <!--        <div class="text-center">-->
-      <!--          <button-->
-      <!--            v-if="step >= 2"-->
-      <!--            v-ripple=""-->
-      <!--            class="secondary-outlined-btn mr-4 px-8"-->
-      <!--            @click="goToPrevStep"-->
-      <!--          >-->
-      <!--            Back-->
-      <!--          </button>-->
-
-      <!--          <button-->
-      <!--            v-if="step <= 2"-->
-      <!--            v-ripple=""-->
-      <!--            class="secondary-btn px-8"-->
-      <!--            @click="goToNextStep"-->
-      <!--          >-->
-      <!--            Next-->
-      <!--          </button>-->
-
-      <!--          <button-->
-      <!--            v-if="step === 3"-->
-      <!--            v-ripple=""-->
-      <!--            class="secondary-btn px-7"-->
-      <!--            @click="goToNextStep"-->
-      <!--          >-->
-      <!--            Preview-->
-      <!--          </button>-->
-
-      <!--          <button-->
-      <!--            v-if="step === 4"-->
-      <!--            v-ripple=""-->
-      <!--            class="secondary-btn px-7"-->
-      <!--            @click="publish"-->
-      <!--          >-->
-      <!--            Publish-->
-      <!--          </button>-->
-      <!--        </div>-->
-
-      <!--        <section class="progress-circle mt-8">-->
-      <!--          <span-->
-      <!--            v-for="(stepNo, index) in totalSteps"-->
-      <!--            :key="index"-->
-      <!--            :class="stepNo === step && 'active'"-->
-      <!--          />-->
-      <!--        </section>-->
-      <!--      </section>-->
     </template>
   </AppFeel>
 </template>
@@ -265,10 +206,9 @@ import endpoints from '@/api/endpoints'
 import sanitizeHtml from 'sanitize-html'
 import { parseTimeUsingMoment } from '@/utils/utility'
 import { mapGetters } from 'vuex'
-import 'quill/dist/quill.core.css'
-import 'quill/dist/quill.snow.css'
-import 'highlight.js/styles/tomorrow.css'
-import hljs from 'highlight.js'
+// import 'highlight.js/styles/tomorrow.css'
+// import hljs from 'highlight.js'
+import RichTextEditor from '~/components/common/RichTextEditor'
 
 function getFromLocalStorageOrReturnDefault(keyName, value) {
   return process.client && localStorage.getItem(keyName)
@@ -280,6 +220,7 @@ export default {
   name: 'CreateNewBlog',
   middleware: 'isAuthenticated',
   components: {
+    RichTextEditor,
     AppFeel,
   },
 
@@ -293,29 +234,6 @@ export default {
   data() {
     return {
       prevURL: null,
-      editorOption: {
-        modules: {
-          toolbar: [
-            ['bold', 'italic', 'underline', 'strike'],
-            ['blockquote', 'code-block'],
-            [{ header: 1 }, { header: 2 }],
-            [{ list: 'ordered' }, { list: 'bullet' }],
-            [{ script: 'sub' }, { script: 'super' }],
-            [{ indent: '-1' }, { indent: '+1' }],
-            [{ direction: 'rtl' }],
-            [{ size: ['small', false, 'large', 'huge'] }],
-            [{ header: [1, 2, 3, 4, 5, 6, false] }],
-            [{ font: [] }],
-            [{ color: [] }, { background: [] }],
-            [{ align: [] }],
-            ['clean'],
-            ['link', 'image', 'video'],
-          ],
-          syntax: {
-            highlight: (text) => hljs.highlightAuto(text).value,
-          },
-        },
-      },
       navigationRoutes,
       noXSS: sanitizeHtml,
       step: 1,
@@ -514,44 +432,6 @@ export default {
       h6 {
         position: relative;
         margin: $large-unit 0;
-      }
-
-      .quill-editor {
-        .ql-toolbar.ql-snow {
-          background-color: wheat !important;
-          box-shadow: $default-box-shadow !important;
-        }
-
-        .ql-container.ql-snow {
-          padding: 0 !important;
-        }
-
-        .ql-toolbar.ql-snow,
-        .ql-container.ql-snow {
-          border: none !important;
-        }
-      }
-    }
-  }
-
-  .progress-circle {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-
-    span {
-      height: $micro-unit;
-      width: $micro-unit;
-      background-color: darken($disabled, $lighten-percentage);
-      border-radius: 50%;
-      margin: 0 $micro-unit;
-      box-shadow: $default-box-shadow;
-
-      &.active {
-        height: $micro-unit + $double-unit;
-        width: $micro-unit + $double-unit;
-        background-color: $secondary-highlight;
       }
     }
   }
