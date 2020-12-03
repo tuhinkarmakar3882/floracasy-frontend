98210d6a64a82de7cee71c503c29652555b7e73b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-03 16:29:49 +0530

    Fixed nuxt.config.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,10 @@
 import * as secrets from './environmentalVariables'
-// import * as packageJson from './package.json'
 
 export default {
   ssr: true,
   components: true,
 
   server: {
-    // host: '0.0.0.0',
     port: 3001,
   },
   serverMiddleware: ['~/api', '~/server/middleware/selective-ssr.js'],
@@ -42,8 +40,6 @@ export default {
   modules: [
     '~/module/csp.js',
     '@nuxtjs/axios',
-    // '@nuxtjs/pwa',
-    // '@nuxt/content',
     'cookie-universal-nuxt',
     [
       'nuxt-lazy-load',
@@ -55,37 +51,13 @@ export default {
         native: false,
         polyfill: true,
         directiveOnly: false,
-
         defaultImage: '/images/default.svg',
-
-        // To remove class set value to false
         loadingClass: 'isLoading',
         loadedClass: 'isLoaded',
         appendClass: 'lazyLoad',
-
-        observerConfig: {
-          // See IntersectionObserver documentation
-        },
+        observerConfig: {},
       },
     ],
-
-    //  // The Following should be used only while building an TWA app
-    // [
-    //   'nuxt-twa-module',
-    //   {
-    //     /* module options */
-    //     defaultUrl: 'https://dev.floracasy.nubes.live',
-    //     hostName: 'dev.floracasy.nubes.live',
-    //     applicationId: 'com.floracasy.dev',
-    //     launcherName: 'Floracasy',
-    //     versionCode: 1,
-    //     versionName: '1.0',
-    //     statusBarColor: packageJson.themeColor,
-    //     sha256Fingerprints: [''],
-    //     iconPath: '/static/icon.png',
-    //     distFolder: '.nuxt/dist/client',
-    //   },
-    // ],
   ],
 
   buildModules: [
@@ -98,7 +70,6 @@ export default {
     extractCSS: {
       ignoreOrder: true,
     },
-    optimizeCSS: true,
     splitChunks: {
       layouts: true,
       pages: true,
@@ -222,7 +193,6 @@ export default {
           'pages/**/*.vue',
           'plugins/**/*.js',
           'nuxt.config.js',
-          // TypeScript
           'plugins/**/*.ts',
           'nuxt.config.ts',
         ],
@@ -230,105 +200,7 @@ export default {
     },
   },
 
-  // pwa: {
-  //   meta: {
-  //     name: packageJson.appName,
-  //     author: 'Floracasy Team',
-  //     appleStatusBarStyle: 'black',
-  //     nativeUI: true,
-  //     background_color: packageJson.themeColor,
-  //     theme_color: packageJson.themeColor,
-  //     status_bar: packageJson.themeColor,
-  //   },
-  //   manifest: {
-  //     name: packageJson.appName,
-  //     description: packageJson.description,
-  //     short_name: 'Floracasy',
-  //     lang: 'en-US',
-  //     background_color: packageJson.themeColor,
-  //     theme_color: packageJson.themeColor,
-  //     status_bar: packageJson.themeColor,
-  //     display: 'standalone',
-  //     // start_url: '',
-  //   },
-  //   // workbox: false,
-  //   workbox: {
-  //     // workboxVersion: require('workbox-cdn/package.json').version,
-  //     // workboxURL: undefined,
-  //     // importScripts: [],
-  //     enabled: true,
-  //     autoRegister: true,
-  //     // dev: undefined,
-  //     // Config
-  //     config: {
-  //       debug: true,
-  //     },
-  //     clientsClaim: true,
-  //     skipWaiting: true,
-  //     offlineAnalytics: false,
-  //     workboxExtensions: [],
-  //
-  //     // Precache
-  //     preCaching: [],
-  //     cacheOptions: {
-  //       cacheId: 'floracasy' + Date.now().toString(),
-  //       directoryIndex: '/',
-  //       revision: Date.now(),
-  //     },
-  //     cachingExtensions: [],
-  //     cleanupOutdatedCaches: true,
-  //
-  //     // Offline
-  //     offline: false,
-  //     offlineStrategy: 'NetworkOnly',
-  //     offlinePage: null,
-  //     offlineAssets: [],
-  //
-  //     // Runtime Caching
-  //     runtimeCaching: [
-  //       {
-  //         urlPattern: 'https://fonts.googleapis.com/.*',
-  //         handler: 'cacheFirst',
-  //         method: 'GET',
-  //         strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-  //       },
-  //       {
-  //         urlPattern: 'https://cdn.materialdesignicons.com/.*',
-  //         handler: 'cacheFirst',
-  //         method: 'GET',
-  //         strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-  //       },
-  //       {
-  //         urlPattern: 'https://fonts.gstatic.com/.*',
-  //         handler: 'cacheFirst',
-  //         method: 'GET',
-  //         strategyOptions: { cacheableResponse: { statuses: [0, 200] } },
-  //       },
-  //     ],
-  //     routingExtensions: [],
-  //     cacheAssets: true,
-  //     // assetsURLPattern: undefined,
-  //     // pagesURLPattern: undefined,
-  //
-  //     // Sw
-  //     // swTemplate: undefined,
-  //     // swUrl: 'service-worker.js',
-  //     // swScope: '/',
-  //     // swDest: undefined,
-  //
-  //     // Router
-  //     // routerBase: undefined,
-  //     // publicPath: undefined
-  //   },
-  // },
-
   telemetry: false,
 
-  // vue: {
-  //   config: {
-  //     productionTip: true,
-  //     devtools: true,
-  //   },
-  // },
   watch: ['~/module/csp.js'],
 }
