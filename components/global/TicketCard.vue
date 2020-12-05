dff97285f05d116ce8aa8b737c813c43c8be6dbe
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-05 20:45:43 +0530

    Added TicketCard.vue Styling
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/TicketCard.vue
+++ components/global/TicketCard.vue
@@ -4,21 +4,24 @@
     class="px-4 py-3 my-8"
     :style="{ borderLeftColor: ticket.color }"
   >
-    <p class="name">{{ ticket.name }}</p>
+    <h6 class="name">{{ ticket.name }}</h6>
     <p class="body">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus ad
       dolorem dolores, eius id ipsam iure magnam modi pariatur possimus qui quis
       quo recusandae reiciendis repellendus rerum sed voluptatum?
     </p>
-    <small><i class="mdi mdi-home" />3d</small>
-    <Chip :background-color="ticket.color">
-      <template slot="content">{{ ticket.status }}</template>
-    </Chip>
+    <section class="bottom">
+      <small> <i class="mdi mdi-clock-time-nine-outline" />3 days ago </small>
+      <Chip :background-color="ticket.color">
+        <template slot="content">{{ ticket.status }}</template>
+      </Chip>
+    </section>
   </blockquote>
 </template>
 
 <script>
 import Chip from '@/components/global/Chip'
+
 export default {
   name: 'TicketCard',
   components: { Chip },
@@ -35,20 +38,33 @@ export default {
 @import 'assets/all-variables';
 
 blockquote {
-  display: grid;
-  grid-template-columns: 0.5fr 4fr 0.5fr;
-  grid-column-gap: 1rem;
-  align-items: center;
   text-align: left;
 
   span {
     font-size: clamp(1.6rem, 8vw, 2rem);
   }
 
-  p {
-    color: lighten($body-text-default, $lighten-percentage);
+  .name {
+    color: #dadada;
     font-size: 1rem;
     width: 100%;
+  }
+
+  .body {
+    color: #959595;
+    font-size: 0.9rem;
+    width: 100%;
+  }
+
+  .bottom {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    small {
+      display: grid;
+      place-items: center;
+    }
   }
 
   i {
