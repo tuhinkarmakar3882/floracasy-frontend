e642a31a68024a25259282df14b49c9252c51f2b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-05 20:26:27 +0530

    Refactored Code into TicketCard.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/global/TicketCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <blockquote
+    v-ripple="`${ticket.color}5F`"
+    class="px-4 py-3 my-8"
+    :style="{ borderLeftColor: ticket.color }"
+  >
+    <span :class="ticket.icon" class="icon" :style="{ color: ticket.color }" />
+    <p class="ticket-name">{{ ticket.name }}</p>
+    <i class="mdi mdi-chevron-right arrow-go" />
+  </blockquote>
+</template>
+
+<script>
+export default {
+  name: 'TicketCard',
+  props: {
+    ticket: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+blockquote {
+  display: grid;
+  grid-template-columns: 0.5fr 4fr 0.5fr;
+  grid-column-gap: 1rem;
+  align-items: center;
+  text-align: left;
+
+  span {
+    font-size: clamp(1.6rem, 8vw, 2rem);
+  }
+
+  p {
+    color: lighten($body-text-default, $lighten-percentage);
+    font-size: 1rem;
+    width: 100%;
+  }
+
+  i {
+    font-size: clamp(20px, 8vw, 28px);
+    color: #3a3a3a;
+  }
+}
+</style>
--- pages/Home/MoreOptions/HelpAndSupport/Tickets/index.vue
+++ pages/Home/MoreOptions/HelpAndSupport/Tickets/index.vue
@@ -6,66 +6,24 @@
     <template slot="app-bar-title"> {{ pageTitle }}</template>
 
     <template slot="main">
-      <CustomListView class="px-4">
-        <template slot="list-items">
-          <blockquote
-            v-for="(ticket, index) in tickets"
-            :key="index"
-            v-ripple="`${ticket.color}5F`"
-            class="px-4 py-3 my-8"
-            :style="{ borderLeftColor: ticket.color }"
-          >
-            <span
-              :class="ticket.icon"
-              class="icon"
-              :style="{ color: ticket.color }"
-            />
-            <p class="ticket-name">{{ ticket.name }}</p>
-            <i class="mdi mdi-chevron-right arrow-go" />
-          </blockquote>
-        </template>
-      </CustomListView>
+      <TicketCard
+        v-for="(ticket, index) in tickets"
+        :key="index"
+        class="mx-4"
+        :ticket="ticket"
+      />
     </template>
   </AppFeel>
 </template>
 
-<style lang="scss" scoped>
-@import 'assets/all-variables';
-
-.tickets-index-page {
-  blockquote {
-    display: grid;
-    grid-template-columns: 0.5fr 4fr 0.5fr;
-    grid-column-gap: 1rem;
-    align-items: center;
-    text-align: left;
-
-    span {
-      font-size: clamp(1.6rem, 8vw, 2rem);
-    }
-
-    p {
-      color: lighten($body-text-default, $lighten-percentage);
-      font-size: 1rem;
-      width: 100%;
-    }
-
-    i {
-      font-size: clamp(20px, 8vw, 28px);
-      color: #3a3a3a;
-    }
-  }
-}
-</style>
-
 <script>
 import AppFeel from '@/components/global/Layout/AppFeel'
 import { navigationRoutes } from '@/navigation/navigationRoutes'
-import CustomListView from '~/components/global/Layout/CustomListView'
+import TicketCard from '~/components/global/TicketCard'
 
 export default {
   name: 'Tickets',
-  components: { CustomListView, AppFeel },
+  components: { TicketCard, AppFeel },
   middleware: 'isAuthenticated',
   data() {
     return {
