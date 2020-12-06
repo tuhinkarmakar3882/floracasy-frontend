b11126a8ef338e53c6effec32abe75932a8a2a91
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-06 23:33:01 +0530

    Added base Styles
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/HelpAndSupport/PopularTopics/WeeklyChallenge.vue
+++ pages/Home/MoreOptions/HelpAndSupport/PopularTopics/WeeklyChallenge.vue
@@ -1,49 +1,66 @@
 <template>
   <AppFeel
     class="weekly-challenge-topic-page"
-    :on-back="navigationRoutes.Home.MoreOptions.HelpAndSupport.index"
+    :on-back="
+      navigationRoutes.Home.MoreOptions.HelpAndSupport.PopularTopics.index
+    "
   >
     <template slot="app-bar-title"> {{ pageTitle }}</template>
 
     <template slot="main">
-      <h3>What is our weekly challenge!</h3>
+      <section class="px-4">
+        <article class="my-4 text-center">
+          <h3 class="heading-title">What is our weekly challenge!</h3>
+          <p>
+            We will try our best to give exciting features to our users. So
+            basically we thought what new we can do for our users ! a out come
+            of our thoughts is &quot;weekly challenge &quot; or you may called
+            it &quot; weekly content writing competition&quot;. But you, our
+            valuable users need to decide is it a great endeavour or not! only
+            you folks can make our assiduity best by participating &amp; win
+            exciting prizes.
+          </p>
+          <img
+            src="https://picsum.photos/156"
+            alt="showing some photos of challenges"
+          />
+        </article>
 
-      <h5>
-        We will try our best to give exciting features to our users. So
-        basically we thought what new we can do for our users ! a out come of
-        our thoughts is &quot;weekly challange&quot; or you may called it
-        &quot;weekly content writing competition&quot;. But you, our valuable
-        users need to decide is it a great endeavour or not! only you folks can
-        make our assiduity best by perticipating &amp; win exciting prizes.
-      </h5>
+        <article class="my-4 text-center">
+          <h3 class="heading-title">What makes it so exciting?</h3>
+          <p>
+            Mainly this is a content writing competition. users can enroll their
+            name in this competition. They can show their commands on content
+            writing. Given a few topics to write , competitors who won these
+            competition won new &amp; exciting prizes.
+          </p>
+        </article>
 
-      <img
-        src="https://picsum.photos/156"
-        alt="showing some photos of challenges"
-      />
-
-      <h3>What makes it so exciting?</h3>
-
-      <h5>
-        Mainly this is a content writing competition. users can enroll their
-        name in this competition. They can show their commands on content
-        writing. Given a few topics to write , competitors who won these
-        competition won new &amp; exciting prizes.
-      </h5>
-
-      <h3>How to claim your winning prizes</h3>
-
-      <h5>
-        one who already won this competition can claim their money by submitting
-        their details on a form given by floracasy team. when one comes to
-        enroll his/her name for the competition he/she needs to give a valid
-        phone number so that we can contact further &amp; deliver their prizes
-        &amp; certificate
-      </h5>
-
-      <label> enter your name </label>
-      <input type="text" />
-      <button>shout out</button>
+        <article class="my-4 text-center">
+          <h3 class="heading-title">How to claim your winning prizes</h3>
+          <p>
+            one who already won this competition can claim their money by
+            submitting their details on a form given by floracasy team. when one
+            comes to enroll his/her name for the competition he/she needs to
+            give a valid phone number so that we can contact further &amp;
+            deliver their prizes &amp; certificate
+          </p>
+        </article>
+        <div class="material-form-field mt-2">
+          <input
+            id="unknown-input"
+            ref="coverImageUrl"
+            type="text"
+            required
+            name="text"
+            autocomplete="off"
+          />
+          <label class="material-form-field-label" for="unknown-input">
+            Enter your name
+          </label>
+        </div>
+        <button class="primary-outlined-btn">shout out</button>
+      </section>
     </template>
   </AppFeel>
 </template>
