aacd71eb422576b8c406f320571f7eb04289f5b4
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-07 21:03:50 +0530

    Added Form
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/HelpAndSupport/ContactSupport.vue
+++ pages/Home/MoreOptions/HelpAndSupport/ContactSupport.vue
@@ -6,8 +6,50 @@
     <template slot="app-bar-title"> {{ pageTitle }}</template>
 
     <template slot="main">
-      <h6>This is my super duper {{ pageTitle }} Page</h6>
-      <p>And I Love it</p>
+      <section class="px-4 my-4">
+        <h5 class="heading-title">Tell us more about it</h5>
+
+        <div class="material-form-field">
+          <input
+            id="issue-title"
+            ref="issueTitle"
+            type="text"
+            required
+            name="text"
+            autocomplete="off"
+          />
+          <label class="material-form-field-label" for="issue-title">
+            Issue Title
+          </label>
+        </div>
+
+        <div class="material-form-field mt-2">
+          <input
+            id="issue-details"
+            ref="issueDetails"
+            type="text"
+            required
+            name="text"
+            autocomplete="off"
+          />
+          <label class="material-form-field-label" for="issue-details">
+            Issue Details
+          </label>
+        </div>
+
+        <div class="text-center mt-8">
+          <RippleButton
+            class="my-6"
+            class-list="primary-btn"
+            :on-click="raiseTicket"
+            :loading="raiseTicketLoading"
+            :disabled="!isValidForm"
+            style="width: 212px"
+          >
+            Raise Support Ticket
+          </RippleButton>
+        </div>
+      </section>
     </template>
   </AppFeel>
 </template>
@@ -15,15 +57,26 @@
 <script>
 import AppFeel from '@/components/global/Layout/AppFeel'
 import { navigationRoutes } from '@/navigation/navigationRoutes'
+import RippleButton from '~/components/global/RippleButton'
 export default {
   name: 'ContactSupport',
-  components: { AppFeel },
+  components: { RippleButton, AppFeel },
   middleware: 'isAuthenticated',
   data() {
     return {
       navigationRoutes,
       pageTitle: 'Contact Support',
+      raiseTicketLoading: false,
+      isValidForm: false,
     }
+  },
+  methods: {
+    raiseTicket() {
+      this.raiseTicketLoading = true
+      setTimeout(() => {
+        this.raiseTicketLoading = false
+      }, 2000)
+    },
   },
 
   head() {
