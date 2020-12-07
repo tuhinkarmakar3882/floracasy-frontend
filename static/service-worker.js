ed772c985e447dba50365ec4ac0677dc459f871d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-07 16:37:40 +0530

    Updated service-worker.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
+const version = '1.0.0'
 const precacheName = 'precache-floracasy-pwa-v' + new Date().getTime()
-const runtimeCacheName = 'runtime-floracasy-pwa-v' + new Date().getTime()
 
 const precacheList = [
   'https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400&family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;1,300;1,400&family=Prata&family=Roboto:wght@300;400&display=swap',
@@ -7,51 +7,98 @@ const precacheList = [
 ]
 
 /* Cache on install */
-self.addEventListener('install', (event) => {
-  this.skipWaiting()
-  console.log('Installing...')
+self.addEventListener('install', function (event) {
+  console.log('WORKER: install event in progress.')
   event.waitUntil(
-    caches.open(precacheName).then((cache) => {
-      return cache.addAll(precacheList)
-    })
-  )
-  console.log('Installed.')
-})
-
-/* Clear cache on activate */
-self.addEventListener('activate', (event) => {
-  console.log('Cleaning old Caches')
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => cacheName.startsWith('django-pwa-'))
-          .filter((cacheName) => cacheName !== precacheName)
-          .map((cacheName) => caches.delete(cacheName))
-      )
-    })
-  )
-  console.log('Activated!')
-})
-
-/* Serve from Cache */
-self.addEventListener('fetch', (event) => {
-  console.log('fetch')
-  event.respondWith(
     caches
-      .match(event.request)
-      .then(async (response) => {
-        if (response) {
-          return response
-        }
-        const networkResponse = await fetch(event.request)
-        const clonedResponse = networkResponse.clone()
-        const runtimeCache = await caches.open(runtimeCacheName)
-        await runtimeCache.put(event.request, networkResponse)
-        return Promise.resolve(clonedResponse)
+      .open(version + precacheName)
+      .then((cache) => {
+        return cache.addAll(precacheList)
       })
-      .catch(() => {
-        return caches.match('/offline/')
+      .then(function () {
+        console.log('WORKER: install completed')
+      })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  console.log('WORKER: fetch event in progress.')
+
+  if (event.request.method !== 'GET') {
+    console.log(
+      'WORKER: fetch event ignored.',
+      event.request.method,
+      event.request.url
+    )
+    return
+  }
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      const networked = fetch(event.request)
+        .then(fetchedFromNetwork, unableToResolve)
+        .catch(unableToResolve)
+
+      console.log(
+        'WORKER: fetch event',
+        cached ? '(cached)' : '(network)',
+        event.request.url
+      )
+      return cached || networked
+
+      function fetchedFromNetwork(response) {
+        const cacheCopy = response.clone()
+        console.log('WORKER: fetch response from network.', event.request.url)
+
+        caches
+          .open(version + 'pages')
+          .then(function add(cache) {
+            cache.put(event.request, cacheCopy)
+          })
+          .then(function () {
+            console.log(
+              'WORKER: fetch response stored in cache.',
+              event.request.url
+            )
+          })
+        return response
+      }
+
+      function unableToResolve() {
+        console.log('WORKER: fetch request failed in both cache and network.')
+        /* Here we're creating a response programmatically. The first parameter is the
+             response body, and the second one defines the options for the response.
+          */
+        return new Response('<h1>Service Unavailable</h1>', {
+          status: 503,
+          statusText: 'Service Unavailable',
+          headers: new Headers({
+            'Content-Type': 'text/html',
+          }),
+        })
+      }
+    })
+  )
+})
+
+self.addEventListener('activate', function (event) {
+  console.log('WORKER: activate event in progress.')
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              return !key.startsWith(version)
+            })
+            .map(function (key) {
+              return caches.delete(key)
+            })
+        )
+      })
+      .then(function () {
+        console.log('WORKER: activate completed.')
       })
   )
 })
