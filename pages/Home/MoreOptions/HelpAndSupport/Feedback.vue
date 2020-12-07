0aafa597a3b0a64e376e9af62957420c74cce2c7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-07 22:16:06 +0530

    Added Feedback.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/HelpAndSupport/Feedback.vue
+++ pages/Home/MoreOptions/HelpAndSupport/Feedback.vue
@@ -7,18 +7,44 @@
 
     <template slot="main">
       <section class="px-4 my-4">
-        <div class="material-form-field mt-2">
+        <h5 class="heading-title">We'd love to hear from you!</h5>
+        <p class="text-center pb-8">
+          We're working tirelessly to bring the best possible experience to you,
+          your opinions are really valuable for us.
+        </p>
+
+        <div class="material-form-field">
           <input
-            id="feedback-message"
-            ref="feedbackMessage"
+            id="feedback-form"
+            ref="feedbackTitle"
+            v-model="feedbackText"
             type="text"
             required
             name="text"
             autocomplete="off"
           />
-          <label class="material-form-field-label" for="feedback-message">
-            Write your feedback
+          <label class="material-form-field-label" for="feedback-form">
+            Type your feedback here
           </label>
+          <small
+            style="display: block; font-weight: 400; font-size: 13px"
+            class="mt-3 hint-text"
+          >
+            (Hint: At least 10 characters are required)
+          </small>
+        </div>
+
+        <div class="text-center mt-7 pt-4">
+          <RippleButton
+            class="my-6"
+            class-list="primary-btn"
+            :on-click="sendFeedback"
+            :loading="sendFeedbackLoading"
+            :disabled="!isValidForm"
+            style="width: 212px"
+          >
+            Send Feedback
+          </RippleButton>
         </div>
       </section>
     </template>
@@ -28,17 +54,33 @@
 <script>
 import AppFeel from '@/components/global/Layout/AppFeel'
 import { navigationRoutes } from '@/navigation/navigationRoutes'
+import RippleButton from '~/components/global/RippleButton'
 export default {
   name: 'Feedback',
-  components: { AppFeel },
+  components: { RippleButton, AppFeel },
   middleware: 'isAuthenticated',
   data() {
     return {
       navigationRoutes,
-      pageTitle: 'Feedback',
+      pageTitle: 'Send Feedback',
+      sendFeedbackLoading: false,
+      feedbackText: '',
+      isValidForm: false,
     }
   },
-
+  watch: {
+    feedbackText(newValue) {
+      this.isValidForm = newValue.trim().length > 10
+    },
+  },
+  methods: {
+    sendFeedback() {
+      this.sendFeedbackLoading = true
+      setTimeout(() => {
+        this.sendFeedbackLoading = false
+      }, 2000)
+    },
+  },
   head() {
     return {
       title: this.pageTitle,
