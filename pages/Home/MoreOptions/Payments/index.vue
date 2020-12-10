268685d8d4d93c2dc5eab21b282a414e4ef38ab6
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-10 19:18:10 +0530

    Added HTML CONTENT for Payment Index page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/Payments/index.vue
+++ pages/Home/MoreOptions/Payments/index.vue
@@ -6,64 +6,63 @@
     <template slot="app-bar-title"> {{ pageTitle }}</template>
 
     <template slot="main">
-      <div>
+      <section class="px-4 my-4">
         <div>
-          <div>
-            <button class="primary-btn">back</button>
-            <h3>Payment</h3>
-            <p>profile icon</p>
-
-            <hr />
-          </div>
-
-          <div>
-            <h2>How much you have earned</h2>
-            <p>total earnings in doller</p>
-            <strong> $. 100 </strong>
-
-            <p>this section is for graph</p>
-          </div>
+          <button class="primary-btn">back</button>
+          <h3>Payment</h3>
+          <p>profile icon</p>
 
           <hr />
+        </div>
 
+        <div>
+          <h2>How much you have earned</h2>
+          <p>total earnings in doller</p>
+          <strong> $. 100 </strong>
+
+          <p>this section is for graph</p>
+        </div>
+
+        <hr />
+
+        <div>
+          <h2>Claim your Earned money</h2>
+          <p>
+            Now you just need to go steps to claim your money. When your earning
+            reaches to calimed money($100), your
+            <strong> claim button</strong> will be enabled.
+          </p>
           <div>
-            <h2>Claim your Earned money</h2>
-            <p>
-              Now you just need to go steps to claim your money. When your
-              earning reaches to calimed money($100), your
-              <strong> claim button</strong> will be enabled.
-            </p>
-            <div>
-              <p>$ 20 more to claim your money</p>
-              <button class="primary-btn">claim</button>
-            </div>
-          </div>
-
-          <hr />
-
-          <div>
-            <h2>just have a look to your earning history : Last 6th month</h2>
-
-            <div>This section is for graph but i can&#39;t make this</div>
-          </div>
-
-          <hr />
-
-          <div>
-            <p>
-              To know your every bolg&#39;s insight view; total view and many
-              more exciting than usual. please go &amp; check our premium pack
-            </p>
-
-            <img
-              src="https://images.unsplash.com/photo-1607448884844-646fe28e07a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=668&amp;q=80"
-              alt="image of premium"
-            />
-
-            <button class="primary-btn">go premium</button>
+            <p>$ 20 more to claim your money</p>
+            <button class="primary-btn">claim</button>
           </div>
         </div>
-      </div>
+
+        <hr />
+
+        <div>
+          <h2>just have a look to your earning history : Last 6th month</h2>
+
+          <div>This section is for graph but i can&#39;t make this</div>
+        </div>
+
+        <hr />
+
+        <div>
+          <p>
+            To know your every bolg&#39;s insight view; total view and many more
+            exciting than usual. please go &amp; check our premium pack
+          </p>
+
+          <img
+            src="https://images.unsplash.com/photo-1607448884844-646fe28e07a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=668&amp;q=80"
+            alt="image of premium"
+            height="250"
+          />
+
+          <button class="primary-btn">go premium</button>
+        </div>
+      </section>
     </template>
   </AppFeel>
 </template>
@@ -71,12 +70,11 @@
 <script>
 import AppFeel from '@/components/global/Layout/AppFeel'
 import { navigationRoutes } from '@/navigation/navigationRoutes'
-import KeyPoint from '@/components/global/KeyPoint'
 
 export default {
   name: 'Payments',
   middleware: 'isAuthenticated',
-  components: { KeyPoint, AppFeel },
+  components: { AppFeel },
   data() {
     return {
       navigationRoutes,
