81dac2c9cb0ad1728d9930159483a57624b8029d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-25 16:14:55 +0530

    Rewriting Axios Error Handling
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,4 +1,9 @@
-import handleAxiosRequestError from '@/utils/ErrorHandling'
+import { cookieSavingConfig } from '~/environmentalVariables'
+
+const errorMessages = {
+  credentialsWereNotProvided: 'Authentication credentials were not provided.',
+  invalidTokens: 'Given token not valid for any token type',
+}
 
 export default async function ({ $axios, $cookies, store }) {
   $axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
@@ -8,10 +13,31 @@ export default async function ({ $axios, $cookies, store }) {
   if (store.state.authState)
     await store.dispatch('NotificationChannel/fetchNotificationChannelId')
 
-  $axios.onError(({ response: error }) => {
+  $axios.onError(async ({ response: error }) => {
     if (!error) {
       console.log('Having Trouble to connect')
     }
-    handleAxiosRequestError(error)
+    if (error.status === 401) {
+      console.log(error.data.detail)
+
+      switch (error.data.detail) {
+        case errorMessages.credentialsWereNotProvided:
+          console.warn('No Credentials were Provided')
+          process.client && window.location.reload()
+          break
+
+        case errorMessages.invalidTokens:
+          console.warn('Credentials Expired.')
+          $cookies.set('access', '', cookieSavingConfig)
+          process.client && window.location.reload()
+          break
+
+        default:
+          console.error('Unhandled Error', error)
+          process.client && window.location.reload()
+      }
+    }
   })
+
+  // $axios.onRequest((req) => console.log(req.headers))
 }
