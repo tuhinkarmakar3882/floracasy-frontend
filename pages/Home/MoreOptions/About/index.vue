a0ac68b272ea372fee9a71fd15bb23422fca2e95
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2020-12-27 16:28:22 +0530

    Working on About Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/About/index.vue
+++ pages/Home/MoreOptions/About/index.vue
@@ -8,68 +8,35 @@
     <template slot="main">
       <main class="my-4">
         <section class="banner">
-          <div class="logo">
-            <Logo class="mr-4" />
-            <h4>Floracasy</h4>
-          </div>
+          <Logo class="mr-4" />
+          <h4>Floracasy</h4>
         </section>
 
-        <section class="px-4">
-          <div class="intro">
-            More you read More you know
-
-            <br />
-
-            &lt; vector art. &gt;
-          </div>
-
-          <br />
-
-          <div class="next part">
-            We started Floracasy because we believe that what you read matters
-            and that good writing is valuable. We believe that writers,
-            bloggers, thinkers, and creatives of every background should be able
-            to pursue their curiosity, generating income directly from their own
-            audiences and on their own terms. When readers pay writers directly,
-            writers can focus on doing the work they care about most. A few
-            hundred paying subscribers can support a livelihood. A few thousand
-            makes it lucrative.
-
-            <button class="active">Join now</button>
-          </div>
-
-          <div class="writer review">
-            <h4>What our writer says about us</h4>
-
-            <p>
-              A blank page is also a door. At Medium you can walk through it.
-              It's easy and free to share your thinking on any topic, connect
-              with an audience, express yourself with a range of publishing
-              tools, and even earn money for your work.
-            </p>
-            <button class="active">Start writing</button>
-          </div>
-
-          <div class="earning">
-            <h4>We create independent environment for earning</h4>
-
-            <p>
-              we try to create a great environment so that everyone who have
-              willpower to write , can write . By writing everyone can earn a
-              good amount of money
-            </p>
-
-            <button class="active">start earning</button>
-          </div>
-
-          <div class="Get priorie">
-            <h4>Get prioritize</h4>
-
-            With special permium pack get full control & explore more
-
-            <button>become premium member</button>
-          </div>
+        <section class="tab-bar">
+          <p
+            v-for="(tab, index) in tabs"
+            :key="index"
+            v-ripple
+            :class="index === activeTab ? 'active-tab' : ''"
+            @click="setActiveTabTo(index)"
+          >
+            {{ tab }}
+          </p>
         </section>
+
+        <div ref="tabNavigation"></div>
+
+        <section v-if="activeTab === 0">
+          <h1 class="text-center">Tab {{ activeTab }}</h1>
+        </section>
+
+        <section v-else-if="activeTab === 1">
+          <h1 class="text-center">Tab {{ activeTab }}</h1>
+        </section>
+
+        <div v-else-if="activeTab === 2">
+          <h1 class="text-center">Tab {{ activeTab }}</h1>
+        </div>
       </main>
     </template>
   </AppFeel>
@@ -83,15 +50,26 @@ import { navigationRoutes } from '@/navigation/navigationRoutes'
 export default {
   name: 'About',
   components: { Logo, AppFeel },
-  middleware: 'isAuthenticated',
+  // middleware: 'isAuthenticated',
   data() {
     return {
-      tabNumber: 0,
+      activeTab: 0,
       pageTitle: 'About us',
       navigationRoutes,
+      tabs: ['Pro Zone', 'Our Story', 'The Vision'],
     }
   },
   mounted() {},
+
+  methods: {
+    setActiveTabTo(newTabNumber) {
+      this.activeTab = newTabNumber
+      this.$nextTick(() => {
+        this.$refs.tabNavigation.scrollTop = 0
+      })
+    },
+  },
+
   head() {
     return {
       title: this.pageTitle,
@@ -110,18 +88,37 @@ $blog-border-radius: 20px;
 
   main {
     .banner {
-      .logo {
-        display: flex;
-        align-items: center;
-        justify-content: center;
+      display: flex;
+      align-items: center;
+      justify-content: center;
 
-        h4 {
-          margin: $standard-unit 0;
-        }
+      h4 {
+        margin: $standard-unit 0;
+      }
+    }
+
+    .tab-bar {
+      display: grid;
+      text-align: center;
+      grid-template-columns: repeat(3, 1fr);
+      position: sticky;
+      top: (2 * $x-large-unit) - $double-unit;
+      background-color: $nav-bar-bg;
+      box-shadow: $down-only-box-shadow;
+      z-index: 1;
+      transition: all 0.3s ease-in-out;
+
+      * {
+        padding: 0.7rem 0;
+        font-size: 1rem;
+        font-weight: 300;
       }
 
-      small {
-        color: $muted;
+      .active-tab {
+        color: $secondary;
+        font-weight: 400;
+        background: $card-background;
+        transition: all 100ms ease-in-out;
       }
     }
   }
