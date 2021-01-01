58dd0f720be4b6ab61bcd673bab50b03f0899354
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-01 16:11:43 +0530

    Improved readability
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Notifications/index.vue
+++ pages/Home/Notifications/index.vue
@@ -1,66 +1,13 @@
 <template>
   <div class="notification-page mb-6">
-    <section
-      ref="banner-block"
-      :key="'banner-item'"
-      class="banner text-center mb-6 px-2"
-      :style="[showBanner ? { height: '300px' } : {}]"
-    >
-      <h5 v-if="!maybe" class="my-4">
-        Would you like to receive notifications?
-      </h5>
-      <div v-if="!maybe" class="actions my-3">
-        <button
-          v-ripple=""
-          class="secondary-btn mb-4 mx-2"
-          @click="alert('Will be implemented shortly')"
-        >
-          Yes
-        </button>
-        <button
-          v-ripple=""
-          class="secondary-outlined-btn mb-4 mx-2"
-          @click="showMaybeAndCollapse"
-        >
-          May be later
-        </button>
-      </div>
-      <p v-if="!maybe" class="mb-3">
-        You can always change this setting later in the
-        <nuxt-link :to="navigationRoutes.Home.MoreOptions.Preferences.index">
-          Preferences
-        </nuxt-link>
-      </p>
+    <RequestPermissionDialog />
 
-      <h5 v-if="maybe" class="text-center mb-4">No Problem</h5>
-      <p v-if="maybe" class="text-center mb-4">
-        Tip: You can always change this setting later in the
-        <nuxt-link :to="navigationRoutes.Home.MoreOptions.Preferences.index">
-          Preferences
-        </nuxt-link>
-      </p>
-    </section>
     <section class="notifications">
-      <div
+      <NotificationItem
         v-for="notification in notifications"
         :key="notification.id"
-        v-ripple="notification.notificationType.color + '55'"
-        class="px-4 py-2"
-      >
-        <p
-          :class="notification.notificationType.icon"
-          :style="{ color: notification.notificationType.color }"
-          style="font-size: 24px"
-        />
-        <p>
-          <span class="message">
-            {{ notification.message }}
-          </span>
-          <br />
-          {{ parseTimeUsingMoment(notification.createdAt) }}
-          <span class="dot" />
-        </p>
-      </div>
+        :notification="notification"
+      />
     </section>
 
     <client-only>
@@ -85,13 +32,16 @@
 
 <script>
 import { mapGetters } from 'vuex'
-import { parseTimeUsingMoment } from '@/utils/utility'
 import { navigationRoutes } from '~/navigation/navigationRoutes'
 import endpoints from '~/api/endpoints'
+import NotificationItem from '~/components/global/Home/Notification/NotificationItem'
 
 export default {
   name: 'Notifications',
   components: {
+    RequestPermissionDialog: () =>
+      import('@/components/global/Home/Notification/RequestPermissionDialog'),
+    NotificationItem,
     LoadingIcon: () => import('@/components/global/LoadingIcon'),
   },
   layout: 'MobileApp',
@@ -103,9 +53,6 @@ export default {
       pageTitle: 'Notifications',
       notifications: [],
       page: 1,
-      showBanner: false,
-      maybe: false,
-      computedHeight: 0,
       notificationEndpoint: endpoints.notification_system.fetch,
     }
   },
@@ -124,43 +71,15 @@ export default {
   },
 
   async mounted() {
-    this.initHeight()
-    if (Notification.permission !== 'granted') {
-      const hideNotificationConsent = localStorage.getItem(
-        'hide-notification-consent'
-      )
-      this.showBanner = hideNotificationConsent
-        ? hideNotificationConsent < 0
-        : true
-
-      hideNotificationConsent &&
-        localStorage.setItem(
-          'hide-notification-consent',
-          (parseInt(hideNotificationConsent) - 1).toString()
-        )
-    }
     await this.$store.dispatch('NavigationState/updateBottomNavActiveLink', {
       linkPosition: 3,
     })
     await this.$store.dispatch('NavigationState/updateTopNavActiveLink', {
       linkPosition: -1,
     })
-
-    this.isContentLoading = false
   },
 
   methods: {
-    parseTimeUsingMoment,
-
-    showMaybeAndCollapse() {
-      this.maybe = true
-
-      setTimeout(() => {
-        this.showBanner = false
-      }, 2500)
-      localStorage.setItem('hide-notification-consent', '200')
-    },
-
     async setupUser() {
       const currentUser = await this.$store.getters['UserManagement/getUser']
       if (!currentUser) {
@@ -187,22 +106,6 @@ export default {
       } catch (e) {
         $state.complete()
       }
-    },
-
-    initHeight() {
-      this.$refs['banner-block'].style.height = 'auto'
-      this.$refs['banner-block'].style.position = 'absolute'
-      this.$refs['banner-block'].style.visibility = 'hidden'
-      this.$refs['banner-block'].style.display = 'block'
-
-      this.computedHeight = getComputedStyle(this.$refs['banner-block']).height
-      this.computedHeight =
-        (parseInt(this.computedHeight.replace('px', '')) + 40).toString() + 'px'
-
-      this.$refs['banner-block'].style.position = null
-      this.$refs['banner-block'].style.visibility = null
-      this.$refs['banner-block'].style.display = null
-      this.$refs['banner-block'].style.height = 0
     },
   },
 
@@ -245,42 +148,6 @@ export default {
       button {
         min-width: auto;
         width: 148px;
-      }
-    }
-  }
-
-  .notifications {
-    div {
-      display: grid;
-      align-items: center;
-      grid-template-columns: 10% 90%;
-      grid-column-gap: $standard-unit;
-      padding: $medium-unit;
-
-      p {
-        font-family: $Nunito-Sans;
-        font-size: $milli-unit;
-        align-self: flex-start;
-
-        .message {
-          color: #cacaca;
-          font-family: $Raleway;
-          font-size: $standard-unit;
-        }
-      }
-
-      .dot {
-        display: inline-block;
-        width: $nano-unit + $single-unit;
-        height: $nano-unit + $single-unit;
-        border-radius: 50%;
-        background-color: $success-light;
-        margin-left: $nano-unit;
-        margin-bottom: $single-unit;
-      }
-
-      &:nth-child(even) {
-        background-color: $nav-bar-bg;
       }
     }
   }
