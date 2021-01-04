eb840e987f2b39c71806f7d78f440bd5fc2d2f5f
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-04 22:39:20 +0530

    Connected FetchCommunityPosts.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Community/FetchCommunityPosts.vue
+++ components/global/Community/FetchCommunityPosts.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="fetch-community-posts-component">
     <h6 class="text-center">{ - FetchCommunityPosts - }</h6>
+    <FollowSuggestions />
   </div>
 </template>
 
 <script>
+import FollowSuggestions from '~/components/global/Community/FollowSuggestions'
 export default {
   name: 'FetchCommunityPosts',
+  components: { FollowSuggestions },
 }
 </script>
