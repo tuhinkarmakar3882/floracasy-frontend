c34bc279c2824bdb3ea6396e8a1f1f56f69dc337
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-04 19:58:32 +0530

    Added Micro Interactions
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Community/index.vue
+++ pages/Home/Community/index.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="community-page">
-    <section class="top-actions px-4 mt-6 mb-8">
-      <input
-        id="search-box"
-        type="text"
-        autocomplete="off"
-        placeholder="Type here to search"
-      />
-      <label
-        for="search-box"
-        class="mdi mdi-magnify px-4 ml-4"
-        aria-label="Type Here to Search for People"
-      />
-      <span v-ripple class="mdi mdi-check px-4 mr-4" />
+    <section class="top-actions pr-4 mt-6 mb-8">
+      <div v-ripple class="add-post-btn px-4">
+        <span class="mdi mdi-camera mdi-36px secondary" />
+      </div>
+
+      <div class="search-box">
+        <input
+          id="search-box"
+          type="text"
+          autocomplete="off"
+          placeholder="Type here to search"
+        />
+        <label
+          for="search-box"
+          class="mdi mdi-magnify px-4 mdi-24px"
+          aria-label="Type Here to Search for People"
+        />
+        <span
+          v-ripple
+          class="mdi mdi-check mdi-24px px-4"
+          aria-label="Click here to search"
+        />
+      </div>
     </section>
 
     <section class="story-updates py-2 px-4">
@@ -107,63 +117,81 @@ export default {
 
 .community-page {
   .top-actions {
-    position: relative;
+    display: flex;
+    align-items: center;
 
-    span,
-    label {
-      height: 48px;
-      position: absolute !important;
-      top: 0;
-      display: grid;
-      font-size: 24px;
-      place-items: center;
-      border-radius: 50px;
-      transition: all 0.2s ease-in-out;
-    }
+    .search-box {
+      position: relative;
+      width: 100%;
 
-    span {
-      right: 0;
-      opacity: 0;
-      display: none;
-      color: $secondary;
-    }
+      $custom-muted: #777;
+      $custom-input-border: #333;
 
-    label {
-      left: 0;
-      color: $disabled;
-    }
-
-    input {
-      transition: all 0.2s ease-in-out;
-      border: 1px solid $disabled;
-      border-radius: 50px;
-      height: 48px;
-      padding: 0 48px;
-      color: $disabled;
-      font-weight: 300;
-      font-family: $Raleway;
-      letter-spacing: 1px;
-      font-size: 1rem;
-
-      &::placeholder {
-        color: $disabled;
-        font-weight: 300;
+      span,
+      label {
+        height: 48px;
+        position: absolute !important;
+        top: 0;
+        display: grid;
+        place-items: center;
+        border-radius: 50px;
+        transition: all 0.2s ease-in-out;
       }
 
-      &:focus,
-      &:not(:placeholder-shown) {
-        border: 1px solid $secondary-highlight;
+      span {
+        right: 0;
+        opacity: 0;
+        transform: scale(0);
         color: $secondary;
-
-        & ~ label {
-          color: $secondary;
-        }
       }
 
-      &:not(:placeholder-shown) {
-        & ~ span {
-          display: grid;
-          opacity: 1;
+      label {
+        left: 0;
+        color: $custom-muted;
+      }
+
+      input {
+        transition: all 0.2s ease-in-out;
+        border: 1px solid $custom-input-border;
+        border-radius: 50px;
+        height: 48px;
+        padding: 0 48px;
+        color: $custom-muted;
+        font-weight: 300;
+        font-family: $Raleway;
+        letter-spacing: 1px;
+        font-size: 1rem;
+
+        &::placeholder {
+          color: $custom-muted;
+          font-weight: 300;
+        }
+
+        &:focus,
+        &:not(:placeholder-shown) {
+          border: 1px solid $secondary-highlight;
+
+          & ~ label {
+            color: $secondary;
+          }
+        }
+
+        &:not(:placeholder-shown) {
+          color: $secondary;
+          padding-left: 20px;
+
+          & ~ span {
+            transform: scale(1);
+            opacity: 1;
+          }
+
+          & ~ label {
+            width: 0;
+            margin: 0;
+            padding: 0;
+            opacity: 0;
+            overflow: hidden;
+          }
         }
       }
     }
