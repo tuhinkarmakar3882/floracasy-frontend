e32f6123c478854b561403ba89ce6e37f46c0ff7
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-04 19:24:28 +0530

    Added Search Box
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Community/index.vue
+++ pages/Home/Community/index.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="community-page">
-    <section class="story-updates py-2">
+    <section class="top-actions px-4 mt-6 mb-8">
+      <input
+        id="search-box"
+        type="text"
+        autocomplete="off"
+        placeholder="Type here to search"
+      />
+      <label
+        for="search-box"
+        class="mdi mdi-magnify px-4 ml-4"
+        aria-label="Type Here to Search for People"
+      />
+      <span v-ripple class="mdi mdi-check px-4 mr-4" />
+    </section>
+
+    <section class="story-updates py-2 px-4">
       <div v-for="(story, index) in stories" :key="index" class="wrapper mx-4">
         <img
           :src="story.user.photoURL + index"
@@ -91,6 +106,69 @@ export default {
 @import 'assets/all-variables';
 
 .community-page {
+  .top-actions {
+    position: relative;
+
+    span,
+    label {
+      height: 48px;
+      position: absolute !important;
+      top: 0;
+      display: grid;
+      font-size: 24px;
+      place-items: center;
+      border-radius: 50px;
+      transition: all 0.2s ease-in-out;
+    }
+
+    span {
+      right: 0;
+      opacity: 0;
+      display: none;
+      color: $secondary;
+    }
+
+    label {
+      left: 0;
+      color: $disabled;
+    }
+
+    input {
+      transition: all 0.2s ease-in-out;
+      border: 1px solid $disabled;
+      border-radius: 50px;
+      height: 48px;
+      padding: 0 48px;
+      color: $disabled;
+      font-weight: 300;
+      font-family: $Raleway;
+      letter-spacing: 1px;
+      font-size: 1rem;
+
+      &::placeholder {
+        color: $disabled;
+        font-weight: 300;
+      }
+
+      &:focus,
+      &:not(:placeholder-shown) {
+        border: 1px solid $secondary-highlight;
+        color: $secondary;
+
+        & ~ label {
+          color: $secondary;
+        }
+      }
+
+      &:not(:placeholder-shown) {
+        & ~ span {
+          display: grid;
+          opacity: 1;
+        }
+      }
+    }
+  }
+
   .story-updates {
     display: flex;
     width: 100%;
@@ -130,6 +208,14 @@ export default {
         z-index: -1;
         border-radius: 50%;
         border: 2px solid $secondary-highlight;
+      }
+
+      &:first-child {
+        margin-left: 0;
+      }
+
+      &:last-child {
+        margin-right: 0;
       }
     }
   }
