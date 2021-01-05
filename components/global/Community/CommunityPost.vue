003e5c83e777d9188c6e02c5edc1fe68d0696510
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-05 22:56:32 +0530

    Added CSS FIX
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Community/CommunityPost.vue
+++ components/global/Community/CommunityPost.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="community-post-component">
     <section class="post-header">
-      <img src="https://picsum.photos/100" alt="1234567890" />
+      <img src="https://picsum.photos/100" alt="1234567890" class="mr-3" />
       <div class="details">
         <p class="vibrant">{{ post.user.displayName }}</p>
         <small>{{ getRelativeTime(new Date()) }}</small>
       </div>
-      <aside>
-        <span class="mdi mdi-dots-vertical" />
-      </aside>
+      <i
+        v-ripple="'#4f4f4f5F'"
+        class="mdi mdi-dots-vertical mr-2 inline-block align-middle ml-auto"
+      />
     </section>
 
-    <section class="post-body">
+    <section class="post-body pt-5 pb-3">
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
         asperiores atque consectetur eos facere hic in ipsa, ipsum laborum
@@ -47,7 +48,8 @@
 </template>
 
 <script lang="ts">
-import { shorten, getRelativeTime } from '@/utils/utility'
+import { getRelativeTime, shorten } from '@/utils/utility'
+import { navigationRoutes } from '~/navigation/navigationRoutes'
 
 interface User {
   displayName: string
@@ -70,6 +72,11 @@ export default {
       required: true,
     } as Post,
   },
+  data() {
+    return {
+      navigationRoutes,
+    }
+  },
   methods: {
     shorten,
     getRelativeTime,
@@ -84,6 +91,56 @@ export default {
 @import 'assets/all-variables';
 
 .community-post-component {
+  .post-header {
+    display: flex;
+    position: relative;
+
+    $image-size: 3.4rem;
+
+    img {
+      min-width: $image-size;
+      min-height: $image-size;
+      height: $image-size;
+      width: $image-size;
+      object-fit: cover;
+      border-radius: 50%;
+      box-shadow: $default-box-shadow;
+    }
+
+    .details {
+      p {
+        font-size: 1.15rem;
+        font-weight: 500;
+        line-height: 23px;
+        margin-bottom: 0.5rem;
+      }
+
+      small {
+        font-size: 0.8rem;
+        font-style: italic;
+        line-height: 16px;
+      }
+    }
+
+    i {
+      position: absolute !important;
+      right: -8px;
+      top: 0;
+      height: 100%;
+      font-size: $x-large-unit;
+      width: 2 * $large-unit;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: $secondary;
+      border-radius: $nano-unit;
+    }
+  }
+
+  .post-body {
+    text-align: justify;
+  }
+
   .post-actions {
     font-family: $Nunito-Sans;
     color: $secondary;
