6713a3f7ebe4233bf981546bb8cd4fbfcdb03148
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-08 21:11:58 +0530

    WIP ADD POST MARKUP and User Fetch
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Community/Posts/AddPost.vue
+++ pages/Home/Community/Posts/AddPost.vue
@@ -1,21 +1,70 @@
 <template>
-  <div class="add-new-post-page">
-    {{ pageTitle }}
-  </div>
+  <AppFeel
+    :on-back="navigationRoutes.Home.Community.index"
+    class="add-new-post-page"
+  >
+    <template slot="app-bar-title"> {{ pageTitle }}</template>
+
+    <template slot="main">
+      <main v-if="!isReady">
+        <LoadingIcon />
+      </main>
+
+      <main v-else>
+        <div class="header mb-6 px-4">
+          <img
+            :src="user.photoURL"
+            alt="profile-picture"
+            height="52"
+            width="52"
+          />
+          <section class="user-details ml-4">
+            <p class="secondary">{{ user.displayName }}</p>
+            <small v-if="user.designation">{{ user.designation }} </small>
+          </section>
+        </div>
+        <div class="actions mt-4">
+          <p v-ripple>
+            <span class="mdi mdi-image mdi-24px secondary mr-2" />
+            Photos
+          </p>
+          <p v-ripple>
+            <span class="mdi mdi-headphones mdi-24px secondary mr-2" />
+            Audio
+          </p>
+          <p v-ripple>
+            <span class="mdi mdi-emoticon mdi-24px secondary mr-2" />
+            Mood
+          </p>
+        </div>
+      </main>
+    </template>
+  </AppFeel>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import { navigationRoutes } from '~/navigation/navigationRoutes'
+import AppFeel from '~/components/global/Layout/AppFeel'
+import LoadingIcon from '~/components/global/LoadingIcon'
 
 export default {
   name: 'AddPost',
+  components: { LoadingIcon, AppFeel },
   middleware: 'isAuthenticated',
 
   data() {
     return {
       navigationRoutes,
       pageTitle: 'Add New Post',
+      isReady: false,
     }
+  },
+
+  computed: {
+    ...mapGetters({
+      user: 'UserManagement/getUser',
+    }),
   },
 
   async mounted() {
@@ -25,6 +74,19 @@ export default {
     await this.$store.dispatch('NavigationState/updateTopNavActiveLink', {
       linkPosition: -1,
     })
+    await this.setupUser()
+
+    this.isReady = true
+  },
+
+  methods: {
+    async setupUser() {
+      const currentUser = await this.$store.getters['UserManagement/getUser']
+      if (!currentUser) {
+        this.loadingProfile = true
+        await this.$store.dispatch('UserManagement/fetchData')
+      }
+    },
   },
 
   head() {
