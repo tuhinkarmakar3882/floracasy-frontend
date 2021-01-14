764af47862b3b8766d507833c6fd95e50647c266
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-14 21:17:30 +0530

    Applying CSS
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Community/Stories/AddStory.vue
+++ pages/Home/Community/Stories/AddStory.vue
@@ -121,3 +121,140 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.camera-container {
+  .camera-button {
+    margin-bottom: 2rem;
+  }
+
+  .camera-box {
+    .camera-shutter {
+      opacity: 0;
+      width: 450px;
+      height: 337.5px;
+      background-color: #fff;
+      position: absolute;
+
+      &.flash {
+        opacity: 1;
+      }
+    }
+  }
+
+  .camera-shoot {
+    margin: 1rem 0;
+
+    button {
+      height: 60px;
+      min-width: auto;
+      width: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      border-radius: 100%;
+
+      img {
+        height: 35px;
+        object-fit: cover;
+      }
+    }
+  }
+
+  .camera-loading {
+    overflow: hidden;
+    height: 100%;
+    position: absolute;
+    width: 100%;
+    min-height: 150px;
+    margin: 3rem 0 0 -1.2rem;
+
+    ul {
+      height: 100%;
+      position: absolute;
+      width: 100%;
+      z-index: 999999;
+      margin: 0;
+    }
+
+    .loader-circle {
+      display: block;
+      height: 14px;
+      margin: 0 auto;
+      top: 50%;
+      left: 100%;
+      transform: translateY(-50%) translateX(-50%);
+      position: absolute;
+      width: 100%;
+      padding: 0;
+
+      li {
+        display: block;
+        float: left;
+        width: 10px;
+        height: 10px;
+        line-height: 10px;
+        padding: 0;
+        position: relative;
+        margin: 0 0 0 4px;
+        background: #999;
+        animation: preload 1s infinite;
+        top: -50%;
+        border-radius: 100%;
+
+        &:nth-child(2) {
+          animation-delay: 0.2s;
+        }
+
+        &:nth-child(3) {
+          animation-delay: 0.4s;
+        }
+      }
+    }
+  }
+
+  #photoTaken {
+    width: 100%;
+    object-fit: contain;
+  }
+}
+
+@keyframes preload {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.4;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.tab-bar {
+  display: grid;
+  text-align: center;
+  grid-template-columns: repeat(3, 1fr);
+  position: sticky;
+  top: (2 * $x-large-unit) - $double-unit;
+  background-color: $nav-bar-bg;
+  box-shadow: $down-only-box-shadow;
+  z-index: 1;
+  transition: all 0.3s ease-in-out;
+
+  * {
+    padding: 0.7rem 0;
+    font-size: 1rem;
+    font-weight: 300;
+  }
+
+  .active-tab {
+    color: $secondary;
+    font-weight: 400;
+    background: $card-background;
+    transition: all 100ms ease-in-out;
+  }
+}
+</style>
