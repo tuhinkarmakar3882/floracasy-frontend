7829fe715302fd2b1bcc8acea1174dc00200fd36
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-24 17:54:47 +0530

    Added AllStory Fetch Hook
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Community/Stories/index.vue
+++ pages/Home/Community/Stories/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="community-post-detail-page">
     {{ pageTitle }}
+    <pre>{{ stories }}</pre>
   </div>
 </template>
 
 <script>
 import { navigationRoutes } from '~/navigation/navigationRoutes'
+import endpoints from '~/api/endpoints'
 
 export default {
   name: 'AllStories',
   middleware: 'isAuthenticated',
+
+  async asyncData({ $axios, from: prevURL }) {
+    const response = await $axios.$get(endpoints.community_service.stories)
+    return { stories: response, prevURL }
+  },
 
   data() {
     return {
