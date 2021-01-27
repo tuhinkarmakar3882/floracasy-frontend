61d87b7e2fdcfeab9e80cf8f46e5d6e325d04d86
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-27 14:23:46 +0530

    Added Ticket Details Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ pages/Home/MoreOptions/HelpAndSupport/Tickets/Details/_identifier.vue
@@ -0,0 +1,41 @@
+<template>
+  <AppFeel
+    :on-back="navigationRoutes.Home.MoreOptions.HelpAndSupport.Tickets.index"
+    class="ticket-details-page"
+  >
+    <template slot="app-bar-title">{{ pageTitle }}</template>
+
+    <template slot="main">
+      <pre>{{ $route.params }}</pre>
+    </template>
+  </AppFeel>
+</template>
+
+<script>
+import AppFeel from '@/components/global/Layout/AppFeel'
+import { navigationRoutes } from '@/navigation/navigationRoutes'
+import endpoints from '@/api/endpoints'
+
+export default {
+  name: 'TicketDetail',
+  components: { AppFeel },
+  middleware: 'isAuthenticated',
+  data() {
+    return {
+      navigationRoutes,
+      pageTitle: 'Ticket Detail',
+      tickets: [],
+      ticketFetchEndpoint: endpoints.help_and_support.fetch,
+    }
+  },
+  mounted() {},
+
+  methods: {},
+
+  head() {
+    return {
+      title: this.pageTitle,
+    }
+  },
+}
+</script>
