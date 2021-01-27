aae82b8f0dbc7ebcac275c16abf34b3d6d310cb4
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-27 13:34:01 +0530

    Wrapped Content
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Community/Story.vue
+++ components/global/Community/Story.vue
@@ -1,23 +1,25 @@
 <template>
-  <div v-ripple class="story">
-    <section class="wrapper mb-4">
-      <img
-        :src="story.user.photoURL"
-        :alt="story.user.photoURL"
-        height="72"
-        width="72"
-      />
-    </section>
+  <div class="story-component">
+    <main v-ripple class="story">
+      <section class="wrapper mb-4">
+        <img
+          :alt="story.user.photoURL"
+          :src="story.user.photoURL"
+          height="72"
+          width="72"
+        />
+      </section>
 
-    <aside>
-      <p>
-        {{
-          story.user.displayName.length > 14
-            ? `${story.user.displayName.substr(0, 14)}...`
-            : story.user.displayName
-        }}
-      </p>
-    </aside>
+      <aside>
+        <p>
+          {{
+            story.user.displayName.length > 14
+              ? `${story.user.displayName.substr(0, 14)}...`
+              : story.user.displayName
+          }}
+        </p>
+      </aside>
+    </main>
   </div>
 </template>
 
@@ -36,56 +38,58 @@ export default {
 <style lang="scss" scoped>
 @import 'assets/all-variables';
 
-.story {
-  display: grid;
-  place-items: center;
-  border-radius: 8px;
-
-  $wrapper-size: 74px;
-  $image-size: 64px;
-
-  .wrapper {
-    text-align: center;
-    position: relative;
-    min-height: $wrapper-size;
-    min-width: $wrapper-size;
+.story-component {
+  .story {
     display: grid;
     place-items: center;
+    border-radius: 8px;
 
-    img {
-      min-height: $image-size;
-      height: $image-size;
-      min-width: $image-size;
-      width: $image-size;
-      object-fit: cover;
-      box-shadow: $default-box-shadow;
-      border-radius: 50%;
-    }
+    $wrapper-size: 74px;
+    $image-size: 64px;
 
-    &::before {
-      content: '';
-      position: absolute;
-      height: $wrapper-size;
-      width: $wrapper-size;
-      top: 0;
-      left: 0;
-      z-index: -1;
-      border-radius: 50%;
-      border: 2px solid $secondary;
-    }
-  }
-
-  aside {
-    align-self: flex-start;
-    width: 100%;
-    height: 4ch;
-    overflow: hidden;
-
-    p {
-      font-weight: 300;
+    .wrapper {
       text-align: center;
-      font-size: 14px;
-      line-height: 1.48;
+      position: relative;
+      min-height: $wrapper-size;
+      min-width: $wrapper-size;
+      display: grid;
+      place-items: center;
+
+      img {
+        min-height: $image-size;
+        height: $image-size;
+        min-width: $image-size;
+        width: $image-size;
+        object-fit: cover;
+        box-shadow: $default-box-shadow;
+        border-radius: 50%;
+      }
+
+      &::before {
+        content: '';
+        position: absolute;
+        height: $wrapper-size;
+        width: $wrapper-size;
+        top: 0;
+        left: 0;
+        z-index: -1;
+        border-radius: 50%;
+        border: 2px solid $secondary;
+      }
+    }
+
+    aside {
+      align-self: flex-start;
+      width: 100%;
+      height: 4ch;
+      overflow: hidden;
+
+      p {
+        font-weight: 300;
+        text-align: center;
+        font-size: 14px;
+        line-height: 1.48;
+      }
     }
   }
 }
