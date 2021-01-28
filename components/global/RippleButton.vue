b13ee4a14c314c654aed7537954b47bc85fc400c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-28 19:19:55 +0530

    Added RippleButton.vue Styles
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/RippleButton.vue
+++ components/global/RippleButton.vue
@@ -1,7 +1,6 @@
 <template>
   <button
     v-ripple="'rgba(255, 255, 255, 0.2)'"
-    :type="buttonType"
     :class="
       disabled || loading
         ? this.$props.classList + ' disabled-btn'
@@ -9,6 +8,7 @@
     "
     :disabled="disabled || loading"
     :loading="loading"
+    :type="buttonType"
     @click="onClick"
   >
     <span v-if="loading" class="loading-animation" />
@@ -46,6 +46,13 @@ export default {
 
 <style lang="scss" scoped>
 @import 'assets/all-variables';
+
+button {
+  min-height: auto;
+  height: auto;
+  width: auto;
+  min-width: auto;
+}
 
 .loading-animation {
   margin-left: auto;
