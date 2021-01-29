abd35a860ee7a8a22e86253fc12aca40f57f8b4b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-01-29 18:04:54 +0530

    Added setupUser() in isAuthenticated.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,7 +1,10 @@
 'use strict'
 
-export default function ({ store, redirect }) {
+import { setupUser } from '~/utils/utility'
+
+export default async function ({ store, redirect }) {
   if (!store.state.authState) {
     return redirect('/Authentication/SignInToContinue')
   }
+  await setupUser(store)
 }
