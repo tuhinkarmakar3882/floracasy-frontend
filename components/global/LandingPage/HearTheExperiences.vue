bedf475e30f983599a204764e42b312f1fd20aed
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-08 19:22:05 +0530

    updated HearTheExperiences.vue Carousel.vue experience
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/LandingPage/HearTheExperiences.vue
+++ components/global/LandingPage/HearTheExperiences.vue
@@ -6,116 +6,12 @@
       topic="Experiences"
     />
 
-    <section class="quote-container">
-      <blockquote>
-        <img
-          alt=""
-          height="84"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          width="84"
-        />
-        <h6 class="mb-0">Arisa Landerson</h6>
-        <p class="vibrant">Student</p>
-        <section class="quote">
-          <span class="mdi mdi-format-quote-open mdi-48px text-left my-0" />
-          <p class="my-0 text-left px-1">
-            <em>
-              I made decisions that I regret, and I took them as learning
-              experiences... I'm human, not perfect, like anybody else.
-            </em>
-          </p>
-          <span class="mdi mdi-format-quote-close mdi-48px text-right my-0" />
-        </section>
-      </blockquote>
-      <blockquote>
-        <img
-          alt=""
-          height="84"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          width="84"
-        />
-        <h6 class="mb-0">Arisa Landerson</h6>
-        <p class="vibrant">Student</p>
-        <section class="quote">
-          <span class="mdi mdi-format-quote-open mdi-48px text-left my-0" />
-          <p class="my-0 text-left px-1">
-            <em>
-              I made decisions that I regret, and I took them as learning
-              experiences... I'm human, not perfect, like anybody else.
-            </em>
-          </p>
-          <span class="mdi mdi-format-quote-close mdi-48px text-right my-0" />
-        </section>
-      </blockquote>
-      <blockquote>
-        <img
-          alt=""
-          height="84"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          width="84"
-        />
-        <h6 class="mb-0">Arisa Landerson</h6>
-        <p class="vibrant">Student</p>
-        <section class="quote">
-          <span class="mdi mdi-format-quote-open mdi-48px text-left my-0" />
-          <p class="my-0 text-left px-1">
-            <em>
-              I made decisions that I regret, and I took them as learning
-              experiences... I'm human, not perfect, like anybody else.
-            </em>
-          </p>
-          <span class="mdi mdi-format-quote-close mdi-48px text-right my-0" />
-        </section>
-      </blockquote>
-      <blockquote>
-        <img
-          alt=""
-          height="84"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          width="84"
-        />
-        <h6 class="mb-0">Arisa Landerson</h6>
-        <p class="vibrant">Student</p>
-        <section class="quote">
-          <span class="mdi mdi-format-quote-open mdi-48px text-left my-0" />
-          <p class="my-0 text-left px-1">
-            <em>
-              I made decisions that I regret, and I took them as learning
-              experiences... I'm human, not perfect, like anybody else.
-            </em>
-          </p>
-          <span class="mdi mdi-format-quote-close mdi-48px text-right my-0" />
-        </section>
-      </blockquote>
-      <blockquote>
-        <img
-          alt=""
-          height="84"
-          src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"
-          width="84"
-        />
-        <h6 class="mb-0">Arisa Landerson</h6>
-        <p class="vibrant">Student</p>
-        <section class="quote">
-          <span class="mdi mdi-format-quote-open mdi-48px text-left my-0" />
-          <p class="my-0 text-left px-1">
-            <em>
-              I made decisions that I regret, and I took them as learning
-              experiences... I'm human, not perfect, like anybody else.
-            </em>
-          </p>
-          <span class="mdi mdi-format-quote-close mdi-48px text-right my-0" />
-        </section>
-      </blockquote>
-    </section>
-
-    <aside class="quotes-counter">
-      <span class="dot active" />
-      <span class="dot" />
-      <span class="dot" />
-      <span class="dot" />
-      <span class="dot" />
-    </aside>
+    <LazyCarousel
+      :carousel-items="quotes"
+      quote-carousel
+      show-navigation-dots
+      show-navigation-arrows
+    />
   </div>
 </template>
 
@@ -124,7 +20,32 @@ export default {
   name: 'HearTheExperiences',
   data() {
     return {
-      points: [],
+      quotes: [
+        {
+          id: 0,
+          name: 'Mr. Tony Stark',
+          designation: 'Iron Man',
+          quote:
+            "I made decisions that I regret, and I took them as learning experiences... I'm human, not perfect, like anybody else.",
+          photoURL: 'https://picsum.photos/250',
+        },
+        {
+          id: 1,
+          name: 'Ms. Black Widow',
+          designation: 'Shield Agent',
+          quote:
+            "I made decisions that I regret, and I took them as learning experiences... I'm human, not perfect, like anybody else.",
+          photoURL: 'https://picsum.photos/251',
+        },
+        {
+          id: 2,
+          name: 'Mr. Steve Rogers',
+          designation: 'First Avenger',
+          quote:
+            "I made decisions that I regret, and I took them as learning experiences... I'm human, not perfect, like anybody else.",
+          photoURL: 'https://picsum.photos/252',
+        },
+      ],
     }
   },
 }
@@ -135,58 +56,5 @@ export default {
 
 .hear-the-experiences-component {
   text-align: center;
-
-  .quotes-counter {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: $micro-unit;
-
-    .dot {
-      display: block;
-      height: $milli-unit;
-      width: $milli-unit;
-      border-radius: 50%;
-      background: $muted;
-
-      &.active {
-        background: $vibrant;
-      }
-    }
-  }
-
-  .quote-container {
-    display: grid;
-    grid-auto-flow: column;
-    overflow: scroll;
-    width: 100%;
-    gap: $medium-unit;
-
-    blockquote {
-      border: none;
-      border-radius: $xx-large-unit;
-      width: 80vw;
-
-      img {
-        border-radius: 50%;
-        box-shadow: $default-box-shadow;
-        height: 2 * $xxx-large-unit;
-        min-height: 2 * $xxx-large-unit;
-        width: 2 * $xxx-large-unit;
-        min-width: 2 * $xxx-large-unit;
-        aspect-ratio: 1;
-        object-fit: cover;
-        margin: auto;
-      }
-
-      span {
-        margin: -$standard-unit;
-      }
-
-      * {
-        display: block;
-      }
-    }
-  }
 }
 </style>
