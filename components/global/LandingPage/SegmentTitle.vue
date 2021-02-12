f87f8a83ef3491b47ad541c6ffc11721dde1297e
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-12 16:04:30 +0530

    Added Icon Support
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- components/global/LandingPage/SegmentTitle.vue
+++ components/global/LandingPage/SegmentTitle.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="top-header">
-    <p v-if="topic" class="topic">{{ topic }}</p>
+    <p v-if="topic" :style="{ color: highlightColor }">
+      <i v-if="leadingIcon" :class="leadingIcon" />
+      <strong>{{ topic }}</strong>
+    </p>
     <h3 v-if="title" class="my-4">{{ title }}</h3>
     <p v-if="leadingParagraph">{{ leadingParagraph }}</p>
   </div>
@@ -10,6 +13,11 @@
 export default {
   name: 'SegmentTitle',
   props: {
+    leadingIcon: {
+      type: String,
+      required: false,
+      default: '',
+    },
     topic: {
       type: String,
       required: false,
@@ -25,17 +33,11 @@ export default {
       required: false,
       default: '',
     },
+    highlightColor: {
+      type: String,
+      required: false,
+      default: '#8FF2E1',
+    },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-@import 'assets/all-variables';
-
-.top-header {
-  .topic {
-    color: $secondary-highlight;
-    font-weight: 700;
-  }
-}
-</style>
