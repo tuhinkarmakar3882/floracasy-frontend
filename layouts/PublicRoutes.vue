4b76b3e19d38380a5af1815feb8cdc9cf1014c58
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-12 16:03:18 +0530

    Added Custom Lazy Loading
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- layouts/PublicRoutes.vue
+++ layouts/PublicRoutes.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <NavigationBar />
+    <LazyNavigationBar />
 
-    <main class="py-4">
+    <main>
       <nuxt />
     </main>
 
-    <Footer />
+    <LazyFooter />
   </div>
 </template>
 
 <script>
-import NavigationBar from '@/components/global/Layout/PublicRoutes/NavigationBar'
-import Footer from '@/components/global/Layout/PublicRoutes/Footer'
-
-export default {
-  components: { Footer, NavigationBar },
-  data: () => {
-    return {}
-  },
-}
+export default {}
 </script>
