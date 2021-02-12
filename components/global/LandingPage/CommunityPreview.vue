53e166b0e870b8987501d635eb1ea94e53324314
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-12 16:05:14 +0530

    Added PLaceholder
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- components/global/LandingPage/CommunityPreview.vue
+++ components/global/LandingPage/CommunityPreview.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="earn-with-us">
-    <pre>CommunityPreview</pre>
+  <div class="community-preview-component">
+    <LazySegmentTitle
+      leading-paragraph="Explore hundreds of integrations for Ghost to speed up your workflow, or build your own
+          custom integrations with our open source developer SDK"
+      title="The more, The merrier"
+      topic="Community"
+      leading-icon="mdi mdi-earth"
+    />
+
+    <button class="secondary-btn my-6" @click="openSignInPage">
+      Connect Easily
+    </button>
   </div>
 </template>
 
 <script>
+import { navigationRoutes } from '~/navigation/navigationRoutes'
+
 export default {
   name: 'CommunityPreview',
+  methods: {
+    async openSignInPage() {
+      await this.$router.push(navigationRoutes.Authentication.SignInToContinue)
+    },
+  },
 }
 </script>
