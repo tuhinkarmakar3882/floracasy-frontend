becb23e574eafd1956760148f277f455666f3133
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-13 14:26:05 +0530

    Added Bare minimum responsiveness to PublicRoutes.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- layouts/PublicRoutes.vue
+++ layouts/PublicRoutes.vue
@@ -2,7 +2,7 @@
   <div>
     <LazyNavigationBar />
 
-    <main>
+    <main class="main-router-content">
       <nuxt />
     </main>
 
@@ -17,11 +17,19 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+@import 'assets/all-variables';
+
 .install-badge {
   position: fixed;
   bottom: 0;
   left: 0;
   width: 100%;
   z-index: 2;
+}
+
+.main-router-content {
+  max-width: $max-width;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
