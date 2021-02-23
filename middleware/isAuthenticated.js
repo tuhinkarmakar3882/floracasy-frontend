7180c92cd9ff7862d4df0db75ed2a2031f2cbbcc
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-23 20:44:42 +0530

    Cleanup COde
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,10 +1,7 @@
 'use strict'
 
-import { setupUser } from '~/utils/utility'
-
 export default async function ({ store, redirect }) {
   if (!store.state.isUserAuthenticated) {
     return redirect('/Authentication/SignInToContinue')
   }
-  await setupUser(store)
 }
