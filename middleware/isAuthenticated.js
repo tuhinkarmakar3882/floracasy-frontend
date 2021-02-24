7e7af2cd3ff7c4b89be6df5f6e92bde14b80bf39
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-24 20:58:48 +0530

    Added This line to prevent error upon state initialization
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,7 +1,10 @@
 'use strict'
 
+import { setupUser } from '~/utils/utility'
+
 export default async function ({ store, redirect }) {
   if (!store.state.isUserAuthenticated) {
     return redirect('/Authentication/SignInToContinue')
   }
+  await setupUser(store)
 }
