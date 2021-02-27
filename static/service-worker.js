238dcf688b1aa1de9b6d8c6741ad1a535e44722f
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-27 16:53:33 +0530

    removed Cached Response from service-worker.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- static/service-worker.js
+++ static/service-worker.js
@@ -30,32 +30,32 @@ self.addEventListener('activate', (event) => {
 })
 
 // Serve from Cache
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch event for ', event.request.url)
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then((response) => {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache')
-          return response
-        }
-        console.log('Network request for ', event.request.url)
-        return fetch(event.request).then((response) => {
-          // TODO 5 - Respond with custom 404 page
-          return caches.open(staticCacheName).then((cache) => {
-            event.request.method === 'GET' &&
-              event.request.destination !== 'image' &&
-              cache.put(event.request.url, response.clone())
-            return response
-          })
-        })
-      })
-      .catch(() => {
-        // TODO 6 - Respond with custom offline page
-      })
-  )
-})
+// self.addEventListener('fetch', (event) => {
+//   console.log('Fetch event for ', event.request.url)
+//   event.respondWith(
+//     caches
+//       .match(event.request)
+//       .then((response) => {
+//         if (response) {
+//           console.log('Found ', event.request.url, ' in cache')
+//           return response
+//         }
+//         console.log('Network request for ', event.request.url)
+//         return fetch(event.request).then((response) => {
+//           // TODO 5 - Respond with custom 404 page
+//           return caches.open(staticCacheName).then((cache) => {
+//             event.request.method === 'GET' &&
+//               event.request.destination !== 'image' &&
+//               cache.put(event.request.url, response.clone())
+//             return response
+//           })
+//         })
+//       })
+//       .catch(() => {
+//         // TODO 6 - Respond with custom offline page
+//       })
+//   )
+// })
 
 //  Workbox
 // const revision = Date.now()
