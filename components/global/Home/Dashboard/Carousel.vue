314bc1399596b3b06fdf5b6228a89a9e2e7ea7d8
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-27 20:15:56 +0530

    Image => contain
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Home/Dashboard/Carousel.vue
+++ components/global/Home/Dashboard/Carousel.vue
@@ -150,7 +150,7 @@ export default {
         width: 2 * $xxx-large-unit;
         min-width: 2 * $xxx-large-unit;
         aspect-ratio: 1;
-        object-fit: scale-down;
+        object-fit: contain;
         margin: auto;
       }
 
