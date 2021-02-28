53897ed174d50d56b85f06804b6dfcb5f18efeaa
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-28 21:00:24 +0530

    Added Lazy Loading for Blogs
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Home/Dashboard/InfiniteScrollingBlogLists.vue
+++ components/global/Home/Dashboard/InfiniteScrollingBlogLists.vue
@@ -2,7 +2,7 @@
   <div class="mb-6 scrollable-blog-list">
     <section v-if="blogs">
       <article v-for="blog in blogs" :key="blog.id">
-        <BlogPost :blog="blog" class="pb-0 pt-8" />
+        <LazyBlogPost :blog="blog" class="pb-0 pt-8" />
       </article>
     </section>
 
@@ -30,11 +30,6 @@ import { processLink } from '~/utils/utility'
 
 export default {
   name: 'InfiniteScrollingBlogLists',
-
-  components: {
-    BlogPost: () => import('@/components/global/BlogPost'),
-    LoadingIcon: () => import('@/components/global/LoadingIcon'),
-  },
 
   props: {
     category: {
