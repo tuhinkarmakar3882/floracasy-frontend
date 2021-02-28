7852b4919c71e9f316600fbbdbb3b41c53f32ced
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-02-28 18:38:02 +0530

    Added ImmersiveView.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/global/Community/ImmersiveView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="immersive-view-component">
+    <h1>Hello ImmersiveView</h1>
+    <slot name="close-button"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImmersiveView',
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.immersive-view-component {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: black;
+  z-index: $bring-to-front;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
