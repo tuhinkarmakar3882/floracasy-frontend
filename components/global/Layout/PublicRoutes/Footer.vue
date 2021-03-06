4d4886afcfa13132bb49a88b247748cf5d49191d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-06 15:53:28 +0530

    Added Bare Bones of Footer.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Layout/PublicRoutes/Footer.vue
+++ components/global/Layout/PublicRoutes/Footer.vue
@@ -1,19 +1,62 @@
 <template>
-  <footer class="full-width">
-    <section class="py-8"></section>
+  <footer class="page-footer">
+    <h5 class="text-center my-2">Floracasy</h5>
+
+    <section class="tag-lines">
+      <p class="option">Blog</p>
+      <p class="option">Community</p>
+      <p class="option">Earning</p>
+      <p class="option">Socialize</p>
+    </section>
+
+    <hr class="faded-divider" />
+
+    <div class="communication">
+      <section class="page-links">
+        <p class="text-center">Quick Links</p>
+        <nuxt-link :to="navigationRoutes.Home.MoreOptions.About">
+          About
+        </nuxt-link>
+        <nuxt-link
+          :to="navigationRoutes.Home.MoreOptions.HelpAndSupport.Feedback"
+        >
+          Support
+        </nuxt-link>
+      </section>
+
+      <section class="social-links">
+        <p class="text-center">Let's Connect</p>
+        <aside>
+          <i class="mdi mdi-linkedin mdi-48px" />
+          <i class="mdi mdi-gmail mdi-48px" />
+        </aside>
+      </section>
+    </div>
 
     <section class="text-center">
-      <a href="#">Back to Top</a>
+      <button class="secondary-outlined-btn my-4">Sign up or Log in</button>
+
+      <br />
+      <a class="no-underline my-4" href="#">
+        <i class="mdi mdi-triangle mdi-18px" />
+        Back to Top
+      </a>
+      <br />
+      <p class="my-2">
+        <small>&copy; Floracasy.com {{ new Date().getFullYear() }}</small>
+      </p>
     </section>
   </footer>
 </template>
 
-<script lang="js">
+<script>
+import { navigationRoutes } from '~/navigation/navigationRoutes'
 
 export default {
   name: 'Footer',
-  data: () => {
+  data() {
     return {
+      navigationRoutes,
       points: [
         'Enjoy Affordable Pro Perks1',
         'Enjoy Affordable Pro Perks2',
@@ -37,16 +80,38 @@ export default {
 <style lang="scss" scoped>
 @import 'assets/all-variables';
 
-footer {
+footer.page-footer {
   background-color: $footer-background;
   width: 100%;
 
-  .contact-us {
-    ul {
-      list-style: none;
-      display: grid;
-      grid-template-columns: repeat(5, 1fr);
-      grid-column-gap: $nano-unit;
+  .tag-lines {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    align-content: center;
+  }
+
+  .communication {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+
+    .page-links {
+      a {
+        display: block;
+      }
+    }
+
+    .social-links {
+      display: flex;
+      flex-direction: column;
+
+      aside {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+      }
     }
   }
 }
