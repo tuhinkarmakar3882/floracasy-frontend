afb7378e15948c23d7203639693ccd1da4cd0deb
Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>
2021-03-08 14:24:35 +0530

    Placed Firebase things into one Place
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- plugins/fcm.js
+++ plugins/fcm.js
@@ -1,18 +1,18 @@
-import firebase from 'firebase/app'
-import 'firebase/messaging'
-import { showUITip } from '~/utils/utility'
-
-export const firebaseCloudMessaging = firebase.messaging()
-
-export default function ({ store }) {
-  firebaseCloudMessaging.onMessage(async function (payload) {
-    const notification = payload.notification
-    await showUITip(
-      store,
-      notification.title,
-      notification.type || 'info',
-      notification.dismissible || true,
-      notification.timeout || 2500
-    )
-  })
-}
+// import firebase from 'firebase/app'
+// import 'firebase/messaging'
+// import { showUITip } from '~/utils/utility'
+//
+// export const firebaseCloudMessaging = firebase.messaging()
+//
+// export default function ({ store }) {
+//   firebaseCloudMessaging.onMessage(async function (payload) {
+//     const notification = payload.notification
+//     await showUITip(
+//       store,
+//       notification.title,
+//       notification.type || 'info',
+//       notification.dismissible || true,
+//       notification.timeout || 2500
+//     )
+//   })
+// }
