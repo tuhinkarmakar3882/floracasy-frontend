5f4e93b81386fc31cf56a4c53f3a61cc84157e36
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-08 20:19:07 +0530

    Extracted QuestionsCard
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/FAQ/index.vue
+++ pages/Home/MoreOptions/FAQ/index.vue
@@ -76,15 +76,11 @@
 </template>
 
 <script>
-import AppFeel from '@/components/global/Layout/AppFeel'
 import { navigationRoutes } from '@/navigation/navigationRoutes'
-import QuestionCard from '~/pages/Home/MoreOptions/FAQ/QuestionCard'
-import CustomListView from '~/components/global/Layout/CustomListView'
 
 export default {
   name: 'FAQ',
   middleware: 'isAuthenticated',
-  components: { CustomListView, QuestionCard, AppFeel },
   data() {
     return {
       drawerIsOpened: false,
