f27ddfc6715185a3c5ad6b44a37f4dbbb5f47c9d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-14 22:00:48 +0530

    Added Ad Component
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/global/Advertisement.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- DashboardAdvertisement#1 -->
+  <ins
+    class="adsbygoogle"
+    data-ad-client="ca-pub-9863542606738743"
+    data-ad-format="auto"
+    data-ad-slot="8879096758"
+    data-full-width-responsive="true"
+    style="display: block"
+  ></ins>
+  <script></script>
+</template>
+
+<script>
+export default {
+  name: 'Advertisement',
+  mounted() {
+    ;(window.adsbygoogle || []).push({})
+  },
+}
+</script>
+
+<style scoped></style>
