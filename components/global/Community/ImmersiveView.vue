f7dc1fe0dc508b76139f29be474b1bf0868dd4ac
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-15 00:03:49 +0530

    Added Ads in ImmersiveView.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/Community/ImmersiveView.vue
+++ components/global/Community/ImmersiveView.vue
@@ -94,7 +94,7 @@
       <FallBackLoader v-if="loadingStories" class="my-4" />
 
       <section
-        v-for="item in allStories"
+        v-for="(item, index) in allStories"
         :key="item.identifier"
         :style="item.style"
         class="scroll-list"
@@ -110,6 +110,8 @@
         <div v-if="item.storyType === 'audio'" style="width: 100%">
           <AudioPlayer :audio-source="item.audio" />
         </div>
+
+        <DisplayAd v-if="index % 3 === 0" />
       </section>
 
       <aside class="controls">
