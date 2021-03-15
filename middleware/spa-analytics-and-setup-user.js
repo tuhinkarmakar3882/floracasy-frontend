e6cc36e4aed4d6585bc5c7d7f5f00335e443368e
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-15 10:21:30 +0530

    updated Analytics Config
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- middleware/spa-analytics-and-setup-user.js
+++ middleware/spa-analytics-and-setup-user.js
@@ -1,9 +1,10 @@
 import { setupUser } from '~/utils/utility'
-import { analyticsID } from '~/environmentVariables'
+import { firebaseConfig } from '~/environmentVariables'
 
 export default async ({ app, store }) => {
   app.router.afterEach(async (to, _) => {
-    process.client && window.gtag('config', analyticsID, { page_path: to })
+    process.client &&
+      window.gtag('config', firebaseConfig.measurementId, { page_path: to })
     if (store.state.isUserAuthenticated) {
       await setupUser(store)
     }
