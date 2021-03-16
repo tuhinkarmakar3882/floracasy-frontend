29753fef94a3d16394ab251e6af9b2b8c5f07f99
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-16 21:57:51 +0530

    Updated About us Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/About/index.vue
+++ pages/Home/MoreOptions/About/index.vue
@@ -9,10 +9,15 @@
 
     <template slot="main">
       <main class="my-4">
-        <section class="banner">
-          <LazyLogo class="mr-4" />
-          <h4>Floracasy</h4>
-        </section>
+        <div class="top-area mb-4">
+          <section class="logo">
+            <LazyLogo class="mr-4" />
+            <h4>Floracasy</h4>
+          </section>
+          <aside class="text-center">
+            <small> Version: {{ versionNumber }} </small>
+          </aside>
+        </div>
 
         <section class="tab-bar">
           <p
@@ -28,45 +33,65 @@
 
         <div ref="tabNavigation"></div>
 
-        <div v-if="activeTab === 0" class="the-pro-zone px-4">
-          <section
-            v-for="(item, index) in content[activeTab].data"
-            :key="`${activeTab}-${index}`"
-          >
-            <h4 class="heading-title">{{ item.title }}</h4>
-            <p>{{ item.body }}</p>
-          </section>
-        </div>
+        <section v-if="activeTab === 0" class="the-pro-zone px-4">
+          <aside class="my-8 py-8 text-center">
+            <h4>Coming soon</h4>
+            <p>Watch this space for more information...</p>
+          </aside>
+        </section>
 
-        <div v-else-if="activeTab === 1" class="the-story px-4">
-          <section
-            v-for="(item, index) in content[activeTab].data"
-            :key="`${activeTab}-${index}`"
-          >
-            <h4 class="heading-title">{{ item.title }}</h4>
-            <p>{{ item.body }}</p>
-          </section>
-        </div>
+        <section v-else-if="activeTab === 1" class="the-vision px-4">
+          <article class="my-4">
+            <h4 class="heading-title mt-4 mb-6">
+              The World can be a Better Place
+            </h4>
+            <p class="my-4">
+              Though, the pandemic has taken away a lot of jobs, We understand
+              that, each one of us is a bearer of a potential amount of
+              knowledge. In this platform, we encourage you to come & share that
+              knowledge to the world, and get rewarded for that.
+            </p>
+            <p class="my-4">
+              With proper knowledge, we believe that, it can make a great
+              positive impact on the world.
+            </p>
+          </article>
 
-        <div v-else-if="activeTab === 2" class="the-vision px-4">
-          <section
-            v-for="(item, index) in content[activeTab].data"
-            :key="`${activeTab}-${index}`"
-          >
-            <h4 class="heading-title">{{ item.title }}</h4>
-            <p>{{ item.body }}</p>
-          </section>
-        </div>
+          <article class="my-4">
+            <h4 class="heading-title mt-4 mb-6">The Knowledge is Valuable</h4>
+            <p class="my-4">
+              And, yet many people still don't quite have enough access to it.
+              Here, we want to bridge the gap with just a little help from you.
+            </p>
+          </article>
+        </section>
+
+        <section v-else-if="activeTab === 2" class="the-story px-4">
+          <article class="my-4">
+            <h4 class="heading-title mt-4 mb-6">Who are we?</h4>
+            <p class="my-4">
+              We are a duo of self motivated enthusiasts (One Developer & One
+              UX-Designer), doing our best to do our part for a platform where
+              people can communicate, collaborate & socialize their knowledge &
+              create great positive impact.
+            </p>
+            <p class="my-4">
+              We saw a lot of people losing jobs & some of them wanted to switch
+              careers. But, sometimes they don't really get that proper
+              resources; sometimes they don't even have access to that. We want
+              to bridge the gap & open up a whole platform full of opportunities
+              :)
+            </p>
+          </article>
+        </section>
       </main>
-
-      <LazyFooter class="px-4 my-0 py-6" />
     </template>
   </AppFeel>
 </template>
 
 <script>
 import { navigationRoutes } from '@/navigation/navigationRoutes'
-
+import * as packageJson from '@/package.json'
 export default {
   name: 'About',
 
@@ -76,55 +101,12 @@ export default {
 
   data() {
     return {
+      versionNumber: packageJson.version,
       prevURL: null,
       activeTab: 1,
       pageTitle: 'About us',
       navigationRoutes,
-      tabs: ['Pro Zone', 'Our Story', 'The Vision'],
-      content: [
-        {
-          data: [
-            {
-              title: 'Why Premium?',
-              body:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid at delectus distinctio dolor eligendi ipsum laboriosam, laborum magnam mollitia obcaecati sequi, soluta temporibus. Aperiam cum deleniti eveniet nam reprehenderit.',
-            },
-          ],
-        },
-        {
-          data: [
-            {
-              title: 'We try to deliver more knowledge',
-              body:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid at delectus distinctio dolor eligendi ipsum laboriosam, laborum magnam mollitia obcaecati sequi, soluta temporibus. Aperiam cum deleniti eveniet nam reprehenderit.',
-            },
-            {
-              title: 'How it all started',
-              body:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid at delectus distinctio dolor eligendi ipsum laboriosam, laborum magnam mollitia obcaecati sequi, soluta temporibus. Aperiam cum deleniti eveniet nam reprehenderit.',
-            },
-            {
-              title: 'The Problem',
-              body:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid at delectus distinctio dolor eligendi ipsum laboriosam, laborum magnam mollitia obcaecati sequi, soluta temporibus. Aperiam cum deleniti eveniet nam reprehenderit.',
-            },
-            {
-              title: 'The Solution',
-              body:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid at delectus distinctio dolor eligendi ipsum laboriosam, laborum magnam mollitia obcaecati sequi, soluta temporibus. Aperiam cum deleniti eveniet nam reprehenderit.',
-            },
-          ],
-        },
-        {
-          data: [
-            {
-              title: 'Global Reach to Readers & Writers',
-              body:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid at delectus distinctio dolor eligendi ipsum laboriosam, laborum magnam mollitia obcaecati sequi, soluta temporibus. Aperiam cum deleniti eveniet nam reprehenderit.',
-            },
-          ],
-        },
-      ],
+      tabs: ['Pro Zone', 'The Vision', 'Our Story'],
     }
   },
   mounted() {},
@@ -155,13 +137,15 @@ $blog-border-radius: 20px;
   transition: all 0.5s ease-in-out;
 
   main {
-    .banner {
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    .top-area {
+      .logo {
+        display: flex;
+        align-items: center;
+        justify-content: center;
 
-      h4 {
-        margin: $standard-unit 0;
+        h4 {
+          margin: $standard-unit 0;
+        }
       }
     }
 
