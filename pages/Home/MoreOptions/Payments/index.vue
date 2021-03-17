71f60009ac687055c879b0c247ee10c88d5adc4b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-17 09:21:10 +0530

    Removed Not Polished Content
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/Payments/index.vue
+++ pages/Home/MoreOptions/Payments/index.vue
@@ -34,92 +34,105 @@
         </aside>
       </section>
 
-      <h4 class="px-4">Now Earn Floracoin & Get Exciting Offers</h4>
-      <hr class="faded-divider" />
+      <h4 class="heading-title my-6 px-4">Tips to Improve Your Earnings</h4>
 
-      <section class="question-answer px-4">
-        <h6>What is Floracoin?</h6>
-        <p>
-          As you all know you can earn money by writing blogs .Here we represent
-          your earning &many more statistics against your blogs.
-        </p>
-      </section>
-
-      <section class="question-answer text-right px-4">
-        <h6>Purpose of Floracoins?</h6>
-        <p>
-          Floracoin Mainly a digital currency for you which is only valid on
-          Floracasy . You can use this Coins for going Premium. You can also get
-          more interesting stuff access buy spending floracoins.
-        </p>
-      </section>
-
-      <InFeedAd class="my-4" use-small-ads />
-
-      <section class="question-answer px-4">
-        <h4>How To Earn Floracoin?</h4>
-        <KeyPoint
-          class="my-4"
+      <section class="py-4 px-4">
+        <LazyKeyPoint
+          v-for="(point, index) in tipsToImprove"
+          :key="index"
+          :point="point"
           :tick-size="20"
-          point="When You Share Floracasy & someone join with Your reference You Can Get 50 Floracoin"
-          tick-color="#65db65"
-        />
-        <KeyPoint
           class="my-4"
-          :tick-size="20"
-          point="On each Blog That Written By You You May Get 15 Floracoin"
-          tick-color="#65db65"
-        />
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="When you get your first threshold money you can get 10 Floracoin"
-          tick-color="#65db65"
+          tick-color="#6DD0BF"
         />
       </section>
 
-      <hr class="faded-divider" />
+      <!--      <h4 class="px-4">Now Earn Floracoin & Get Exciting Offers</h4>-->
+      <!--      <hr class="faded-divider" />-->
+      <!---->
+      <!--      <section class="question-answer px-4">-->
+      <!--        <h6>What is Floracoin?</h6>-->
+      <!--        <p>-->
+      <!--          As you all know you can earn money by writing blogs .Here we represent-->
+      <!--          your earning &many more statistics against your blogs.-->
+      <!--        </p>-->
+      <!--      </section>-->
+      <!---->
+      <!--      <section class="question-answer text-right px-4">-->
+      <!--        <h6>Purpose of Floracoins?</h6>-->
+      <!--        <p>-->
+      <!--          Floracoin Mainly a digital currency for you which is only valid on-->
+      <!--          Floracasy . You can use this Coins for going Premium. You can also get-->
+      <!--          more interesting stuff access buy spending floracoins.-->
+      <!--        </p>-->
+      <!--      </section>-->
+      <!---->
+      <!--      <InFeedAd class="my-4" use-small-ads />-->
+      <!---->
+      <!--      <section class="question-answer px-4">-->
+      <!--        <h4>How To Earn Floracoin?</h4>-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="When You Share Floracasy & someone join with Your reference You Can Get 50 Floracoin"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="On each Blog That Written By You You May Get 15 Floracoin"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="When you get your first threshold money you can get 10 Floracoin"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--      </section>-->
+      <!---->
+      <!--      <hr class="faded-divider" />-->
+      <!---->
+      <!--      <section class="premium-perks px-4">-->
+      <!--        <h4>Get Ultimate Exclusive Access</h4>-->
 
-      <section class="premium-perks px-4">
-        <h4>Get Ultimate Exclusive Access</h4>
-
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="Get Add Free Experience"
-          tick-color="#65db65"
-        />
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="Get Audio blogs"
-          tick-color="#65db65"
-        />
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="Ultimate Use Of Our Messaging Services"
-          tick-color="#65db65"
-        />
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="Priority 24×7 Contact Support"
-          tick-color="#65db65"
-        />
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="Get More User Friendly Interface"
-          tick-color="#65db65"
-        />
-        <KeyPoint
-          class="my-4"
-          :tick-size="20"
-          point="Get Free 50 flora coin for Net Purchase"
-          tick-color="#65db65"
-        />
-      </section>
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="Get Add Free Experience"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="Get Audio blogs"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="Ultimate Use Of Our Messaging Services"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="Priority 24×7 Contact Support"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="Get More User Friendly Interface"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--        <KeyPoint-->
+      <!--          class="my-4"-->
+      <!--          :tick-size="20"-->
+      <!--          point="Get Free 50 flora coin for Net Purchase"-->
+      <!--          tick-color="#65db65"-->
+      <!--        />-->
+      <!--      </section>-->
     </template>
   </AppFeel>
 </template>
@@ -142,6 +155,12 @@ export default {
       navigationRoutes,
       prevURL: null,
       pageTitle: 'Payments',
+      tipsToImprove: [
+        'Try to be a Consistent Blog Writer on the Platform',
+        'Great content, keeps your readers engaged',
+        'Try engaging with your community via our Social Section',
+        'Remember, Consistency is the key to be Successful',
+      ],
       points: [
         'Get Detailed Insights',
         'Premium Interface',
