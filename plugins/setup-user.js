6cd559863c6cd2d3051a2f3ab662cd36f40217c9
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-24 11:32:30 +0530

    Simplified setup-user.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- plugins/setup-user.js
+++ plugins/setup-user.js
@@ -1,6 +1,6 @@
 import { setupUser } from '~/utils/utility'
 
-export default async ({ app, store }) => {
+export default ({ app, store }) => {
   app.router.afterEach(async (__, _) => {
     store.state.isUserAuthenticated && (await setupUser(store))
   })
