54ac09e25b5f8cad69003431c0facc813a505a7c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-24 15:39:31 +0530

    Cleaned Code & connected UserTimeLine to Account/overview
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Account/Overview/_id.vue
+++ pages/Home/Account/Overview/_id.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="py-6 details-page">
-    <FallBackLoader v-if="loadingProfile" class="text-center" />
+    <FallBackLoader v-if="loadingProfile" />
 
     <LoadingError
       v-else-if="loadingError"
@@ -51,106 +51,7 @@
 
       <hr class="faded-divider" />
 
-      <section class="timeline-wrapper">
-        <h4 class="heading-title mt-0 mb-8 pb-4">The Timeline</h4>
-
-        <div ref="tabNavigation"></div>
-
-        <nav class="tab-bar">
-          <p
-            v-ripple
-            :class="tabNumber === 0 && 'active-tab'"
-            @click="changeTab(0)"
-          >
-            <span class="mdi mdi-newspaper" />
-            Blogs
-          </p>
-          <p
-            v-ripple
-            :class="tabNumber === 1 && 'active-tab'"
-            @click="changeTab(1)"
-          >
-            <span class="mdi mdi-earth" />
-            Posts
-          </p>
-        </nav>
-
-        <main class="timeline pt-4">
-          <section v-if="tabNumber === 0">
-            <article
-              v-for="(blog, index) in recentBlogs"
-              :key="blog.identifier"
-            >
-              <LazyBlogPost :blog="blog" class="activity pt-4" />
-
-              <InFeedAd v-if="index % 2 === 0" use-small-ads />
-            </article>
-
-            <footer v-if="!loadingError">
-              <client-only>
-                <infinite-loading
-                  key="infinite-blog-fetch"
-                  @infinite="loadBlogs"
-                >
-                  <template slot="spinner">
-                    <LoadingIcon class="mt-4 mb-8" />
-                    <p>Getting your blogs...</p>
-                  </template>
-
-                  <template slot="error">
-                    <p class="danger-light my-6">Network Error</p>
-                  </template>
-
-                  <template slot="no-more">
-                    <div />
-                  </template>
-
-                  <template slot="no-results">
-                    <p class="my-5">It's Lonely Here...</p>
-                  </template>
-                </infinite-loading>
-              </client-only>
-            </footer>
-          </section>
-
-          <section v-if="tabNumber === 1">
-            <article
-              v-for="(post, index) in recentPosts"
-              :key="post.identifier"
-            >
-              <CommunityPost :post="post" class="activity py-8" />
-
-              <InFeedAd v-if="index % 2 === 0" use-small-ads />
-            </article>
-
-            <footer v-if="!loadingError">
-              <client-only>
-                <infinite-loading
-                  key="infinite-posts-fetch"
-                  @infinite="loadPosts"
-                >
-                  <template slot="spinner">
-                    <LoadingIcon class="mt-4 mb-8" />
-                    <p>Getting your posts...</p>
-                  </template>
-
-                  <template slot="error">
-                    <p class="danger-light my-6">Network Error</p>
-                  </template>
-
-                  <template slot="no-more">
-                    <div />
-                  </template>
-
-                  <template slot="no-results">
-                    <p class="my-5">It's Lonely Here...</p>
-                  </template>
-                </infinite-loading>
-              </client-only>
-            </footer>
-          </section>
-        </main>
-      </section>
+      <UserTimeline :user-uid="$route.params.id" />
     </main>
   </div>
 </template>
@@ -159,7 +60,7 @@
 import { mapGetters } from 'vuex'
 import endpoints from '~/api/endpoints'
 import { navigationRoutes } from '~/navigation/navigationRoutes'
-import { getRelativeTime, processLink, showUITip } from '~/utils/utility'
+import { getRelativeTime, showUITip } from '~/utils/utility'
 import { useMessageService } from '~/environmentVariables'
 
 export default {
@@ -168,20 +69,12 @@ export default {
   middleware: 'isAuthenticated',
   data() {
     return {
-      tabNumber: 0,
-
       useMessageService,
       navigationRoutes,
       pageTitle: 'Profile Details',
 
       statisticsItem: null,
       otherUser: null,
-
-      getBlogsByUserUIDEndpoint: endpoints.blog.getBlogsByUid,
-      recentBlogs: [],
-
-      getPostsByUserUIDEndpoint: endpoints.community_service.posts.getByUserUID,
-      recentPosts: [],
 
       followOrUnfollowLoading: false,
       followOrUnfollowWorking: false,
@@ -240,50 +133,6 @@ export default {
       }
     },
 
-    async loadBlogs($state) {
-      if (!this.getBlogsByUserUIDEndpoint) {
-        $state.complete()
-        return
-      }
-      try {
-        const { results, next } = await this.$axios.$get(
-          this.getBlogsByUserUIDEndpoint,
-          { params: { uid: this.$route.params.id } }
-        )
-        if (results.length) {
-          this.getBlogsByUserUIDEndpoint = processLink(next)
-          this.recentBlogs.push(...results)
-          $state.loaded()
-        } else {
-          $state.complete()
-        }
-      } catch (e) {
-        $state.complete()
-      }
-    },
-
-    async loadPosts($state) {
-      if (!this.getPostsByUserUIDEndpoint) {
-        $state.complete()
-        return
-      }
-      try {
-        const { results, next } = await this.$axios.$get(
-          this.getPostsByUserUIDEndpoint,
-          { params: { uid: this.$route.params.id } }
-        )
-        if (results.length) {
-          this.getPostsByUserUIDEndpoint = processLink(next)
-          this.recentPosts.push(...results)
-          $state.loaded()
-        } else {
-          $state.complete()
-        }
-      } catch (e) {
-        $state.complete()
-      }
-    },
-
     async initializeChatThread(receiverData) {
       this.messageLoading = true
       this.messageWorking = true
@@ -330,11 +179,6 @@ export default {
       this.followOrUnfollowLoading = false
       this.followOrUnfollowWorking = false
     },
-
-    changeTab(newTabNumber) {
-      this.tabNumber = newTabNumber
-      this.$refs.tabNavigation.scrollIntoView()
-    },
   },
 
   head() {
@@ -371,37 +215,6 @@ export default {
       align-items: center;
       padding: 1rem;
       gap: 1rem;
-    }
-  }
-
-  .tab-bar {
-    display: grid;
-    text-align: center;
-    grid-template-columns: repeat(2, 1fr);
-    position: sticky;
-    top: 56px;
-    background-color: $nav-bar-bg;
-    box-shadow: $down-only-box-shadow;
-    z-index: 1;
-    transition: all 0.3s ease-in-out;
-
-    * {
-      padding: 0.7rem 0;
-      font-size: 1rem;
-      font-weight: 300;
-    }
-
-    .active-tab {
-      color: $white;
-      font-weight: 400;
-      background: $active-gradient;
-      transition: all 0.1s ease-in-out;
-    }
-  }
-
-  .activity {
-    &:nth-child(even) {
-      background: $segment-background;
     }
   }
 }
