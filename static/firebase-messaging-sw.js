b71050643d3b28b09b5e8a1c72fbc5fd746bf7c1
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-24 11:31:20 +0530

    Updated firebase-messaging-sw.js
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,6 +1,9 @@
 self.addEventListener('notificationclick', function (event) {
-  // event.notification.data.FCM_MSG.data.click_action ||
-  self.clients.openWindow('https://floracasy.com/Home/Notifications', '_blank')
+  self.clients.openWindow(
+    event?.notification?.data?.FCM_MSG?.data?.click_action ||
+      'https://floracasy.com/Home/Notifications',
+    '_blank'
+  )
   event.notification.close()
 })
 
