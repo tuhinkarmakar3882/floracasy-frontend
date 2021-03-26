034c2255ecfcd288e38e824ae23461a92098dcd4
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-26 21:26:13 +0530

    Added FollowSuggestions.vue Component
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/global/Community/FollowSuggestions.vue
@@ -0,0 +1,89 @@
+<template>
+  <section v-if="suggestedPeople" class="people-you-may-follow">
+    <h6 class="px-4">Explore People</h6>
+
+    <main class="container py-4 px-4">
+      <FollowCard
+        v-for="people in suggestedPeople"
+        :key="`suggested-people-${people.userUID}`"
+        :userdata="people"
+        class="px-4 py-4 search-result"
+      />
+    </main>
+
+    <footer class="text-center">
+      <nuxt-link
+        v-ripple
+        :to="navigationRoutes.Home.Community.Explore"
+        class="no-underline text-center py-4"
+      >
+        View All <i class="mdi mdi-play" />
+      </nuxt-link>
+    </footer>
+  </section>
+</template>
+
+<script>
+import endpoints from '~/api/endpoints'
+import { navigationRoutes } from '~/navigation/navigationRoutes'
+
+export default {
+  name: 'FollowSuggestions',
+  data() {
+    return {
+      navigationRoutes,
+      suggestedPeople: [],
+    }
+  },
+  async mounted() {
+    await this.getSuggestedPeople()
+  },
+  methods: {
+    async getSuggestedPeople() {
+      try {
+        this.suggestedPeople = await this.$axios
+          .$get(endpoints.follow_system.getSuggestions)
+          .then((res) => res.results)
+      } catch (e) {
+        // console.log(e)
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.people-you-may-follow {
+  h6 {
+    position: relative;
+
+    &::after {
+      content: '';
+      position: absolute;
+      height: $double-unit;
+      width: 2 * $xx-large-unit;
+      background: $primary-light;
+      bottom: -$milli-unit;
+      left: $standard-unit;
+    }
+  }
+
+  main.container {
+    display: grid;
+    grid-auto-flow: column;
+    grid-gap: 16px;
+    overflow: scroll;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+  footer {
+    a {
+      display: block;
+    }
+  }
+}
+</style>
