43232fdf2e48f28c95f82ff93566dd99d38ab556
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-26 21:27:38 +0530

    Cleaned RippleButton.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/global/RippleButton.vue
+++ components/global/RippleButton.vue
@@ -1,17 +1,13 @@
 <template>
   <button
     v-ripple="'rgba(255, 255, 255, 0.2)'"
-    :class="
-      disabled || loading
-        ? this.$props.classList + ' disabled-btn'
-        : this.$props.classList
-    "
+    :class="[this.$props.classList, (disabled || loading) && ' disabled-btn']"
     :disabled="disabled || loading"
     :loading="loading"
     :type="buttonType"
     @click="onClick"
   >
-    <span v-if="loading" class="loading-animation" />
+    <i v-if="loading" class="mdi mdi-loading mdi-spin mdi-24px vibrant" />
     <slot v-else>Button</slot>
   </button>
 </template>
@@ -43,28 +39,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss" scoped>
-@import 'assets/all-variables';
-
-.loading-animation {
-  margin-left: auto;
-  margin-right: auto;
-  display: block;
-  height: $x-large-unit !important;
-  width: $x-large-unit !important;
-  border: $nano-unit solid darken($muted, 10%);
-  border-top: $nano-unit solid $secondary-matte;
-  border-radius: 100%;
-  animation: spin 1.5s linear infinite;
-
-  @keyframes spin {
-    0% {
-      transform: rotate(0);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-}
-</style>
