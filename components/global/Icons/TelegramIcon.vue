2f299d6ac65e0a7daa11f13e701a77cf68016672
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-03-28 15:50:12 +0530

    Added Icons
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/global/Icons/TelegramIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    class=""
+    height="40"
+    style="enable-background: new 0 0 512 512"
+    viewBox="0 0 512 512"
+    width="40"
+    x="0px"
+    xml:space="preserve"
+    y="0px"
+  >
+    <circle cx="255.997" cy="256" r="255.997" style="fill: #41b4e6" />
+    <path
+      d="M512,256.003c0-6.238-0.235-12.419-0.673-18.546L405.228,131.36L106.772,248.759l114.191,114.192
+                        l1.498,5.392l1.939-1.955l0.008,0.008l-1.947,1.947L348.778,494.66C444.298,457.5,512,364.663,512,256.003z"
+      style="fill: #0091c8"
+    />
+    <polygon
+      points="231.138,293.3 346.829,380.647 405.228,131.36 106.771,248.759 197.588,278.84
+                        363.331,167.664 "
+      style="fill: #fff"
+    />
+    <polygon
+      points="197.588,278.84 222.461,368.344 231.138,293.3 363.331,167.664 "
+      style="fill: #d2d2d7"
+    />
+    <polygon
+      points="268.738,321.688 222.461,368.344 231.138,293.3 "
+      style="fill: #b9b9be"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'TelegramIcon',
+}
+</script>
