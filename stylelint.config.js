a825dc6728552684a95b73f2610f105c712a0fca
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-04-09 11:55:58 +0530

    Updated Namespace
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   rules: {},
-  ignoreFiles: ['assets/strict/custom_quill.scss'],
+  ignoreFiles: ['assets/override/quill.scss'],
 }
