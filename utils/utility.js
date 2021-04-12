d16c7f8d9bf5859b79da245fb52b7f63d6e90b34
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-04-12 12:17:24 +0530

    Updated Day JS Locale
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- utils/utility.js
+++ utils/utility.js
@@ -3,7 +3,27 @@ import { sanitizationConfig } from '~/config/sanitizationConfig'
 
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
+const updateLocale = require('dayjs/plugin/updateLocale')
 dayjs.extend(relativeTime)
+dayjs.extend(updateLocale)
+
+dayjs.updateLocale('en', {
+  relativeTime: {
+    future: 'in %s',
+    past: '%s',
+    s: 'Just now',
+    m: '1 min',
+    mm: '%d mins',
+    h: '1 hr',
+    hh: '%d hrs',
+    d: '1 day',
+    dd: '%d days ago',
+    M: '1 month ago',
+    MM: '%d months',
+    y: '1 year ago',
+    yy: '%d years ago',
+  },
+})
 
 export const getRelativeTime = (timeString) => {
   return dayjs().to(dayjs(timeString))
