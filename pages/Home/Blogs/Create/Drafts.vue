38f4e3a63ca77d27095c86f0583b31500dddca2b
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-04-21 23:58:32 +0530

    Added DraftsPage
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Blogs/Create/Drafts.vue
+++ pages/Home/Blogs/Create/Drafts.vue
@@ -6,17 +6,52 @@
       </template>
     </AppBarHeader>
 
-    <main>
-      <article v-for="draft in drafts" :key="draft.uniqueId" class="px-4 py-2">
-        <h5>{{ draft.title }}</h5>
-        <p>{{ draft.subtitle }}</p>
+    <FallBackLoader v-if="loadingContent" class="my-8">
+      <template #fallback>
+        <p class="text-center">Loading Your Drafts</p>
+      </template>
+    </FallBackLoader>
 
-        <button v-ripple class="secondary-outlined-btn my-4">
-          Continue Editing
-        </button>
-        <button v-ripple class="danger-outlined-btn my-4">Delete Draft</button>
-      </article>
-    </main>
+    <LoadingError
+      v-else-if="loadingError"
+      class="my-8 px-4"
+      error-section="draft blogs"
+      no-remedy-option
+    />
+
+    <transition name="scale-up" v-else-if="emptyDraft">
+      <aside class="px-4 py-6 text-center">
+        <p class="my-4">No Saved Blog Drafts Found</p>
+        <nuxt-link :to="navigationRoutes.Home.Blogs.Create.AddBlog">
+          <button v-ripple class="secondary-outlined-btn">Create Blog</button>
+        </nuxt-link>
+      </aside>
+    </transition>
+
+    <transition name="scale-up" v-else>
+      <main>
+        <article
+          v-for="draft in drafts"
+          :key="draft.uniqueId"
+          class="px-4 py-2"
+        >
+          <h5>{{ draft.title }}</h5>
+          <p>{{ draft.subtitle }}</p>
+
+          <button v-ripple class="secondary-outlined-btn my-4">
+            Continue Editing
+          </button>
+
+          <button
+            v-ripple
+            class="danger-outlined-btn my-4"
+            @click="deleteDraft(draft.uniqueId)"
+          >
+            Delete Draft
+          </button>
+        </article>
+      </main>
+    </transition>
   </div>
 </template>
 
@@ -31,6 +66,9 @@ export default {
   middleware: 'isAuthenticated',
   data() {
     return {
+      emptyDraft: false,
+      loadingContent: true,
+      loadingError: false,
       pageTitle: 'Drafts',
       navigationRoutes,
       db: undefined,
@@ -39,9 +77,16 @@ export default {
   },
 
   async mounted() {
-    await this.createLocalDBIfNotExists()
+    try {
+      await this.createLocalDBIfNotExists()
 
-    this.drafts = await this.db.getAllFromIndex('drafts', 'uniqueId')
+      this.drafts = await this.db.getAllFromIndex('drafts', 'uniqueId')
+      this.emptyDraft = !this.drafts.length
+    } catch (e) {
+      this.loadingError = true
+    } finally {
+      this.loadingContent = false
+    }
   },
 
   methods: {
@@ -55,6 +100,11 @@ export default {
           store.createIndex('uniqueId', 'uniqueId')
         },
       })
+    },
+
+    async deleteDraft(uniqueId) {
+      const tx = this.db.transaction('drafts', 'readwrite')
+      await Promise.all([tx.store.delete(uniqueId), tx.done])
     },
   },
 
@@ -75,6 +125,16 @@ export default {
       background: $body-bg-alternate;
       box-shadow: $default-box-shadow;
     }
+  }
+
+  button {
+    min-width: auto;
+  }
+
+  aside,
+  main {
+    max-width: $large-screen;
+    margin: auto;
   }
 }
 </style>
