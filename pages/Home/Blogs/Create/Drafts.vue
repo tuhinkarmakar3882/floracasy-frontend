326c957680ff9be7899bc75eb9a58f068c1890cd
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-04-21 20:37:52 +0530

    WIP Drafts.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/Blogs/Create/Drafts.vue
+++ pages/Home/Blogs/Create/Drafts.vue
@@ -1,29 +1,62 @@
 <template>
-  <AppFeel
-    :on-back="navigationRoutes.Home.Blogs.Create.index"
-    class="draft-blog-page"
-  >
-    <template slot="app-bar-title">
-      {{ pageTitle }}
-    </template>
-    <template slot="main" class="my-4">This is a Draft View</template>
-  </AppFeel>
+  <div class="draft-blog-page">
+    <AppBarHeader>
+      <template #title>
+        {{ pageTitle }}
+      </template>
+    </AppBarHeader>
+
+    <main>
+      <article v-for="draft in drafts" :key="draft.uniqueId" class="px-4 py-2">
+        <h5>{{ draft.title }}</h5>
+        <p>{{ draft.subtitle }}</p>
+
+        <button v-ripple class="secondary-outlined-btn my-4">
+          Continue Editing
+        </button>
+        <button v-ripple class="danger-outlined-btn my-4">Delete Draft</button>
+      </article>
+    </main>
+  </div>
 </template>
 
 <script>
 import { navigationRoutes } from '@/navigation/navigationRoutes'
+import AppBarHeader from '~/components/Layout/AppBarHeader'
+import { openDB } from 'idb'
 
 export default {
   name: 'Drafts',
+  components: { AppBarHeader },
   middleware: 'isAuthenticated',
   data() {
     return {
       pageTitle: 'Drafts',
       navigationRoutes,
+      db: undefined,
+      drafts: [],
     }
   },
 
-  mounted() {},
+  async mounted() {
+    await this.createLocalDBIfNotExists()
+
+    this.drafts = await this.db.getAllFromIndex('drafts', 'uniqueId')
+  },
+
+  methods: {
+    async createLocalDBIfNotExists() {
+      this.db = await openDB('Blogs', 1, {
+        upgrade(db) {
+          const store = db.createObjectStore('drafts', {
+            keyPath: 'uniqueId',
+          })
+          // Create an index on the 'date' property of the objects.
+          store.createIndex('uniqueId', 'uniqueId')
+        },
+      })
+    },
+  },
 
   head() {
     return {
@@ -32,3 +65,16 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.draft-blog-page {
+  article {
+    &:nth-child(even) {
+      background: $body-bg-alternate;
+      box-shadow: $default-box-shadow;
+    }
+  }
+}
+</style>
