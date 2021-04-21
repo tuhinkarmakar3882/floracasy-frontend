206d3526247dcb7a1d270997aa14a69fed2ef695
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-04-21 18:54:24 +0530

    Simplified SignInToContinue.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Authentication/SignInToContinue.vue
+++ pages/Authentication/SignInToContinue.vue
@@ -1,49 +1,19 @@
 <template>
   <div class="get-started-authentication-page">
-    <div v-if="showLoaderAnimation">
-      <p class="text-center mb-8">{{ stateInformation }}</p>
-      <LoadingIcon />
-    </div>
+    <a aria-label="Close Popup" title="Close Popup" @click="$router.back()">
+      <i class="mdi mdi-close" />
+    </a>
 
-    <div v-else>
-      <a
-        aria-label="close button"
-        class="close-btn"
-        @click="
-          () => {
-            this.$router.replace('/')
-          }
-        "
-      >
-        <span class="mdi mdi-close" />
-      </a>
+    <h3 class="my-4">Let's Get Started</h3>
 
-      <h3 class="my-6 text-center">Let&rsquo;s Get Started</h3>
-      <small class="text-center" style="display: block">
-        Get Started with the most advanced <br />
-        password less sign in
-      </small>
-      <section class="signupOptionsGrid my-8">
-        <button
-          v-ripple
-          class="secondary-outlined-btn"
-          @click="signInWithPopup"
-        >
-          Continue with
-          <GoogleIcon class="ml-1 icon" />
-        </button>
-      </section>
-    </div>
+    <p class="my-4">It's Secure, Faster, Better</p>
+
+    <GoogleSignInButton class="secondary-outlined-btn" />
   </div>
 </template>
 
 <script>
-import firebase from 'firebase/app'
 import 'firebase/auth'
-import endpoints from '@/api/endpoints'
-import * as secrets from '@/environmentVariables'
-import { navigationRoutes } from '@/navigation/navigationRoutes'
-import { LogAnalyticsEvent, showUITip } from '~/utils/utility'
 
 export default {
   name: 'SignInToContinue',
@@ -65,122 +35,6 @@ export default {
     }
   },
 
-  mounted() {},
-
-  methods: {
-    async signInWithPopup() {
-      await showUITip(this.$store, '', '', true)
-
-      this.showLoaderAnimation = true
-
-      this.updateInfo('Loading OAuth Provider')
-      const provider = new firebase.auth.GoogleAuthProvider()
-
-      this.updateInfo('Waiting for you to complete the login')
-      const user = await firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then((result) => result.user)
-        .catch(() => {})
-
-      if (user && user !== {}) {
-        this.updateInfo('Fetching Certificates...')
-        const jsonUser = user.toJSON()
-        const frontendPayload = {
-          uid: jsonUser?.uid,
-          displayName: jsonUser?.displayName,
-          phoneNumber: jsonUser?.phoneNumber,
-          email: jsonUser?.email,
-          photoURL: jsonUser?.photoURL,
-          createdAt: jsonUser?.createdAt,
-          lastLoginAt: jsonUser?.lastLoginAt,
-        }
-        const backendPayload = {
-          ...frontendPayload,
-          emailVerified: jsonUser?.emailVerified,
-          authDomain: jsonUser?.authDomain,
-          providerId: jsonUser?.providerData[0]?.uid,
-          updatedAt: jsonUser?.lastLoginAt,
-          accessToken: jsonUser?.stsTokenManager?.accessToken,
-        }
-
-        this.updateInfo('Validating Credentials...')
-        try {
-          const response = await this.$axios.$post(
-            endpoints.auth.authenticate,
-            backendPayload
-          )
-
-          this.updateInfo('Logging you in...')
-          await this.login(frontendPayload, response)
-        } catch (e) {
-          await this.abort()
-        }
-      } else {
-        await this.abort()
-      }
-    },
-
-    async login(payload, { token: tokens }) {
-      this.updateInfo('Setting up things for you...')
-      await this.saveAndApplyTokens(tokens)
-
-      this.updateInfo('Securing the Connection...')
-      await this.updateVuexStates(payload)
-
-      this.updateInfo('Welcome')
-
-      LogAnalyticsEvent('google_sign_in')
-
-      await this.$router.replace(navigationRoutes.Home.DashBoard)
-    },
-
-    async abort() {
-      this.updateInfo('Error, While Logging you in.')
-      await this.$store.dispatch('logout')
-
-      this.showLoaderAnimation = false
-      this.updateInfo('Checking...')
-      localStorage.clear()
-
-      await showUITip(
-        this.$store,
-        'Unable to Login. Please Refresh',
-        'error',
-        false
-      )
-    },
-
-    async saveAndApplyTokens(tokens) {
-      await this.$cookies.set(
-        'access',
-        tokens.access,
-        secrets.cookieSavingConfig
-      )
-      await this.$cookies.set(
-        'refresh',
-        tokens.refresh,
-        secrets.cookieSavingConfig
-      )
-      this.$axios.setToken(tokens.access, 'Bearer')
-    },
-
-    async updateVuexStates(payload) {
-      await this.$store.dispatch('login')
-      await this.$store.dispatch('UserManagement/setUserData', {
-        user: payload,
-      })
-      await this.$store.dispatch('UserManagement/fetchData')
-      await this.$store.dispatch(
-        'NotificationChannel/fetchNotificationChannelId'
-      )
-    },
-
-    updateInfo(message) {
-      this.stateInformation = message || 'Opening Popup for Login'
-    },
-  },
-
   head() {
     return {
       title: this.pageTitle,
@@ -193,15 +47,18 @@ export default {
 @import 'assets/all-variables';
 
 .get-started-authentication-page {
-  display: block;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   position: relative;
-  width: clamp(300px, 90vw, 650px);
+  width: clamp(300px, 90vw, 500px);
   background: darken($card-bg, 5%);
   border-radius: 10px;
   padding: 2rem 1.5rem;
   box-shadow: 0 0 4px lighten($card-bg, 50%);
 
-  .close-btn {
+  a {
     position: absolute;
     top: $standard-unit;
     right: $large-unit;
@@ -210,26 +67,8 @@ export default {
     z-index: 1;
   }
 
-  .signupOptionsGrid {
-    display: grid;
-    grid-auto-flow: row;
-    grid-template-columns: 1fr;
-    place-items: center;
-    grid-row-gap: 3vh;
-
-    button {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: 1.8 * $large-unit;
-      width: 250px;
-      font-size: 1rem;
-
-      & .icon {
-        height: 100%;
-        padding: $standard-unit - $nano-unit;
-      }
-    }
+  button {
+    width: 290px;
   }
 }
 </style>
