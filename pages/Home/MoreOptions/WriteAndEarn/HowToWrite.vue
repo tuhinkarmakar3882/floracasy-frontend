bad317ed4a19656ec6d0c72c1ecb6313da3e5d11
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-15 15:09:19 +0530

    Updated Connections in HowToWrite.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/WriteAndEarn/HowToWrite.vue
+++ pages/Home/MoreOptions/WriteAndEarn/HowToWrite.vue
@@ -1,12 +1,9 @@
 <template>
-  <AppFeel
-    :on-back="navigationRoutes.Home.MoreOptions.WriteAndEarn"
-    class="how-to-write-page"
-  >
-    <template slot="app-bar-title">
-      {{ pageTitle }}
-    </template>
-    <template slot="main" class="my-4">
+  <div class="how-to-write-page">
+    <AppBarHeader auto-hide>
+      <template #title>{{ pageTitle }}</template>
+    </AppBarHeader>
+    <main class="px-4 my-4">
       <p class="my-8 text-center py-4 px-4">
         We're working hard to prepare a guide in order to help you write blogs,
         faster.
@@ -14,201 +11,138 @@
         <br />
         Come back Soon to view it :)
       </p>
-      <!--      <main class="px-4 my-4">-->
-      <!--        <section v-for="(guideline, index) in guidelines" :key="index">-->
-      <!--          <h4 class="heading-title">{{ guideline.title }}</h4>-->
-      <!--          <p>{{ guideline.description }}</p>-->
-      <!--          <img-->
-      <!--            v-if="guideline.image"-->
-      <!--            :alt="guideline.title"-->
-      <!--            :src="guideline.image"-->
-      <!--          />-->
-      <!--        </section>-->
 
-      <!--        <div>-->
-      <!--          <h4 class="heading-title">How to add sub title</h4>-->
+      <div v-if="false">
+        <p>
+          Hey! folks, might you face some difficulty to write blog or may be you
+          get confused with toolbox that How you can write blog & getting more
+          attracted towards your audience.
+        </p>
 
-      <!--          <p>-->
-      <!--            Subtitles are as important as title. It's a must to add a subtitle-->
-      <!--            otherwise your blog is not going to publish. To add a subtitle, To-->
-      <!--            add a title, you just need to fill up the input box as shown in the-->
-      <!--            below illustration.-->
-      <!--          </p>-->
-      <!--          <img-->
-      <!--            alt="gify to demostrate add sub title"-->
-      <!--            src="http://img_screenshort"-->
-      <!--          />-->
-      <!--        </div>-->
+        <h4>B</h4>
+        <p>
+          B is mainly used to make blod your writing.First select the portion
+          you need to make bold,tap on B.Yes this is as simple as this.
+        </p>
 
-      <!--        <div>-->
-      <!--          <h4 class="heading-title">How to choose a category</h4>-->
-      <!--          <p>-->
-      <!--            Category is a important part of your blog &amp; also its mandatory-->
-      <!--            to choose a category according to your blog. You can also add a-->
-      <!--            category if you want but for this you need show a specific reason-->
-      <!--            that why you are going to add a new category , our team will you out-->
-      <!--            in that case.-->
+        <h4>I</h4>
+        <p>
+          I is mainly used to make Italic your writing.First select the portion
+          you need to make italic,tap on I.Yes this is as simple as this.
+        </p>
 
-      <!--            <label>Choose a category:</label>-->
+        <h3>U</h3>
+        <p>this is mainly used to make underline.</p>
 
-      <!--            <select name="category">-->
-      <!--              <option value="category">Options</option>-->
-      <!--              <option value="music">Music</option>-->
-      <!--              <option value="Technology ">Technology</option>-->
-      <!--              <option value="Gaming">Gaming</option>-->
-      <!--              <option value="Cooking">Cooking</option>-->
-      <!--            </select>-->
-      <!--          </p>-->
-      <!--        </div>-->
+        <h3>_</h3>
+        <p>
+          It's mainly used to set a devider line. It may work as a separating
+          line between 2 segment.
+        </p>
 
-      <!--        <div>-->
-      <!--          <h4 class="heading-title">Function of Toolbox</h4>-->
+        <h3>H1</h3>
+        <p>
+          It's mainly used to set Heading. Heading has different sizes like H1
+          is use to create the largest heading size.
+        </p>
 
-      <!--          <p>-->
-      <!--            Toolbox mainly give a styling to your content . By using toolbox you-->
-      <!--            can style your containt . you can add list ; colour ; alignment-->
-      <!--            &amp; many more whatever you want . each &amp; every element of-->
-      <!--            toolbox will describe below.-->
-      <!--          </p>-->
+        <h3>H2</h3>
+        <p>
+          It's also used to set Heading. Heading has different sizes, like H2 is
+          use to create the larger size than H3 but smaller size than H1.
+        </p>
 
-      <!--          <h4 class="heading-title">Types of Headings</h4>-->
+        <h3>H3</h3>
+        <p>
+          It's also used to set Heading. Heading has different sizes, like H3 is
+          use to create the larger size than H4 but smaller size than H2.
+        </p>
 
-      <!--          <p>-->
-      <!--            This is mainly use for headline .you can use this tool to chng your-->
-      <!--            text headline-->
-      <!--          </p>-->
+        <h3>H4</h3>
+        <p>
+          It's also used to set Heading. Heading has different sizes, like H4 is
+          use to create the larger size than H5 but smaller size than H3
+        </p>
 
-      <!--          <ul>-->
-      <!--            <li>Normal headline : this is mainly use for normal use</li>-->
-      <!--            <li>H1 heading: The size of this heading is biggest</li>-->
-      <!--            <li>H2 heading: This heading size is bigger than H3</li>-->
-      <!--            <li>H3 heading: This heading size is bigger than H4</li>-->
-      <!--            <li>H4 heading:This heading size is bigger than H5</li>-->
-      <!--            <li>H5 heading: This heading size is bigger than H6</li>-->
-      <!--            <li>H6 heading: This heading size is smallest</li>-->
-      <!--          </ul>-->
+        <h3>H5</h3>
+        <p>
+          It's also used to set Heading. Heading has different sizes, like H5 is
+          use to create the larger size than h6 but smaller size than h4
+        </p>
 
-      <!--          <h3>B :</h3>-->
-      <!--          <p>-->
-      <!--            &quot;B&quot; stands for &quot;Bold&quot; so its use to make bold-->
-      <!--            your words-->
-      <!--          </p>-->
+        <h3>H6</h3>
+        <p>It's used to make the smallest Heading size.</p>
 
-      <!--          <h3>I :</h3>-->
-      <!--          <p>-->
-      <!--            &quot;I&quot; stands for &quot;Italic&quot; so its use to make-->
-      <!--            Italic your words-->
-      <!--          </p>-->
+        <h3>Alignment</h3>
+        <p>
+          You can align your words or sentences with the help of this tool. Here
+          mainly 3 type of alignment you can use from this toolbox. 1st one is
+          mainly used for right alignment. 2nd one is used for center alignment.
+          3rd one is for left alignment.
+        </p>
 
-      <!--          <h3>U :</h3>-->
-      <!--          <p>-->
-      <!--            &quot;U&quot; stands for &quot;Underline&quot; so its use to get-->
-      <!--            Underline-->
-      <!--          </p>-->
+        <h3>s</h3>
+        <p>
+          This tool is mainly used to scratchthrough. Simply select the portion
+          you want to scratchthrough and click the option.
+        </p>
 
-      <!--          <h3>$(sorry i couldn&#39;t find the exact icon):</h3>-->
+        <h3>Table</h3>
+        <p>
+          This tool is used to add table in your blog.You can add 2type of table
+          here. 1. Order List:- You can add order list simple tapping the above
+          seen icon and yes you list is added. 2. unorder List:- You can add
+          unorder list simple tapping the above seen icon and yes you list is
+          added.
+        </p>
 
-      <!--          <p>&quot;$&quot; stands for scratch through ..so its use for it.</p>-->
+        <h3>Superscript</h3>
+        <p>
+          when you want to add superscript text, just tap on superscript icon,
+          then add the text you want to add as superscript.
+        </p>
 
-      <!--          <h3>Alignments</h3>-->
+        <h3>subscript</h3>
+        <p>
+          when you want to add subscript text, just tap on subscript icon, then
+          add the text you want to add as subscript.
+        </p>
 
-      <!--          <img loading="lazy"decoding="async"alt=" screenshort or gify" src="#0.1_" />-->
+        <h3>link</h3>
+        <p>
+          1st Type the word or link name which you want to make a link, then
+          select the portion & tap on link icon from toolbox.A window come in
+          front of your screen,type your full link IP address and add this. For
+          better knowledge please check the below GIF.
+        </p>
 
-      <!--          <p>-->
-      <!--            You can easily change your alignment by using these tools. These are-->
-      <!--            4 alignment type:-->
-      <!--          </p>-->
+        <h3>image</h3>
+        <p>
+          Tap on image icon from toolbox and type your image IP address and
+          press the add button. The image is added in your blog.
+        </p>
 
-      <!--          <ul>-->
-      <!--            <li>Left alignment (icon)</li>-->
-      <!--            <li>Right alignment (icon)</li>-->
-      <!--            <li>Centre Alignment (icon)</li>-->
-      <!--            <li>Alignment (icon)</li>-->
-      <!--          </ul>-->
+        <h3>Vedio</h3>
+        <p>
+          Tap on vedio icon from toolbox and type your vedio IP address or vedio
+          link and press the add button. The vedio is added in your blog.
+        </p>
 
-      <!--          <h1 class="mdi mdi-format-quote-open" />-->
+        <h3>blogquote</h3>
+        <p>
+          blogquote is mainly use to give a special importance of some specific
+          portion. blogquote is super easy to use, just tap the button from
+          toolbox & start writing.
+        </p>
 
-      <!--          <p>This is a Blockquote</p>-->
-
-      <!--          <br />-->
-
-      <!--          <h3>this icon &lt;/&gt;</h3>-->
-
-      <!--          <p>you may use this tool to write codes</p>-->
-
-      <!--          <h3>How to create list in your content</h3>-->
-
-      <!--          <p>-->
-      <!--            This element is use to add list in your content . you can add-->
-      <!--            verious type of list in your blog . we will describe types of list-->
-      <!--            which you can add below-->
-      <!--          </p>-->
-
-      <!--          <img loading="lazy"decoding="async"alt="screen recording" src="#0.1_" />-->
-
-      <!--          <ul>-->
-      <!--            <li>-->
-      <!--              Unorder list (icon) : you can add a ordered list simple by using-->
-      <!--              this element. we will add picture so that you can use it without-->
-      <!--              any hassle.-->
-      <!--            </li>-->
-      <!--            <br />-->
-
-      <!--            <li>-->
-      <!--              Order list (icon) : you can add a Unordered list simple by using-->
-      <!--              this element. we will add picture so that you can use it without-->
-      <!--              any hassle.-->
-      <!--            </li>-->
-
-      <!--            <br />-->
-
-      <!--            <li>-->
-      <!--              Normal list (icon ) : you can add a ordered list simple by using-->
-      <!--              this element. we will add picture so that you can use it without-->
-      <!--              any hassle.-->
-      <!--            </li>-->
-      <!--            <br />-->
-      <!--          </ul>-->
-
-      <!--          <h3>How to set position</h3>-->
-
-      <!--          <img loading="lazy"decoding="async"alt="screenshorts" src="#0.1_" />-->
-
-      <!--          <p>-->
-      <!--            You can simply set your position by using this particular element-->
-      <!--            from toolbox.-->
-      <!--          </p>-->
-
-      <!--          <h3>How to add colour</h3>-->
-
-      <!--          <h4 class="heading-title">Use of A (icon)</h4>-->
-
-      <!--          <p>-->
-      <!--            This tool is mainly use to add colour your text. this is a good idea-->
-      <!--            to have a look forward to seeing you on the way to get the best way-->
-      <!--            .-->
-      <!--          </p>-->
-
-      <!--          <img loading="lazy"decoding="async"alt=" screenshort" src="#0.1_" />-->
-
-      <!--          <h3>link (icon)</h3>-->
-
-      <!--          <p>Its mainly use to add link in your contant .</p>-->
-
-      <!--          <h3>Image (icon)</h3>-->
-      <!--          <p>its mainly use to add image in your contant.</p>-->
-
-      <!--          <h3>last (icon)</h3>-->
-
-      <!--          <p>i don&#39;t know it&#39;s use only developer can explain this</p>-->
-      <!--        </div>-->
-      <!--      </main>-->
-    </template>
-
-    <template #footer>
-      <InFeedAd class="my-4" use-small-ads />
-    </template>
-  </AppFeel>
+        <h3>Code</h3>
+        <p>
+          code is use to write any kind of code in your blog. just tap the code
+          button from toolbox & start writing code in your blog
+        </p>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
@@ -245,3 +179,14 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.how-to-write-page {
+  main {
+    max-width: $large-screen;
+    margin: auto;
+  }
+}
+</style>
