00097a5f4e9d1a682eb0850ee2302832b3e8fbd0
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-16 20:47:03 +0530

    Randomized Blog Loadings
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/Mobile/View/Dashboard/InfiniteScrollingBlogLists.vue
+++ components/Mobile/View/Dashboard/InfiniteScrollingBlogLists.vue
@@ -115,7 +115,16 @@ export default {
         )
         if (results.length) {
           this.blogFetchCursorEndpoint = processLink(next, false)
-          this.blogs.push(...results)
+          const shuffleArray = (array) => {
+            for (let i = array.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1))
+              const temp = array[i]
+              array[i] = array[j]
+              array[j] = temp
+            }
+            return array
+          }
+          this.blogs.push(...shuffleArray(results))
           $state.loaded()
         } else {
           $state.complete()
