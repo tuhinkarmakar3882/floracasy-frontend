a2af756ec7590c838feecbc5222c3c358aa7f7d0
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-25 15:55:06 +0530

    Added Fallback Stuffs
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/Common/GoogleAdsense/InFeedAd.vue
+++ components/Common/GoogleAdsense/InFeedAd.vue
@@ -1,37 +1,38 @@
 <template>
-  <transition name="scale-up">
-    <div v-if="showAds" ref="ads">
-      <ins
-        v-if="useAlternateAccount"
-        class="adsbygoogle"
-        data-ad-client="ca-pub-6503565742033720"
-        data-ad-format="fluid"
-        data-ad-layout-key="-5u+dg+44-5g+2z"
-        data-ad-slot="9825202092"
-        style="display: block"
-      />
-      <ins
-        v-else-if="useSmallAds"
-        class="adsbygoogle"
-        data-ad-client="ca-pub-9863542606738743"
-        data-ad-format="fluid"
-        data-ad-layout-key="-fb+5w+4e-db+86"
-        data-ad-slot="5290672704"
-        style="display: block"
-      />
-      <ins
-        v-else
-        class="adsbygoogle"
-        data-ad-client="ca-pub-9863542606738743"
-        data-ad-format="fluid"
-        data-ad-layout-key="-50+c6-23-9g+y5"
-        data-ad-slot="2010436071"
-        style="display: block"
-      />
-    </div>
+  <div v-if="showAds" ref="ads">
+    <ins
+      v-if="useAlternateAccount"
+      class="adsbygoogle"
+      data-ad-client="ca-pub-6503565742033720"
+      data-ad-format="fluid"
+      data-ad-layout-key="-5u+dg+44-5g+2z"
+      data-ad-slot="9825202092"
+      style="display: block"
+    />
+    <ins
+      v-else-if="useSmallAds"
+      class="adsbygoogle"
+      data-ad-client="ca-pub-9863542606738743"
+      data-ad-format="fluid"
+      data-ad-layout-key="-fb+5w+4e-db+86"
+      data-ad-slot="5290672704"
+      style="display: block"
+    />
+    <ins
+      v-else
+      class="adsbygoogle"
+      data-ad-client="ca-pub-9863542606738743"
+      data-ad-format="fluid"
+      data-ad-layout-key="-50+c6-23-9g+y5"
+      data-ad-slot="2010436071"
+      style="display: block"
+    />
+  </div>
 
-    <div v-else ref="ads" />
-  </transition>
+  <div v-else ref="ads" class="fallback-container">
+    <i class="mdi mdi-google-ads" />
+    <p class="mx-4">Advertisement will Load Here</p>
+  </div>
 </template>
 
 <script>
@@ -94,3 +95,39 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.fallback-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 1rem 0.5rem;
+  background: linear-gradient(
+      45deg,
+      transparent 0%,
+      $navigation-bar-color 12.5%,
+      $body-bg-alternate 25%,
+      $card-bg 37.5%,
+      $card-bg-alternate 50%,
+      $card-bg 62.5%,
+      $body-bg-alternate 75%,
+      $navigation-bar-color 87.5%,
+      transparent 100%
+    )
+    right no-repeat;
+  background-size: 400%;
+  animation: shift-background 20s infinite alternate-reverse ease-in-out;
+
+  @keyframes shift-background {
+    from {
+      background-position: left;
+    }
+    to {
+      background-position: right;
+    }
+  }
+}
+</style>
