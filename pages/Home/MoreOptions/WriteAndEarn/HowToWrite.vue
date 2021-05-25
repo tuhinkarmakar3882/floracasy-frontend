c13be32923dc7901189155a5bedb53f37a2a504c
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-25 21:02:37 +0530

    Added How to Write Page
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- pages/Home/MoreOptions/WriteAndEarn/HowToWrite.vue
+++ pages/Home/MoreOptions/WriteAndEarn/HowToWrite.vue
@@ -1,147 +1,225 @@
 <template>
   <div class="how-to-write-page">
-    <AppBarHeader auto-hide>
+    <AppBarHeader sticky>
       <template #title>{{ pageTitle }}</template>
+      <template #action-button>
+        <nuxt-link v-ripple :to="navigationRoutes.Home.Blogs.Create.AddBlog">
+          <button class="secondary-btn">Create Blog</button>
+        </nuxt-link>
+      </template>
     </AppBarHeader>
-    <main class="px-4 my-4">
-      <p class="my-8 text-center py-4 px-4">
-        We're working hard to prepare a guide in order to help you write blogs,
-        faster.
-        <br />
-        <br />
-        Come back Soon to view it :)
-      </p>
 
-      <div v-if="false">
-        <p>
-          Hey! folks, might you face some difficulty to write blog or may be you
-          get confused with toolbox that How you can write blog & getting more
-          attracted towards your audience.
-        </p>
+    <div class="main-container">
+      <main class="px-4 my-4">
+        <div>
+          <h1 class="mb-8 text-center">Write your first Blog</h1>
+          <hr />
 
-        <h4>B</h4>
-        <p>
-          B is mainly used to make blod your writing.First select the portion
-          you need to make bold,tap on B.Yes this is as simple as this.
-        </p>
+          <p>
+            Follow this guide to write your first blog on Floracasy & We hope to
+            get you familiarized with the toolbox within a minute or so!
+          </p>
 
-        <h4>I</h4>
-        <p>
-          I is mainly used to make Italic your writing.First select the portion
-          you need to make italic,tap on I.Yes this is as simple as this.
-        </p>
+          <h2>Get Started</h2>
 
-        <h3>U</h3>
-        <p>this is mainly used to make underline.</p>
+          <section>
+            <p>
+              Start with a strong <strong>HEADING</strong> introducing the title
+              of your content along with blog category as you can see in the
+              image below. They are required fields to get started though you
+              can add subtitle and image cover url too to make your blog stand
+              alone among others.
+            </p>
 
-        <h3>_</h3>
-        <p>
-          It's mainly used to set a devider line. It may work as a separating
-          line between 2 segment.
-        </p>
+            <img alt="" src="/images/tutorial/first_blog.png" />
+          </section>
+          <h2>After clicking on Next</h2>
+          <hr />
 
-        <h3>H1</h3>
-        <p>
-          It's mainly used to set Heading. Heading has different sizes like H1
-          is use to create the largest heading size.
-        </p>
+          <section>
+            <h3>Introduction</h3>
+            <p>
+              Always start your blog with an Introduction or a Goal that defines
+              the purpose of the blog.
+            </p>
+            <img alt="" src="/images/tutorial/Introduction.png" />
+          </section>
 
-        <h3>H2</h3>
-        <p>
-          It's also used to set Heading. Heading has different sizes, like H2 is
-          use to create the larger size than H3 but smaller size than H1.
-        </p>
+          <section>
+            <h3>Heading Tags</h3>
+            <p>
+              You can always use different headings, e.g. H1, H2, H3, H4, H5,
+              H6, according to your need.
+            </p>
+            <img alt="" src="/images/tutorial/Headings.png" />
+          </section>
 
-        <h3>H3</h3>
-        <p>
-          It's also used to set Heading. Heading has different sizes, like H3 is
-          use to create the larger size than H4 but smaller size than H2.
-        </p>
+          <section>
+            <h3>Text Alignments</h3>
+            <p>
+              Using alignments is an important part of good writing. Always use
+              alignments as per need.
+            </p>
+            <ul>
+              <li>1st one is mainly used for left alignment.</li>
+              <li>2nd one is used for center alignment.</li>
+              <li>3rd one is for right alignment.</li>
+            </ul>
+            <img alt="" src="/images/tutorial/Alignement.png" />
+          </section>
 
-        <h3>H4</h3>
-        <p>
-          It's also used to set Heading. Heading has different sizes, like H4 is
-          use to create the larger size than H5 but smaller size than H3
-        </p>
+          <section>
+            <h3>Text Formats</h3>
+            <section>
+              <h6>Bold</h6>
+              <p>
+                B is mainly used to make bold your writing. First select the
+                portion you want to make bold, tap on B. (or use
+                CTRL+B[windows/linux] or CMD+B[mac])
+              </p>
+              <img alt="" src="/images/tutorial/Boldtext.png" />
+            </section>
+            <section>
+              <h6>Italic</h6>
+              <p>
+                I is mainly used to make Italic your writing. First select the
+                portion you need to make italic,tap on I. (or use
+                CTRL+I[windows/linux] or CMD+I[mac])
+              </p>
+              <img alt="" src="/images/tutorial/Italic.png" />
+            </section>
+            <section>
+              <h6>UnderLine</h6>
+              <p>
+                U is mainly used to make underline. First select the portion you
+                need to make italic,tap on U. (or use CTRL+U[windows/linux] or
+                CMD+U[mac])
+              </p>
+              <img alt="" src="/images/tutorial/underscore.png" />
+            </section>
+            <section>
+              <h6>Strike Through</h6>
+              <p>
+                This tool is mainly used to strike-through. Simply select the
+                portion you want to strike-through and click the option.
+              </p>
+              <img alt="" src="/images/tutorial/strike.png" />
+            </section>
+            <section>
+              <h6>Sub Script</h6>
+              <p>
+                When you want to add subscript text, just tap on subscript icon,
+                then add the text you want to add as subscript.
+              </p>
+              <img alt="" src="/images/tutorial/subscript.png" />
+            </section>
+            <section>
+              <h6>Super Script</h6>
+              <p>
+                When you want to add superscript text, just tap on superscript
+                icon, then add the text you want to add as superscript.
+              </p>
+              <img alt="" src="/images/tutorial/superscript.png" />
+            </section>
+          </section>
 
-        <h3>H5</h3>
-        <p>
-          It's also used to set Heading. Heading has different sizes, like H5 is
-          use to create the larger size than h6 but smaller size than h4
-        </p>
+          <section>
+            <h3>Code Snippet</h3>
+            <p>
+              If you want to use a code snippet in your blog, this is for you.
+            </p>
+            <img alt="" src="/images/tutorial/code.png" />
+          </section>
 
-        <h3>H6</h3>
-        <p>It's used to make the smallest Heading size.</p>
+          <section>
+            <h3>Blockquote</h3>
+            <p>Use Blockquote to highlight a text.</p>
+            <img alt="" src="/images/tutorial/blockquote.png" />
+          </section>
 
-        <h3>Alignment</h3>
-        <p>
-          You can align your words or sentences with the help of this tool. Here
-          mainly 3 type of alignment you can use from this toolbox. 1st one is
-          mainly used for right alignment. 2nd one is used for center alignment.
-          3rd one is for left alignment.
-        </p>
+          <section>
+            <h3>Add Image</h3>
+            <p>
+              You can also add/upload images to the blog for better explanation.
+            </p>
+            <img alt="" src="/images/tutorial/add_image.png" />
+          </section>
 
-        <h3>s</h3>
-        <p>
-          This tool is mainly used to scratchthrough. Simply select the portion
-          you want to scratchthrough and click the option.
-        </p>
+          <section>
+            <h3>Add External links</h3>
+            <p>
+              Want to use external links? use the chain like icon on toolbar.
+              You can embed the following as a Rich Content,
+            </p>
+            <ul>
+              <li>Any Youtube Video URL</li>
+              <li>Any Vimeo Video URL</li>
+              <li>Any CodePen Link</li>
+              <li>Any CodeSandbox Link</li>
+              <li>Any Instagram Link</li>
+              <li>Any Instagram Link</li>
+            </ul>
+            <p>Anything else, will be added as a Link preview</p>
+            <img alt="" src="/images/tutorial/external_link.png" />
+          </section>
 
-        <h3>Table</h3>
-        <p>
-          This tool is used to add table in your blog.You can add 2type of table
-          here. 1. Order List:- You can add order list simple tapping the above
-          seen icon and yes you list is added. 2. unorder List:- You can add
-          unorder list simple tapping the above seen icon and yes you list is
-          added.
-        </p>
+          <section>
+            <h3>Lists</h3>
+            <hr />
+            <section>
+              <h6>Ordered List</h6>
+              <img alt="" src="/images/tutorial/ordered_list.png" />
+            </section>
+            <section>
+              <h6>Unordered List</h6>
+              <img alt="" src="/images/tutorial/unorder.png" />
+            </section>
+          </section>
 
-        <h3>Superscript</h3>
-        <p>
-          when you want to add superscript text, just tap on superscript icon,
-          then add the text you want to add as superscript.
-        </p>
+          <section>
+            <h3>Divider</h3>
+            <p>
+              Don't be afraid to use dividers if you're writing a large blog or
+              even a small, dividers make readability easy and also adds some
+              style to the blog.
+            </p>
+            <img alt="" src="/images/tutorial/divider.png" />
+          </section>
 
-        <h3>subscript</h3>
-        <p>
-          when you want to add subscript text, just tap on subscript icon, then
-          add the text you want to add as subscript.
-        </p>
+          <section>
+            <h3>Clear All Formatting</h3>
+            <p>
+              In any case you want to undo all or particular text formatting,
+              you can use last tool in toolbar.
+            </p>
+            <img alt="" src="/images/tutorial/undo.png" />
+          </section>
 
-        <h3>link</h3>
-        <p>
-          1st Type the word or link name which you want to make a link, then
-          select the portion & tap on link icon from toolbox.A window come in
-          front of your screen,type your full link IP address and add this. For
-          better knowledge please check the below GIF.
-        </p>
+          <section>
+            <h2>Preview</h2>
+            <p>
+              You can always preview your blog before publishing the blog. Also
+              you have to add keywords matching to your blog content in the
+              keyword box it will help your blog to cut the rank and better
+              SEO.<br />
+              Don't forget to share your blog among your social handles and
+              friends it will surely increase your blog reach.
+            </p>
+            <img alt="" src="/images/tutorial/preview_2.png" />
+          </section>
+        </div>
 
-        <h3>image</h3>
-        <p>
-          Tap on image icon from toolbox and type your image IP address and
-          press the add button. The image is added in your blog.
-        </p>
-
-        <h3>Vedio</h3>
-        <p>
-          Tap on vedio icon from toolbox and type your vedio IP address or vedio
-          link and press the add button. The vedio is added in your blog.
-        </p>
-
-        <h3>blogquote</h3>
-        <p>
-          blogquote is mainly use to give a special importance of some specific
-          portion. blogquote is super easy to use, just tap the button from
-          toolbox & start writing.
-        </p>
-
-        <h3>Code</h3>
-        <p>
-          code is use to write any kind of code in your blog. just tap the code
-          button from toolbox & start writing code in your blog
-        </p>
-      </div>
-    </main>
+        <div>
+          <h2 class="text-center">Happy Blogging!</h2>
+          <blockquote class="mb-8">
+            <p>
+              Hope with this knowledge, you can now start to create amazing
+              looking articles that are going to create a tremendous Impact!
+            </p>
+          </blockquote>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -187,6 +265,14 @@ export default {
   main {
     max-width: $large-screen;
     margin: auto;
+
+    img {
+      margin: 1rem auto 4rem;
+      box-shadow: 0 0 4px #4a4a4a;
+      border-radius: $micro-unit;
+      width: 100%;
+      object-fit: contain;
+    }
   }
 }
 </style>
