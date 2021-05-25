b5f5ab249f3a170fd4b27ff8f2ba27b56b4852b1
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-25 14:58:57 +0530

    WIP Adding Intersection Observer for InFeedAd.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- components/Common/GoogleAdsense/InFeedAd.vue
+++ components/Common/GoogleAdsense/InFeedAd.vue
@@ -1,31 +1,37 @@
 <template>
-  <ins
-    v-if="useAlternateAccount"
-    class="adsbygoogle"
-    data-ad-client="ca-pub-6503565742033720"
-    data-ad-format="fluid"
-    data-ad-layout-key="-5u+dg+44-5g+2z"
-    data-ad-slot="9825202092"
-    style="display: block"
-  />
-  <ins
-    v-else-if="useSmallAds"
-    class="adsbygoogle"
-    data-ad-client="ca-pub-9863542606738743"
-    data-ad-format="fluid"
-    data-ad-layout-key="-fb+5w+4e-db+86"
-    data-ad-slot="5290672704"
-    style="display: block"
-  />
-  <ins
-    v-else
-    class="adsbygoogle"
-    data-ad-client="ca-pub-9863542606738743"
-    data-ad-format="fluid"
-    data-ad-layout-key="-50+c6-23-9g+y5"
-    data-ad-slot="2010436071"
-    style="display: block"
-  />
+  <transition name="scale-up">
+    <div v-if="showAds" ref="ads">
+      <ins
+        v-if="useAlternateAccount"
+        class="adsbygoogle"
+        data-ad-client="ca-pub-6503565742033720"
+        data-ad-format="fluid"
+        data-ad-layout-key="-5u+dg+44-5g+2z"
+        data-ad-slot="9825202092"
+        style="display: block"
+      />
+      <ins
+        v-else-if="useSmallAds"
+        class="adsbygoogle"
+        data-ad-client="ca-pub-9863542606738743"
+        data-ad-format="fluid"
+        data-ad-layout-key="-fb+5w+4e-db+86"
+        data-ad-slot="5290672704"
+        style="display: block"
+      />
+      <ins
+        v-else
+        class="adsbygoogle"
+        data-ad-client="ca-pub-9863542606738743"
+        data-ad-format="fluid"
+        data-ad-layout-key="-50+c6-23-9g+y5"
+        data-ad-slot="2010436071"
+        style="display: block"
+      />
+    </div>
+
+    <div v-else ref="ads" />
+  </transition>
 </template>
 
 <script>
@@ -43,15 +49,48 @@ export default {
   data() {
     return {
       useAlternateAccount,
+      showAds: false,
     }
   },
   mounted() {
-    ;(window.adsbygoogle || []).push({})
+    // this.useDomBasedAds()
+    this.setupIntersectionObserver()
+  },
+  methods: {
+    useDomBasedAds() {
+      ;(window.adsbygoogle || []).push({})
 
-    process.env.NODE_ENV === 'production' &&
-      LogAnalyticsEvent(
-        window?.adsbygoogle?.loaded ? 'ads_requested' : 'ads_blocked'
-      )
+      process.env.NODE_ENV === 'production' &&
+        LogAnalyticsEvent(
+          window?.adsbygoogle?.loaded ? 'ads_requested' : 'ads_blocked'
+        )
+    },
+
+    setupIntersectionObserver() {
+      const observer = new IntersectionObserver(this.handleIntersection, {
+        threshold: 1,
+      })
+      const target = this.$refs.ads
+      observer.observe(target)
+    },
+
+    handleIntersection(entries) {
+      entries.map((entry) => {
+        if (entry.isIntersecting) {
+          // entry.target.classList.add('visible')
+          this.showAds = true
+          ;(window.adsbygoogle || []).push({})
+          process.env.NODE_ENV === 'production' &&
+            LogAnalyticsEvent(
+              window?.adsbygoogle?.loaded ? 'ads_requested' : 'ads_blocked'
+            )
+        } else {
+          this.showAds = false
+          // entry.target.classList.remove('visible')
+        }
+        return entry
+      })
+    },
   },
 }
 </script>
