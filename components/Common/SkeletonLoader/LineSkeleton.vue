5716acdd8c344001db43f58fec55e016de619e68
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-26 23:02:02 +0530

    Added Barebone Loaders
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/Common/SkeletonLoader/LineSkeleton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :style="styles" class="skeleton-loader" />
+</template>
+
+<script>
+export default {
+  name: 'LineSkeleton',
+  props: {
+    display: {
+      type: String,
+      required: false,
+      default: 'block',
+    },
+    width: {
+      type: String,
+      required: false,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      required: false,
+      default: '1rem',
+    },
+  },
+  data() {
+    return { styles: {} }
+  },
+  mounted() {
+    this.styles = {
+      display: this.display,
+      width: this.width,
+      height: this.height,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/skeletons';
+</style>
---
+++ components/Common/SkeletonLoader/UserResultSkeleton.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="user-results-skeleton">
+    <ImageSkeleton height="80px" radius="50%" width="80px" />
+
+    <main>
+      <LineSkeleton />
+      <LineSkeleton class="my-2" />
+      <LineSkeleton width="40%" />
+    </main>
+  </section>
+</template>
+
+<script>
+import ImageSkeleton from '~/components/Common/SkeletonLoader/ImageSkeleton'
+import LineSkeleton from '~/components/Common/SkeletonLoader/LineSkeleton'
+export default {
+  name: 'UserResultSkeleton',
+  components: { LineSkeleton, ImageSkeleton },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.user-results-skeleton {
+  display: grid;
+  grid-template-columns: 80px 1fr;
+  grid-gap: $milli-unit;
+  align-items: center;
+}
+</style>
