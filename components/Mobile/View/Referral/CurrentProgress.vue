c08b3831b36b687840a5b86637334f855e299048
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-05-26 23:03:30 +0530

    Added CurrentProgress.vue
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

---
+++ components/Mobile/View/Referral/CurrentProgress.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="current-progress-component">
+    <section>
+      <h5 class="px-4">Extra Features</h5>
+      <hr class="faded-divider" />
+      <LineProgress :percentage="percentage" class="px-4" />
+      <button class="secondary-outlined-btn">Go to Marketplace</button>
+    </section>
+  </div>
+</template>
+
+<script>
+import LineProgress from '~/components/Common/Tools/LineProgress'
+
+export default {
+  name: 'CurrentProgress',
+  components: { LineProgress },
+  data() {
+    return {
+      percentage: 0,
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      if (this.percentage > 99) return
+      this.percentage += 1
+    }, 100)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/all-variables';
+
+.current-progress-component {
+  section {
+    margin: 0 0 $standard-unit;
+
+    h5 {
+      margin: 0;
+      position: relative;
+    }
+
+    button {
+      display: block;
+      margin: 1rem auto;
+    }
+  }
+}
+</style>
