83e48bb480a22af67881da946c3b8598f6d7b931
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-01 22:00:14 +0530

    Added Skeleton Loader for my account Details Page
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- components/Accounts/UserTimeline.vue
+++ components/Accounts/UserTimeline.vue
@@ -26,7 +26,7 @@
     <main class="timeline pt-4">
       <section v-if="tabNumber === 0">
         <article v-for="blog in recentBlogs" :key="blog.identifier">
-          <LazyBlogPost :blog="blog" class="activity pt-4" />
+          <BlogPost :blog="blog" class="activity pt-4" />
 
           <InFeedAd />
         </article>
@@ -35,8 +35,14 @@
           <client-only>
             <infinite-loading key="infinite-blog-fetch" @infinite="loadBlogs">
               <template slot="spinner">
-                <LoadingIcon class="mt-4 mb-8" />
-                <p>Getting your blogs...</p>
+                <section class="my-8 pb-2 px-4">
+                  <LineSkeleton class="my-4" height="1.2rem" />
+                  <LineSkeleton class="mb-6" height="2.5rem" />
+                  <LineSkeleton class="my-4" height="150px" />
+                  <LineSkeleton class="my-4" width="80%" />
+                  <LineSkeleton class="my-4" width="50%" />
+                  <LineSkeleton class="my-4" width="30%" />
+                </section>
               </template>
 
               <template slot="error">
@@ -44,11 +50,33 @@
               </template>
 
               <template slot="no-more">
-                <div />
+                <section class="px-4 py-2">
+                  <h6 class="my-4">Good Going!</h6>
+                  <p class="mt-6 mb-8">
+                    Remember the more you write, the better your skills become.
+                  </p>
+                  <nuxt-link
+                    v-ripple
+                    :to="navigationRoutes.Home.Blogs.Create.AddBlog"
+                  >
+                    <button class="secondary-btn">Create More Blogs</button>
+                  </nuxt-link>
+                </section>
               </template>
 
               <template slot="no-results">
-                <p class="my-5">It's Lonely Here...</p>
+                <h6 class="my-4">It's Lonely Here...</h6>
+                <p class="mt-6 mb-8">
+                  Looks like, you haven't written any blogs yet!
+                  <br />
+                  Start Writing now to Start Earning!
+                </p>
+                <nuxt-link
+                  v-ripple
+                  :to="navigationRoutes.Home.Blogs.Create.AddBlog"
+                >
+                  <button class="secondary-btn">Create Blog</button>
+                </nuxt-link>
               </template>
             </infinite-loading>
           </client-only>
@@ -56,18 +84,32 @@
       </section>
 
       <section v-if="tabNumber === 1">
-        <article v-for="(post, index) in recentPosts" :key="post.identifier">
-          <CommunityPost :post="post" class="activity py-8" />
+        <article v-for="post in recentPosts" :key="post.identifier">
+          <LazyCommunityPost :post="post" class="activity py-8" />
 
-          <InFeedAd v-if="index % 2 === 0" use-small-ads />
+          <InFeedAd />
         </article>
 
         <footer v-if="!loadingError">
           <client-only>
             <infinite-loading key="infinite-posts-fetch" @infinite="loadPosts">
               <template slot="spinner">
-                <LoadingIcon class="mt-4 mb-8" />
-                <p>Getting your posts...</p>
+                <section class="my-8 pb-2 px-4">
+                  <section class="sample-response-reversed my-4">
+                    <ImageSkeleton height="40px" radius="50%" width="40px" />
+
+                    <aside>
+                      <LineSkeleton width="80%" />
+
+                      <LineSkeleton class="my-2" width="40%" />
+                    </aside>
+                  </section>
+                  <LineSkeleton class="my-4" height="1.2rem" />
+                  <LineSkeleton class="my-4" height="150px" />
+                  <LineSkeleton class="my-4" width="80%" />
+                  <LineSkeleton class="my-4" width="50%" />
+                  <LineSkeleton class="my-4" width="30%" />
+                </section>
               </template>
 
               <template slot="error">
@@ -75,11 +117,31 @@
               </template>
 
               <template slot="no-more">
-                <div />
+                <section class="px-4 py-2">
+                  <h6 class="my-4">Awesome, Job!</h6>
+                  <p class="mt-6 mb-8">
+                    Community post can help build a bond with your readers!
+                  </p>
+                  <nuxt-link
+                    v-ripple
+                    :to="navigationRoutes.Home.Community.Posts.add"
+                  >
+                    <button class="secondary-btn">Create More Posts</button>
+                  </nuxt-link>
+                </section>
               </template>
 
               <template slot="no-results">
-                <p class="my-5">It's Lonely Here...</p>
+                <h6 class="my-4">It's Lonely Here...</h6>
+                <p class="mt-6 mb-8">
+                  Looks like, you haven't created any posts yet!
+                </p>
+                <nuxt-link
+                  v-ripple
+                  :to="navigationRoutes.Home.Community.Posts.add"
+                >
+                  <button class="secondary-btn">Create Post</button>
+                </nuxt-link>
               </template>
             </infinite-loading>
           </client-only>
@@ -91,9 +153,15 @@
 <script>
 import { processLink } from '~/utils/utility'
 import endpoints from '~/api/endpoints'
+import InFeedAd from '~/components/Common/GoogleAdsense/InFeedAd'
+import BlogPost from '~/components/Blogs/BlogPost'
+import LineSkeleton from '~/components/Common/SkeletonLoader/LineSkeleton'
+import { navigationRoutes } from '~/navigation/navigationRoutes'
+import ImageSkeleton from '~/components/Common/SkeletonLoader/ImageSkeleton'
 
 export default {
   name: 'UserTimeline',
+  components: { ImageSkeleton, LineSkeleton, BlogPost, InFeedAd },
   props: {
     userUid: {
       type: String,
@@ -103,6 +171,7 @@ export default {
 
   data() {
     return {
+      navigationRoutes,
       tabNumber: 0,
       loadingError: false,
 
@@ -200,6 +269,13 @@ export default {
     &:nth-child(even) {
       background: $body-bg-alternate;
     }
+  }
+
+  section.sample-response-reversed {
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    grid-gap: 16px;
+    gap: 16px;
   }
 }
 </style>
