d501f0d0482e719a35633b8c24660aa5efc98ecf
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-01 22:00:14 +0530

    Added Skeleton Loader for my account Details Page
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- pages/Home/Account/Details.vue
+++ pages/Home/Account/Details.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="py-6 details-page">
-    <FallBackLoader v-if="loadingProfile" />
+    <aside v-if="loadingProfile" class="px-4 sample-response">
+      <section class="my-4">
+        <ImageSkeleton height="64px" radius="50%" width="64px" />
+
+        <aside>
+          <LineSkeleton height="2rem" />
+
+          <LineSkeleton class="my-4 mx-auto" height="1.2rem" width="70%" />
+
+          <LineSkeleton class="my-4 mx-auto" width="40%" />
+        </aside>
+      </section>
+
+      <section class="my-8">
+        <div>
+          <ImageSkeleton height="52px" radius="50%" width="52px" />
+          <LineSkeleton class="my-4" />
+        </div>
+        <div>
+          <ImageSkeleton height="52px" radius="50%" width="52px" />
+          <LineSkeleton class="my-4" />
+        </div>
+        <div>
+          <ImageSkeleton height="52px" radius="50%" width="52px" />
+          <LineSkeleton class="my-4" />
+        </div>
+      </section>
+
+      <LineSkeleton height="3rem" class="my-8 mx-auto" width="80%" />
+
+      <section>
+        <LineSkeleton class="my-4" height="1.2rem" />
+        <LineSkeleton class="mb-6" height="2.5rem" />
+        <LineSkeleton class="my-4" height="150px" />
+        <LineSkeleton class="my-4" width="80%" />
+        <LineSkeleton class="my-4" width="50%" />
+        <LineSkeleton class="my-4" width="30%" />
+      </section>
+    </aside>
 
     <LoadingError
       v-else-if="loadingError"
@@ -71,9 +109,13 @@ import { mapGetters } from 'vuex'
 import endpoints from '@/api/endpoints'
 import { navigationRoutes } from '~/navigation/navigationRoutes'
 import { showUITip } from '~/utils/utility'
+import LineSkeleton from '~/components/Common/SkeletonLoader/LineSkeleton'
+import ImageSkeleton from '~/components/Common/SkeletonLoader/ImageSkeleton'
+import UserTimeline from '~/components/Accounts/UserTimeline'
 
 export default {
   name: 'Details',
+  components: { UserTimeline, ImageSkeleton, LineSkeleton },
   layout: 'ResponsiveApp',
   middleware: 'isAuthenticated',
 
@@ -209,6 +251,27 @@ export default {
 
     .other-info {
       text-align: left;
+    }
+  }
+
+  aside.sample-response {
+    section:first-child {
+      display: grid;
+      grid-template-columns: 64px 1fr;
+      grid-gap: 16px;
+      gap: 16px;
+    }
+
+    section:nth-child(2) {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 16px;
+      gap: 16px;
+
+      div {
+        display: grid;
+        place-items: center;
+      }
     }
   }
 }
