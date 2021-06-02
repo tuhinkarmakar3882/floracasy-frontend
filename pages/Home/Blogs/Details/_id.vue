7dd93e4f155d35c7fc9fc8d038c4bc639df4f1ed
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-02 20:11:14 +0530

    Removed Logs
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- pages/Home/Blogs/Details/_id.vue
+++ pages/Home/Blogs/Details/_id.vue
@@ -337,7 +337,6 @@ export default {
           `<p><br /></p><p><br /></p> <ins class="adsbygoogle" data-ad-client="ca-pub-9863542606738743" data-ad-format="fluid" data-ad-layout-key="-50+c6-23-9g+y5" data-ad-slot="2010436071" style="display: block"></ins>`
         )
       } catch (e) {
-        console.log(e)
         return safeHTML
       }
     },
