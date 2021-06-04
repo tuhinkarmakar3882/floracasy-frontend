8fd6741047bdfe0ca2cbca866b939157223c61c6
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-04 13:04:06 +0530

    Added Message System endpoints.js
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- api/endpoints.js
+++ api/endpoints.js
@@ -74,6 +74,7 @@ const endpoints = {
   },
   message_system: {
     chats: '/allow_authenticated/message_system/chats/',
+    getMessages: '/allow_authenticated/message_system/chats/{roomId}/',
     conversation: '/allow_authenticated/message_system/conversation/',
   },
   feedback_collection: {
