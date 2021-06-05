9eecd680cf00321929b7c3240d2e942a08c6f566
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-06-05 21:28:02 +0530

    Updated InstallBadge.vue
    Signed-off-by: Tuhin Karmakar <tuhin.karmakar@thoughtworks.com>

--- components/Common/Tools/InstallBadge.vue
+++ components/Common/Tools/InstallBadge.vue
@@ -1,25 +1,49 @@
 <template>
-  <transition name="scale-down">
-    <div v-if="showInstallPrompt" class="install-badge-component">
-      <i
-        v-if="showCloseButton"
-        v-ripple
-        class="mdi mdi-close mdi-18px px-4"
-        @click="hideInstallBadge"
-      />
+  <div class="install-badge-component-container">
+    <transition name="scale-down">
+      <section v-if="showInstallPrompt" class="install-badge-component">
+        <i
+          v-if="showCloseButton"
+          v-ripple
+          class="mdi mdi-close mdi-18px px-4"
+          @click="hideInstallBadge"
+        />
 
-      <section @click="installPWA">
-        <p>Floracasy</p>
-        <span>Get our free app. It won't take up space in your device!</span>
+        <section @click="showOverlay = true">
+          <p>Floracasy</p>
+          <span>Get our free app. It won't take up space in your device!</span>
+        </section>
+
+        <i
+          v-ripple
+          class="mdi mdi-download-circle mdi-36px ml-auto px-4"
+          @click="showOverlay = true"
+        />
       </section>
+    </transition>
 
-      <i
-        v-ripple
-        class="mdi mdi-download-circle mdi-36px ml-auto px-4"
-        @click="installPWA"
-      />
-    </div>
-  </transition>
+    <transition name="scale-down">
+      <aside v-if="showOverlay" class="loader" @click="showOverlay = false">
+        <section class="my-6">
+          <a
+            href="https://play.google.com/store/apps/details?id=com.floracasy.twa&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"
+            ><img
+              alt="Get it on Google Play"
+              src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+              style="width: 250px"
+          /></a>
+        </section>
+
+        <section class="my-6" @click="installPWA">
+          <img
+            alt="Get it as a Progressive Web App"
+            style="width: 220px"
+            src="https://blueislanddigital.com/wp-content/uploads/2019/10/Progressive-Web-App.png"
+          />
+        </section>
+      </aside>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -38,6 +62,7 @@ export default {
   data() {
     return {
       showInstallPrompt: false,
+      showOverlay: false,
     }
   },
 
@@ -108,38 +133,56 @@ export default {
 <style lang="scss" scoped>
 @import 'assets/all-variables';
 
-.install-badge-component {
-  height: 3 * $x-large-unit;
-  display: flex;
-  align-items: center;
-  background: $active-gradient;
-  box-shadow: $up-only-box-shadow;
-
-  * {
-    color: $white;
-    transition: all 300ms ease-in-out;
-  }
-
-  i {
-    display: grid;
-    place-items: center;
+.install-badge-component-container {
+  .install-badge-component {
     height: 3 * $x-large-unit;
-    border-radius: $micro-unit $micro-unit 0 0;
+    display: flex;
+    align-items: center;
+    background: $active-gradient;
+    box-shadow: $up-only-box-shadow;
+
+    * {
+      color: $white;
+      transition: all 300ms ease-in-out;
+    }
+
+    i {
+      display: grid;
+      place-items: center;
+      height: 3 * $x-large-unit;
+      border-radius: $micro-unit $micro-unit 0 0;
+    }
+
+    section {
+      p {
+        font-size: 1rem;
+        font-family: $Prata;
+        font-weight: 500;
+      }
+
+      span {
+        font-size: 0.7rem;
+        line-height: 1;
+        margin: 0;
+        padding: 0;
+      }
+    }
   }
 
-  section {
-    p {
-      font-size: 1rem;
-      font-family: $Prata;
-      font-weight: 500;
-    }
-
-    span {
-      font-size: 0.7rem;
-      line-height: 1;
-      margin: 0;
-      padding: 0;
-    }
+  aside.loader {
+    position: fixed;
+    top: $zero-unit;
+    left: $zero-unit;
+    right: $zero-unit;
+    bottom: $zero-unit;
+    z-index: $bring-to-front;
+    background: rgba($black, 0.85);
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
